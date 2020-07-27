aecedadd87d7908d1aeaf03ce0a16d393e768a8b
Jerry Dong <shichongdong80@163.com>
2020-07-27 16:20:52 +0800

    Json serializer

--- App/ThMEPEngineCore/ThMEPEngineCoreApp.cs
+++ App/ThMEPEngineCore/ThMEPEngineCoreApp.cs
@@ -1,4 +1,8 @@
 ﻿using Autodesk.AutoCAD.Runtime;
+using System.IO;
+using System.Collections;
+using System.Web.Script.Serialization;
+using System;
 
 namespace ThMEPEngineCore
 {
@@ -12,6 +16,35 @@ namespace ThMEPEngineCore
         public void Terminate()
         {
             //
+        }
+
+        [CommandMethod("TIANHUACAD", "THEXTRACTMODEL", CommandFlags.Modal)]
+        public void ThExtractModel()
+        {
+            JavaScriptSerializer _JavaScriptSerializer = new JavaScriptSerializer();
+            var _JsonBlocks = ReadTxt(Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Blocks.json"));
+            ArrayList _ListBlocks = _JavaScriptSerializer.Deserialize<ArrayList>(_JsonBlocks);
+
+
+            var _JsonElementTypes = ReadTxt(Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ElementTypes.json"));
+            ArrayList _ListElementTypes = _JavaScriptSerializer.Deserialize<ArrayList>(_JsonElementTypes);
+        }
+
+        private string ReadTxt(string _Path)
+        {
+            try
+            {
+                using (StreamReader _StreamReader = File.OpenText(_Path))
+                {
+                    return _StreamReader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
     }
 }
