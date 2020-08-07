242c63e0cdf5c881cda8ee8158225428b59263a8
Jerry Dong <shichongdong80@163.com>
2020-08-07 16:11:15 +0800

    Define ThIfcSpatialStructureElement

--- App/ThMEPEngineCore/Model/ThIfcWallStandardCase.cs
+++ App/ThMEPEngineCore/Model/ThIfcWallStandardCase.cs
@@ -1,19 +1,7 @@
-﻿using Xbim.Ifc4.SharedBldgElements;
-
-namespace ThMEPEngineCore.Model
+﻿namespace ThMEPEngineCore.Model
 {
     public class ThIfcWallStandardCase : ThIfcWall
     {
-        private IfcWallStandardCase Impl { get; set; }
-        public ThIfcWallStandardCase(string name)
-        {
-            var model = ThIfcStoreService.Instance.Model;
-            using (var tx = model.BeginTransaction())
-            {
-                Impl = model.Instances.New<IfcWallStandardCase>();
-                Impl.Name = name;
-                tx.Commit();
-            }
-        }
+        //
     }
 }
