d2c0f580a3ba186ecb380451463c742a54083fea
Jerry Dong <shichongdong80@163.com>
2020-08-09 16:28:36 +0800

    Implement ThProjectDirStructureUtils

---
+++ App/ThMEPEngineCore/ThProjectDirStructureUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThMEPEngineCore
+{
+    public class ThProjectDirStructureUtils
+    {
+        /// <summary>
+        /// 地下室
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static DirectoryInfo Garage(string root)
+        {
+            return new DirectoryInfo(Path.Combine(root, "地下室"));
+        }
+    }
+}
