89208f7a9f8a79a9977afa5654b2d40584679372
Jerry Dong <shichongdong80@163.com>
2020-08-27 22:10:39 +0800

    Initialize ThMEPWSS project

---
+++ App/ThMEPWSS/ThMEPWSSApp.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace ThMEPWSS
+{
+    public class ThMEPWSSApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //
+        }
+
+        public void Terminate()
+        {
+            //
+        }
+    }
+}
