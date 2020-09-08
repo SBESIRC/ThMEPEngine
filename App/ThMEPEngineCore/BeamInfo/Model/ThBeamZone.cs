94de2c2d49c560c512f7cbd9e945dbfe15edc8b9
Jerry Dong <shichongdong80@163.com>
2020-09-08 22:42:34 +0800

    Implement ThBeamZone

---
+++ App/ThMEPEngineCore/BeamInfo/Model/ThBeamZone.cs
@@ -0,0 +1,13 @@
+﻿using AcPolygon = Autodesk.AutoCAD.DatabaseServices.Polyline;
+
+namespace ThMEPEngineCore.BeamInfo.Model
+{
+    public class ThBeamZone
+    { 
+        public AcPolygon Region { get; private set; }
+        public ThBeamZone(AcPolygon polygon)
+        {
+            //
+        }
+    }
+}
