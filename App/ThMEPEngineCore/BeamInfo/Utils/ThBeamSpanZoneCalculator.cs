47d18bc7fdc18493f742c667e25a05274d3ccb9e
dongj <shichongdong80@163.com>
2020-09-10 13:16:19 +0800

    Define ThBeamSpanZoneCalculator (#42)

---
+++ App/ThMEPEngineCore/BeamInfo/Utils/ThBeamSpanZoneCalculator.cs
@@ -0,0 +1,22 @@
+﻿using AcPolygon = Autodesk.AutoCAD.DatabaseServices.Polyline;
+
+namespace ThMEPEngineCore.BeamInfo.Utils
+{
+    public class ThBeamSpanZoneCalculator
+    {
+        public static AcPolygon CalculateProtectedRegion(AcPolygon polygon)
+        {
+            return Smooth(polygon);
+        }
+
+        public static AcPolygon CalculateDistributableRegion(AcPolygon polygon)
+        {
+            return Smooth(polygon);
+        }
+
+        private static AcPolygon Smooth(AcPolygon polygon)
+        {
+            return null;
+        }
+    }
+}
