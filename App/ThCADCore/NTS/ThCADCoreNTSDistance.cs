58055ef16f13e83057b276bb5a31aaa4e565c6d6
Jerry Dong <shichongdong80@163.com>
2020-09-15 15:51:03 +0800

    Implement ThCADCoreNTSDistance, point to polyline distance

---
+++ App/ThCADCore/NTS/ThCADCoreNTSDistance.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using NetTopologySuite.Algorithm.Distance;
+
+namespace ThCADCore.NTS
+{
+    public static class ThCADCoreNTSDistance
+    {
+        public static double Distance(this Point3d point, Polyline polyline)
+        {
+            var distance = new PointPairDistance();
+            DistanceToPoint.ComputeDistance(polyline.ToNTSLineString(), 
+                point.ToNTSCoordinate(), distance);
+            return distance.Distance;
+        }
+    }
+}
