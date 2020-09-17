0f4ab649b715e61ee7c246c024dd7c601bc1a0ed
Jerry Dong <shichongdong80@163.com>
2020-09-17 17:58:09 +0800

    Delete unused utility methods

--- App/Utilities/ThPolylineExtension.cs
+++ App/Utilities/ThPolylineExtension.cs
@@ -7,58 +7,8 @@ using Autodesk.AutoCAD.DatabaseServices;
  
 namespace TianHua.AutoCAD.Utility.ExtensionTools
 {
-    /// <summary>
-    /// Provides the Offset() extension method for the Polyline type
-    /// </summary>
     public static class ThPolylineExtension
     {
-        /// <summary>
-        /// Enumeration of offset side options
-        /// </summary>
-        public enum OffsetSide
-        {
-            In, Out, Left, Right, Both
-        }
-
-        /// <summary>
-        /// Offset the source polyline to specified side(s).
-        /// </summary>
-        /// <param name="source">The polyline to be offseted.</param>
-        /// <param name="offsetDist">The offset distance.</param>
-        /// <param name="side">The offset side(s).</param>
-        /// <returns>A polyline sequence resulting from the offset of the source polyline.</returns>
-        public static IEnumerable<Polyline> Offset(this Polyline source, double offsetDist, OffsetSide side)
-        {
-            offsetDist = Math.Abs(offsetDist);
-            using (var plines = new DisposableSet<Polyline>())
-            {
-                IEnumerable<Polyline> offsetRight = source.GetOffsetCurves(offsetDist).Cast<Polyline>();
-                plines.AddRange(offsetRight);
-                IEnumerable<Polyline> offsetLeft = source.GetOffsetCurves(-offsetDist).Cast<Polyline>();
-                plines.AddRange(offsetLeft);
-                double areaRight = offsetRight.Select(pline => pline.Area).Sum();
-                double areaLeft = offsetLeft.Select(pline => pline.Area).Sum();
-                switch (side)
-                {
-                    case OffsetSide.In:
-                        return plines.RemoveRange(
-                           areaRight < areaLeft ? offsetRight : offsetLeft);
-                    case OffsetSide.Out:
-                        return plines.RemoveRange(
-                           areaRight < areaLeft ? offsetLeft : offsetRight);
-                    case OffsetSide.Left:
-                        return plines.RemoveRange(offsetLeft);
-                    case OffsetSide.Right:
-                        return plines.RemoveRange(offsetRight);
-                    case OffsetSide.Both:
-                        plines.Clear();
-                        return offsetRight.Concat(offsetLeft);
-                    default:
-                        return null;
-                }
-            }
-        }
-
         public static Polyline ExpandBy(this Polyline rectangle, double deltaX, double deltaY)
         {
             var v0 = rectangle.GetPoint3dAt(0) - rectangle.GetPoint3dAt(3);
