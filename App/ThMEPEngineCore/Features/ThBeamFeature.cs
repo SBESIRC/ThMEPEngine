78ee5ab8e98dc80c5286f4513d41bdb93cd7f644
Jerry Dong <shichongdong80@163.com>
2020-09-21 15:09:45 +0800

    Define ThBeamFeature

---
+++ App/ThMEPEngineCore/Features/ThBeamFeature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTopologySuite.Features;
+using ThMEPEngineCore.Model;
+
+namespace ThMEPEngineCore.Features
+{
+    public class ThBeamFeature
+    {
+        public Feature Impl { get; private set; }
+        public ThBeamFeature(ThIfcLineBeam lineBeam)
+        {
+            Impl = new Feature();
+        }
+    }
+}
