f3ff2eb277739f7e55f70794c0720b817715c149
Jerry Dong <shichongdong80@163.com>
2020-09-21 14:55:33 +0800

    Define ThBeamGeoJsonWriter

---
+++ App/ThMEPEngineCore/IO/ThBeamGeoJsonWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NetTopologySuite.IO;
+using ThMEPEngineCore.Model;
+
+namespace ThMEPEngineCore.IO
+{
+    public class ThBeamGeoJsonWriter : GeoJsonWriter
+    {
+        public string Write(ThIfcLineBeam lineBeam)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
