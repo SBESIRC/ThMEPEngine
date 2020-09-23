9cd3cfa3d7be96d7bba22a092f683379c5b8bab9
liugs <trasure@126.com>
2020-09-23 14:54:29 +0800

    integrate draw profile code for print linked outlines

---
+++ App/ThCADExtension/ThLayerTool.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Linq2Acad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThCADExtension
+{
+    public static class ThLayerTool
+    {
+        /// <summary>
+        /// 创建新的图层
+        /// </summary>
+        /// <param name="allLayers"></param>
+        /// <param name="aimLayer"></param>
+        public static void CreateLayer(string aimLayer, Color color)
+        {
+            LayerTableRecord layerRecord = null;
+            using (var db = AcadDatabase.Active())
+            {
+                foreach (var layer in db.Layers)
+                {
+                    if (layer.Name.Equals(aimLayer))
+                    {
+                        layerRecord = db.Layers.Element(aimLayer);
+                        break;
+                    }
+                }
+
+                // 创建新的图层
+                if (layerRecord == null)
+                {
+                    layerRecord = db.Layers.Create(aimLayer);
+                    layerRecord.Color = color;
+                    layerRecord.IsPlottable = false;
+                }
+            }
+        }
+    }
+}
