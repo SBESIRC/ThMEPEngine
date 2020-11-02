3638a734c912a6e75840a0811a6103ae398f6cec
Jerry Dong <shichongdong80@163.com>
2020-11-02 16:57:07 +0800

    Define THYWG command

---
+++ App/TianHua.Electrical.UI/ElectricalUIApp.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace TianHua.Electrical.UI
+{
+    public class ElectricalUIApp : IExtensionApplication
+    {
+        private fmSmokeLayout SmokeLayoutUI { get; set; }
+
+
+        public void Initialize()
+        {
+            SmokeLayoutUI = null;
+        }
+
+        public void Terminate()
+        {
+            SmokeLayoutUI = null;
+        }
+
+        [CommandMethod("TIANHUACAD", "THYWG", CommandFlags.Modal)]
+        public void THYWG()
+        {
+            if (SmokeLayoutUI == null)
+            {
+                SmokeLayoutUI = new fmSmokeLayout();
+            }
+            AcadApp.ShowModelessDialog(SmokeLayoutUI);
+        }
+    }
+}
