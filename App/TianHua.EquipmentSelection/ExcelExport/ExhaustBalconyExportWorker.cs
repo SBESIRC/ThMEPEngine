f11af1671fdd34e46892e7c17476221ab0ebcc15
york <chenyue1991@sina.com>
2020-11-02 15:17:28 +0800

    FanSelection project, remove Excel COM

--- App/TianHua.EquipmentSelection/ExcelExport/ExhaustBalconyExportWorker.cs
+++ App/TianHua.EquipmentSelection/ExcelExport/ExhaustBalconyExportWorker.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
 using TianHua.FanSelection.Function;
 using TianHua.FanSelection.Model;
 using TianHua.Publics.BaseCode;
@@ -12,37 +8,37 @@ namespace TianHua.FanSelection.ExcelExport
 {
     class ExhaustBalconyExportWorker : BaseExportWorker
     {
-        public override void ExportToExcel(IFanModel exhaustfanmodel, Worksheet setsheet, Worksheet targetsheet, FanDataModel fandatamodel, ExcelRangeCopyOperator copyoperator)
+        public override void ExportToExcel(IFanModel exhaustfanmodel, ExcelWorksheet setsheet, ExcelWorksheet targetsheet, FanDataModel fandatamodel, ExcelRangeCopyOperator copyoperator)
         {
             ExhaustCalcModel exhaustModel = exhaustfanmodel as ExhaustCalcModel;
-            setsheet.SetCellValue("D2", fandatamodel.FanNum);
-            setsheet.SetCellValue("D3", fandatamodel.Name);
-            setsheet.SetCellValue("D5", FuncStr.NullToStr(Math.Max(exhaustModel.MinAirVolume.NullToDouble(), exhaustModel.Final_CalcAirVolum.NullToDouble())));
-            setsheet.SetCellValue("D6", exhaustModel.Final_CalcAirVolum);
-            setsheet.SetCellValue("D7", FuncStr.NullToStr(Math.Round(ExhaustModelCalculator.GetDtValue(exhaustModel))));
-            setsheet.SetCellValue("D8", FuncStr.NullToStr(Math.Round(ExhaustModelCalculator.GetOverfloorCalcAirVolum(exhaustModel))));
-            setsheet.SetCellValue("D9", FuncStr.NullToStr(Math.Round(exhaustModel.Spill_FireOpening.NullToDouble()+ exhaustModel.Spill_OpenBalcony.NullToDouble())));
-            setsheet.SetCellValue("D10", "1");
-            setsheet.SetCellValue("D11", FuncStr.NullToStr(Math.Round(ExhaustModelCalculator.GeTValue(exhaustModel))));
-            setsheet.SetCellValue("D12", "1.2");
-            setsheet.SetCellValue("D13", "293.15");
-            setsheet.SetCellValue("D14", FuncStr.NullToStr(Math.Round(0.7 * exhaustModel.HeatReleaseRate.NullToDouble())));
-            setsheet.SetCellValue("D15", FuncStr.NullToStr(Math.Round(exhaustModel.HeatReleaseRate.NullToDouble())));
-            setsheet.SetCellValue("D16", "1.01");
-            setsheet.SetCellValue("D17", FuncStr.NullToStr(Math.Round(exhaustModel.SpaceHeight.NullToDouble())));
-            setsheet.SetCellValue("D18", FuncStr.NullToStr(Math.Round(exhaustModel.MinAirVolume.NullToDouble())));
-            setsheet.SetCellValue("D19", FuncStr.NullToStr(Math.Round(exhaustModel.SpaceHeight.NullToDouble())));
-            setsheet.SetCellValue("D20", FuncStr.NullToStr(Math.Round(exhaustModel.Spill_FuelBalcony.NullToDouble())));
-            setsheet.SetCellValue("D21", FuncStr.NullToStr(Math.Round(exhaustModel.Spill_BalconySmokeBottom.NullToDouble())));
-            setsheet.SetCellValue("D22", FuncStr.NullToStr(Math.Round(exhaustModel.Spill_FireOpening.NullToDouble())));
-            setsheet.SetCellValue("D23", FuncStr.NullToStr(Math.Round(exhaustModel.Spill_OpenBalcony.NullToDouble())));
+            setsheet.Cells["D2"].Value = fandatamodel.FanNum;
+            setsheet.Cells["D3"].Value = fandatamodel.Name;
+            setsheet.Cells["D5"].Value = FuncStr.NullToStr(Math.Max(exhaustModel.MinAirVolume.NullToDouble(), exhaustModel.Final_CalcAirVolum.NullToDouble()));
+            setsheet.Cells["D6"].Value = exhaustModel.Final_CalcAirVolum;
+            setsheet.Cells["D7"].Value = FuncStr.NullToStr(Math.Round(ExhaustModelCalculator.GetDtValue(exhaustModel)));
+            setsheet.Cells["D8"].Value = FuncStr.NullToStr(Math.Round(ExhaustModelCalculator.GetOverfloorCalcAirVolum(exhaustModel)));
+            setsheet.Cells["D9"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.Spill_FireOpening.NullToDouble() + exhaustModel.Spill_OpenBalcony.NullToDouble()));
+            setsheet.Cells["D10"].Value = "1";
+            setsheet.Cells["D11"].Value = FuncStr.NullToStr(Math.Round(ExhaustModelCalculator.GeTValue(exhaustModel)));
+            setsheet.Cells["D12"].Value = "1.2";
+            setsheet.Cells["D13"].Value = "293.15";
+            setsheet.Cells["D14"].Value = FuncStr.NullToStr(Math.Round(0.7 * exhaustModel.HeatReleaseRate.NullToDouble()));
+            setsheet.Cells["D15"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.HeatReleaseRate.NullToDouble()));
+            setsheet.Cells["D16"].Value = "1.01";
+            setsheet.Cells["D17"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.SpaceHeight.NullToDouble()));
+            setsheet.Cells["D18"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.MinAirVolume.NullToDouble()));
+            setsheet.Cells["D19"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.SpaceHeight.NullToDouble()));
+            setsheet.Cells["D20"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.Spill_FuelBalcony.NullToDouble()));
+            setsheet.Cells["D21"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.Spill_BalconySmokeBottom.NullToDouble()));
+            setsheet.Cells["D22"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.Spill_FireOpening.NullToDouble()));
+            setsheet.Cells["D23"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.Spill_OpenBalcony.NullToDouble()));
 
             switch (exhaustModel.ExhaustCalcType)
             {
                 case "空间-净高小于等于6m":
                 case "走道回廊-房间内和走道或回廊都设置排烟":
                     SetWithArea(exhaustModel, setsheet);
-                    copyoperator.CopyRangeToOtherSheet(setsheet, "A1:D28", targetsheet);
+                    copyoperator.CopyRangeToOtherSheet(setsheet, 1, 1, 28, 4, targetsheet);
                     break;
                 case "空间-净高大于6m":
                 case "空间-汽车库":
@@ -50,28 +46,28 @@ namespace TianHua.FanSelection.ExcelExport
                 case "中庭-周围场所设有排烟系统":
                 case "中庭-周围场所不设排烟系统":
                     SetWithoutArea(exhaustModel, setsheet);
-                    copyoperator.CopyRangeToOtherSheet(setsheet, "A1:D27", targetsheet);
+                    copyoperator.CopyRangeToOtherSheet(setsheet, 1, 1, 27, 4, targetsheet);
                     break;
                 default:
                     break;
             }
         }
-        private void SetWithArea(ExhaustCalcModel exhaustModel, Worksheet setsheet)
+        private void SetWithArea(ExhaustCalcModel exhaustModel, ExcelWorksheet setsheet)
         {
-            setsheet.SetCellValue("D24", FuncStr.NullToStr(Math.Round(exhaustModel.CoveredArea.NullToDouble())));
-            setsheet.SetCellValue("D25", FuncStr.NullToStr(Math.Round(exhaustModel.HeatReleaseRate.NullToDouble())));
-            setsheet.SetCellValue("D26", FuncStr.NullToStr(Math.Round(exhaustModel.SmokeThickness.NullToDouble())));
-            setsheet.SetCellValue("C27", exhaustModel.SmokeFactorOption);
-            setsheet.SetCellValue("D27", FuncStr.NullToStr(Math.Round(exhaustModel.SmokeFactorValue.NullToDouble(), 1)));
-            setsheet.SetCellValue("D28", FuncStr.NullToStr(Math.Round(exhaustModel.MaxSmokeExtraction.NullToDouble())));
+            setsheet.Cells["D24"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.CoveredArea.NullToDouble()));
+            setsheet.Cells["D25"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.HeatReleaseRate.NullToDouble()));
+            setsheet.Cells["D26"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.SmokeThickness.NullToDouble()));
+            setsheet.Cells["C27"].Value = exhaustModel.SmokeFactorOption;
+            setsheet.Cells["D27"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.SmokeFactorValue.NullToDouble(), 1));
+            setsheet.Cells["D28"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.MaxSmokeExtraction.NullToDouble()));
         }
-        private void SetWithoutArea(ExhaustCalcModel exhaustModel, Worksheet setsheet)
+        private void SetWithoutArea(ExhaustCalcModel exhaustModel, ExcelWorksheet setsheet)
         {
-            setsheet.SetCellValue("D24", FuncStr.NullToStr(Math.Round(exhaustModel.HeatReleaseRate.NullToDouble())));
-            setsheet.SetCellValue("D25", FuncStr.NullToStr(Math.Round(exhaustModel.SmokeThickness.NullToDouble())));
-            setsheet.SetCellValue("C26", exhaustModel.SmokeFactorOption);
-            setsheet.SetCellValue("D26", FuncStr.NullToStr(Math.Round(exhaustModel.SmokeFactorValue.NullToDouble(), 1)));
-            setsheet.SetCellValue("D27", FuncStr.NullToStr(Math.Round(exhaustModel.MaxSmokeExtraction.NullToDouble())));
+            setsheet.Cells["D24"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.HeatReleaseRate.NullToDouble()));
+            setsheet.Cells["D25"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.SmokeThickness.NullToDouble()));
+            setsheet.Cells["C26"].Value = exhaustModel.SmokeFactorOption;
+            setsheet.Cells["D26"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.SmokeFactorValue.NullToDouble(), 1));
+            setsheet.Cells["D27"].Value = FuncStr.NullToStr(Math.Round(exhaustModel.MaxSmokeExtraction.NullToDouble()));
         }
 
     }
--- App/TianHua.EquipmentSelection/ExcelExport/FireFrontExportWorker.cs
+++ App/TianHua.EquipmentSelection/ExcelExport/FireFrontExportWorker.cs
@@ -1,54 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
 using TianHua.FanSelection.Model;
 
 namespace TianHua.FanSelection.ExcelExport
 {
     public class FireFrontExportWorker : BaseExportWorker
     {
-        public override void ExportToExcel(IFanModel fanmodel, Worksheet setsheet, Worksheet targetsheet, FanDataModel fandatamodel, ExcelRangeCopyOperator copyoperator)
+        public override void ExportToExcel(IFanModel fanmodel, ExcelWorksheet setsheet, ExcelWorksheet targetsheet, FanDataModel fandatamodel, ExcelRangeCopyOperator copyoperator)
         {
             FireFrontModel fireFrontModel = fanmodel as FireFrontModel;
-            setsheet.SetCellValue("D2", fandatamodel.FanNum);
-            setsheet.SetCellValue("D3", fandatamodel.Name);
-            setsheet.SetCellValue("D4", Math.Max(fireFrontModel.QueryValue, (fireFrontModel.DoorOpeningVolume + fireFrontModel.LeakVolume)).ToString());
-            setsheet.SetCellValue("D5", (fireFrontModel.DoorOpeningVolume + fireFrontModel.LeakVolume).ToString());
-            setsheet.SetCellValue("D6", fireFrontModel.DoorOpeningVolume.ToString());
-            setsheet.SetCellValue("D8", fireFrontModel.LeakVolume.ToString());
-            setsheet.SetCellValue("D9", fireFrontModel.OverAk.ToString());
-            setsheet.SetCellValue("D10", "1");
-            setsheet.SetCellValue("D11", Math.Min(fireFrontModel.Count_Floor, 3).ToString());
-            setsheet.SetCellValue("D12", (fireFrontModel.Length_Valve * fireFrontModel.Width_Valve/1000000).ToString());
-            setsheet.SetCellValue("D13", (fireFrontModel.Count_Floor < 3 ? 0 : fireFrontModel.Count_Floor-3).ToString());
-            setsheet.SetCellValue("D14", fireFrontModel.QueryValue.ToString());
-            setsheet.SetCellValue("D15", fireFrontModel.Count_Floor.ToString());
-            setsheet.SetCellValue("D16", GetLoadRange(fireFrontModel.Load.ToString()));
-            setsheet.SetCellValue("D17", fireFrontModel.Length_Valve.ToString());
-            setsheet.SetCellValue("D18", fireFrontModel.Width_Valve.ToString());
+            setsheet.Cells["D2"].Value = fandatamodel.FanNum;
+            setsheet.Cells["D3"].Value = fandatamodel.Name;
+            setsheet.Cells["D4"].Value = Math.Max(fireFrontModel.QueryValue, (fireFrontModel.DoorOpeningVolume + fireFrontModel.LeakVolume)).ToString();
+            setsheet.Cells["D5"].Value = (fireFrontModel.DoorOpeningVolume + fireFrontModel.LeakVolume).ToString();
+            setsheet.Cells["D6"].Value = fireFrontModel.DoorOpeningVolume.ToString();
+            setsheet.Cells["D8"].Value = fireFrontModel.LeakVolume.ToString();
+            setsheet.Cells["D9"].Value = fireFrontModel.OverAk.ToString();
+            setsheet.Cells["D10"].Value = "1";
+            setsheet.Cells["D11"].Value = Math.Min(fireFrontModel.Count_Floor, 3).ToString();
+            setsheet.Cells["D12"].Value = (fireFrontModel.Length_Valve * fireFrontModel.Width_Valve / 1000000).ToString();
+            setsheet.Cells["D13"].Value = (fireFrontModel.Count_Floor < 3 ? 0 : fireFrontModel.Count_Floor - 3).ToString();
+            setsheet.Cells["D14"].Value = fireFrontModel.QueryValue.ToString();
+            setsheet.Cells["D15"].Value = fireFrontModel.Count_Floor.ToString();
+            setsheet.Cells["D16"].Value = GetLoadRange(fireFrontModel.Load.ToString());
+            setsheet.Cells["D17"].Value = fireFrontModel.Length_Valve.ToString();
+            setsheet.Cells["D18"].Value = fireFrontModel.Width_Valve.ToString();
 
             int formRowIndex = 19;
             for (int i = 1; i <= fireFrontModel.FrontRoomDoors2.Sum(f => f.Value.Count); i++)
             {
-                setsheet.CopyRangeToNext("A19", "D21", 3 * i);
+                setsheet.CopyRangeToNext(19, 1, 21, 4, 3 * i);
             }
             foreach (var floor in fireFrontModel.FrontRoomDoors2)
             {
                 for (int i = 0; i < floor.Value.Count; i++)
                 {
-                    setsheet.SetCellValue("A" + formRowIndex, floor.Key);
-                    setsheet.SetCellValue("B" + formRowIndex, "前室疏散门" + (i + 1));
-                    setsheet.SetCellValue("D" + formRowIndex, floor.Value[i].Height_Door_Q.ToString());
-                    setsheet.SetCellValue("D" + (formRowIndex + 1), floor.Value[i].Width_Door_Q.ToString());
-                    setsheet.SetCellValue("D" + (formRowIndex + 2), floor.Value[i].Count_Door_Q.ToString());
+                    setsheet.Cells["A" + formRowIndex].Value = floor.Key;
+                    setsheet.Cells["B" + formRowIndex].Value = "前室疏散门" + (i + 1);
+                    setsheet.Cells["D" + formRowIndex].Value = floor.Value[i].Height_Door_Q.ToString();
+                    setsheet.Cells["D" + (formRowIndex + 1)].Value = floor.Value[i].Width_Door_Q.ToString();
+                    setsheet.Cells["D" + (formRowIndex + 2)].Value = floor.Value[i].Count_Door_Q.ToString();
                     formRowIndex += 3;
                 }
             }
-
-            copyoperator.CopyRangeToOtherSheet(setsheet, "A1:D"+ (formRowIndex - 1).ToString(), targetsheet);
+            copyoperator.CopyRangeToOtherSheet(setsheet, 1, 1, formRowIndex - 1, 4, targetsheet);
         }
     }
 }
