99c5d6311726346fcffe14eecde8af4c5d8c8673
dongj <shichongdong80@163.com>
2020-11-11 18:13:30 +0800

    Rename ThMEPCui module name (#131)

--- App/ThCui/Properties/AssemblyInfo.cs
+++ App/ThCui/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ThCui")]
+[assembly: AssemblyTitle("ThMEPCui")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ThCui")]
+[assembly: AssemblyProduct("ThMEPCui")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
