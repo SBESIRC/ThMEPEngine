7eb4ee8837cec8af024a451d6a9033fd7213c1ae
Jerry Dong <shichongdong80@163.com>
2020-11-11 13:37:56 +0800

    Initialize Plumbing project

---
+++ App/TianHua.Plumbing.UI/PlumbingUIApp.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace TianHua.Plumbing.UI
+{
+    public class PlumbingUIApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //
+        }
+
+        public void Terminate()
+        {
+            //
+        }
+    }
+}
