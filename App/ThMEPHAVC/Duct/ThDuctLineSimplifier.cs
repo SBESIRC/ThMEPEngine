d07288f93fa916b9da13897f936833451aca711a
Jerry Dong <shichongdong80@163.com>
2020-11-20 13:58:30 +0800

    Fix build error

--- App/ThMEPHAVC/Duct/ThDuctLineSimplifier.cs
+++ App/ThMEPHAVC/Duct/ThDuctLineSimplifier.cs
@@ -13,11 +13,14 @@ namespace ThMEPHAVC.Duct
             public double DistGap2Extend { get; set; }
 
             public double AngleTolerance { get; set; }
+
+            public double ArcChord { get; set; }
         }
 
         public static List<Line> Simplifier(DBObjectCollection curves, SimplifyParameters parameters)
         {
             return ThMEPLineExtension.LineSimplifier(curves,
+                parameters.ArcChord,
                 parameters.DistGap2Extend,
                 parameters.DistGap2Merge,
                 parameters.AngleTolerance);
