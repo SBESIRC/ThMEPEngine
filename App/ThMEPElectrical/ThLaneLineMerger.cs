63ccfa76926d64639882c729d43caae8df55e3ac
Jerry Dong <shichongdong80@163.com>
2020-11-25 18:18:48 +0800

    Define ThLaneLineMerger

---
+++ App/ThMEPElectrical/ThLaneLineMerger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPElectrical
+{
+    public class ThLaneLineMerger
+    {
+        /// <summary>
+        /// 生成主车道
+        /// </summary>
+        /// <param name="lines"></param>
+        /// <param name="startPoint"></param>
+        /// <returns></returns>
+        public static DBObjectCollection LineMerge(DBObjectCollection lines, Point3d startPoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
