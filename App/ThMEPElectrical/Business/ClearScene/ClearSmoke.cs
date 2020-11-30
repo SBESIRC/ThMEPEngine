c48cb02c973e33ed4d77ed55d8b4286e672f39b2
coconavy <986489314@qq.com>
2020-11-30 17:30:28 +0800

    add one scene to clear hatch

--- App/ThMEPElectrical/Business/ClearScene/ClearSmoke.cs
+++ App/ThMEPElectrical/Business/ClearScene/ClearSmoke.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ThMEPElectrical.Geometry;
+using ThMEPElectrical.Business.BlindAreaReminder;
+using ThMEPElectrical.Model;
 
 namespace ThMEPElectrical.Business.ClearScene
 {
     public class ClearSmoke
     {
         private List<Polyline> m_polylines;
+        private List<PolygonInfo> m_polygonInfos = new List<PolygonInfo>();
 
         public static void MakeClearSmoke(List<Polyline> polylines)
         {
@@ -23,6 +26,8 @@ namespace ThMEPElectrical.Business.ClearScene
         public ClearSmoke(List<Polyline> polylines)
         {
             m_polylines = polylines;
+            foreach (var poly in m_polylines)
+                m_polygonInfos.Add(new PolygonInfo(poly));
         }
 
         public void DoClear()
@@ -55,6 +60,8 @@ namespace ThMEPElectrical.Business.ClearScene
                     }
                 }
             }
+
+            ClearHatch.MakeClearHatch(m_polygonInfos);
         }
 
         private bool IsInvalid(Point3d point)
