b058bd0280caf972ace5e794ea5d9f9b58b718f8
Jerry Dong <shichongdong80@163.com>
2020-12-07 11:43:04 +0800

    THTCD command with frame

--- App/ThMEPElectrical/ThLaneLineCmds.cs
+++ App/ThMEPElectrical/ThLaneLineCmds.cs
@@ -2,10 +2,12 @@
 using AcHelper;
 using Linq2Acad;
 using System.Linq;
+using ThCADExtension;
 using ThMEPEngineCore.Engine;
 using ThMEPEngineCore.Service;
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.DatabaseServices;
 
 namespace ThMEPElectrical
@@ -18,7 +20,14 @@ namespace ThMEPElectrical
             using (AcadDatabase acadDatabase = AcadDatabase.Active())
             using (ThLaneRecognitionEngine laneLineEngine = new ThLaneRecognitionEngine())
             {
-                laneLineEngine.Recognize(Active.Database, new Point3dCollection());
+                var result = Active.Editor.GetEntity("\n请选择需要提取车道中心线的范围框线");
+                if (result.Status != PromptStatus.OK)
+                {
+                    return;
+                }
+
+                Polyline frame = acadDatabase.Element<Polyline>(result.ObjectId);
+                laneLineEngine.Recognize(Active.Database, frame.Vertices());
                 var lines = laneLineEngine.Spaces.Select(o => o.Boundary).ToList();
                 ThLaneLineSimplifier.Simplify(lines.ToCollection(), 1500).ForEach(o => acadDatabase.ModelSpace.Add(o));
             }
