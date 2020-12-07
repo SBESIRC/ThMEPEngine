96f523df515c6aaa950ce4fb541c5d16ee5dbad0
Jerry Dong <shichongdong80@163.com>
2020-12-07 16:12:39 +0800

    Add ThMEPElectrical solution and ThMEPLighting project

---
+++ App/ThMEPLighting/ThMEPLightingApp.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace ThMEPLighting
+{
+    public class ThMEPLightingApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
