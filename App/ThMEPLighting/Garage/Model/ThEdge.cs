06fcbfa500a344f9a4ffd07e66a95cb900ae9a33
jackwhoknow <trasure@126.com>
2021-01-15 15:01:42 +0800

    implement ThLaneLineGraphEngine

---
+++ App/ThMEPLighting/Garage/Model/ThEdge.cs
@@ -0,0 +1,18 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThMEPLighting.Garage.Model
+{
+    public class ThEdge<TVertex> :Edge<TVertex> where TVertex:ThVertex
+    {
+        public double Length { get; set; }
+        public ThEdge(TVertex source, TVertex target) : base(source, target)
+        {
+            Length = source.Position.DistanceTo(target.Position);
+        }
+    }
+}
