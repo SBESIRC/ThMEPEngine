8c563ae788c5446460a998d6822f9bdc9f54956f
Jerry Dong <shichongdong80@163.com>
2021-01-22 11:21:05 +0800

    Define ThMEPCollisionDetector (#240)

---
+++ App/ThMEPEngineCore/Algorithm/ThMEPCollisionDetector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ThCADCore.NTS;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPEngineCore.Algorithm
+{
+    public class ThMEPCollisionDetector
+    {
+        private Dictionary<string, ThCADCoreNTSSpatialIndex> SpatialIndexes { get; set; }
+
+        public ThMEPCollisionDetector()
+        {
+            SpatialIndexes = new Dictionary<string, ThCADCoreNTSSpatialIndex>();
+        }
+
+        public void AddSpatialIndex(ThCADCoreNTSSpatialIndex spatialIndex, string context)
+        {
+            SpatialIndexes[context] = spatialIndex;
+        }
+
+        public void RemoveSpatialIndex(string context)
+        {
+            if (SpatialIndexes.ContainsKey(context))
+            {
+                SpatialIndexes.Remove(context);
+            }
+        }
+
+        public bool CollidePoly(Polyline poly)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
