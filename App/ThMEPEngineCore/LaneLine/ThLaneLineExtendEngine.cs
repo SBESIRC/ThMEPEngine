615df876d5dd7eb7da5772d87a5a558d9726410a
Jerry Dong <shichongdong80@163.com>
2021-01-31 21:56:22 +0800

    ThLaneLineExtendEngine, IsProperIntersects, fix precision issue using KdTree

--- App/ThMEPEngineCore/LaneLine/ThLaneLineExtendEngine.cs
+++ App/ThMEPEngineCore/LaneLine/ThLaneLineExtendEngine.cs
@@ -5,6 +5,7 @@ using ThCADExtension;
 using Dreambuild.AutoCAD;
 using System.Collections.Generic;
 using NetTopologySuite.Geometries;
+using NetTopologySuite.Index.KdTree;
 using Autodesk.AutoCAD.DatabaseServices;
 
 namespace ThMEPEngineCore.LaneLine
@@ -29,7 +30,14 @@ namespace ThMEPEngineCore.LaneLine
         {
             var geometry = lines.ToMultiLineString().Intersection(line.ToNTSGeometry());
             // 判断是否存在多个交点（但是要排查共线的情况）
-            return geometry is MultiPoint;
+            if (geometry is MultiPoint points)
+            {
+                // 考虑到精度问题，在一定范围内的点认为是一个点
+                var kdTree = new KdTree<Point>(1.0);
+                var kdNodes = points.Geometries.Cast<Point>().Select(o => kdTree.Insert(o.Coordinate, o));
+                return kdNodes.Distinct().Count() > 1;
+            }
+            return false;
         }
 
         private static List<Line> CreateExtendedLines(DBObjectCollection lines)
