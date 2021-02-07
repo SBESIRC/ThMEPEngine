486eb19ba60ad04fd07e7e1ce1738422d1136a84
Jerry Dong <shichongdong80@163.com>
2021-02-07 13:00:34 +0800

    Implement Line.ShortenLine

--- App/ThCADExtension/ThLineExtension.cs
+++ App/ThCADExtension/ThLineExtension.cs
@@ -10,10 +10,26 @@ namespace ThCADExtension
             return line.StartPoint.GetVectorTo(line.EndPoint).GetNormal();
         }
 
+        // https://adndevblog.typepad.com/autocad/2012/07/using-curveextend.html
         public static Line ExtendLine(this Line line, double distance)
         {
             var direction = line.LineDirection();
             return new Line(line.StartPoint - direction * distance, line.EndPoint + direction * distance);
+        }
+
+        // https://www.keanw.com/2010/07/shortening-a-set-of-autocad-lines-using-net.html
+        public static Line ShortenLine(this Line line, double distance)
+        {
+            double ep = line.EndParam;
+            double sp = line.StartParam;
+            double delta = (ep - sp) * (distance / line.Length);
+            DoubleCollection dc = new DoubleCollection()
+            {
+                sp + delta,
+                ep - delta,
+            };
+            DBObjectCollection objs = line.GetSplitCurves(dc);
+            return objs[1] as Line;
         }
     }
 }
