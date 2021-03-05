ca18dcb72198264efb467e4e0854e5addf57b6cb
Jerry Dong <shichongdong80@163.com>
2021-03-05 17:14:31 +0800

    Add Cybros hard-coded token
    (cherry picked from commit 3b961c900e3e3d49387d28846d802bcb0f182f49)

--- App/ThAnalytics/SDK/APIMessage.cs
+++ App/ThAnalytics/SDK/APIMessage.cs
@@ -9,7 +9,7 @@ namespace ThAnalytics.SDK
     {
         public static THConfig m_Config = new THConfig()
         {
-            Token = "",
+            Token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjaW0tYXV0by1hY2NvdW50QHRoYXAuY29tLmNuIiwic2NwIjoidXNlciIsImV4cCI6MTY0NjQ2NzM5MSwiYXVkIjoiQ0FEIiwiaWF0IjoxNjE0OTMxMzkxLCJqdGkiOiI4MDI2OWY3My0xNWQ3LTRkNTgtOTJlNC1jZTgxY2Q1ODkwZTUifQ.6SEE_6xzmmvDwj6SYFaaOhWHOmQN4atKPsXs2PXtWOc",
             AppVersion = "V1.0",
             ServerUrl = @"https://cybros.thape.com.cn",
             SSOUrl = @"https://sso.thape.com.cn/users/sign_in"
