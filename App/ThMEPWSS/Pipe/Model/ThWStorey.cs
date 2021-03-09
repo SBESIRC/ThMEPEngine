9e3dc94e89745d7653661dbec488e3479bca9ff2
Jerry Dong <shichongdong80@163.com>
2021-03-09 15:28:14 +0800

    Define ThWStoreys model

---
+++ App/ThMEPWSS/Pipe/Model/ThWStorey.cs
@@ -0,0 +1,15 @@
+﻿using ThCADExtension;
+using ThMEPEngineCore.Model;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPWSS.Pipe.Model
+{
+    public class ThWStoreys : ThIfcSpatialStructureElement
+    { 
+        public ThBlockReferenceData Data { get; set; }
+        public ThWStoreys(ObjectId obj)
+        {
+            Data = new ThBlockReferenceData(obj);
+        }
+    }
+}
