06c068b7c3204ff0433c7017132e28a12d9d8e17
jackwhoknow <trasure@126.com>
2021-03-19 10:23:44 +0800

    Access Boundary

--- App/ThMEPWSS/Pipe/Service/ThTopFloorCompositeBalconyRoomService.cs
+++ App/ThMEPWSS/Pipe/Service/ThTopFloorCompositeBalconyRoomService.cs
@@ -34,7 +34,7 @@ namespace ThMEPWSS.Pipe.Service
             foreach (var room in compositeBalconyRoom)
             {
                 var bboundary = FirstFloorSpace.Boundary as Polyline;
-                if (GeomUtils.PtInLoop(bboundary, room.Balcony.Space.Boundary.GetCenter()))
+                if (GeomUtils.PtInLoop(bboundary, room.Balcony.Boundary.GetCenter()))
                 {
                     balconyroom.Add(room);
                 }
