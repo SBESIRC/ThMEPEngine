b655c853ea48998e4443eb067ab0eb2d398cf790
jackwhoknow <trasure@126.com>
2021-03-24 10:37:42 +0800

    ThTopFloorDivisionLineService replace ThIfcSpace with ThIfcRoom

--- App/ThMEPWSS/Pipe/Service/ThTopFloorDivisionLineService.cs
+++ App/ThMEPWSS/Pipe/Service/ThTopFloorDivisionLineService.cs
@@ -1,33 +1,33 @@
-﻿using System.Collections.Generic;
-using ThCADCore.NTS;
-using Autodesk.AutoCAD.DatabaseServices;
+﻿using ThCADCore.NTS;
 using ThMEPWSS.Pipe.Geom;
 using ThMEPEngineCore.Model;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
 
 namespace ThMEPWSS.Pipe.Service
 {
     public class ThTopFloorDivisionLineService
     {
         private List<Line> DivisionLines { get; set; }
-        private ThIfcSpace Space { get; set; }
+        private ThIfcRoom Room { get; set; }
         private ThCADCoreNTSSpatialIndex SpatialIndex { get; set; }
         private ThTopFloorDivisionLineService(
-           ThIfcSpace space,
+           ThIfcRoom room,
            List<Line> divisionLines)
         {
             DivisionLines = divisionLines;
-            Space = space;
+            Room = room;
             var objs = new DBObjectCollection();
             SpatialIndex = new ThCADCoreNTSSpatialIndex(objs);
         }
         public static List<Line> Find(
-            ThIfcSpace space,
+            ThIfcRoom room,
             List<Line> divisionLines)
         {
             var lines = new List<Line>();
             foreach(var line in divisionLines)
             {
-                var boundary = space.Boundary as Polyline;
+                var boundary = room.Boundary as Polyline;
                 if (GeomUtils.PtInLoop(boundary, line.StartPoint)&& GeomUtils.PtInLoop(boundary, line.EndPoint))
                 {
                     lines.Add(line);
