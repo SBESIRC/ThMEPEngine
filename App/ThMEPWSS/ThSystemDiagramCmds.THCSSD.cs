bd8ec7f83dd4b6af138cc83d0aa7a0272de0e887
wangwenguang <wangwenguang@thape.com.cn>
2021-05-07 10:32:12 +0800

    Tian hua create suply system diagam command

---
+++ App/ThMEPWSS/ThSystemDiagramCmds.THCSSD.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using ThCADExtension;
+using ThMEPWSS.Command;
+using ThMEPWSS.Pipe.Engine;
+using Dbg = ThMEPWSS.DebugNs.ThDebugTool;
+using Linq2Acad;
+using ThMEPWSS.Assistant;
+using ThMEPWSS.Pipe.Service;
+using ThCADCore.NTS;
+using DotNetARX;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using Autodesk.AutoCAD.EditorInput;
+using System.IO;
+
+namespace ThMEPWSS
+{
+    public partial class ThSystemDiagramCmds
+    {
+        /// <summary>
+        /// Tian Hua Create water suply system diagram
+        /// </summary>
+        [CommandMethod("TIANHUACAD", "THCSSD", CommandFlags.Modal)]
+        public void ThCreateWaterSuplySystemDiagram()
+        {
+            try
+            {
+                //using (var cmd = new ThRainSystemDiagramCmd())
+                //{
+                //    cmd.Execute();
+                //}
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
