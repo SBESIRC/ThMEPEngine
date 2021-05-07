8875c3b09c2c76e8b6c37da166f127747c5227eb
wangwenguang <wangwenguang@thape.com.cn>
2021-05-07 10:51:34 +0800

    Added two files

---
+++ App/ThMEPWSS/Command/ThWaterSuplySystemDiagramCmd.cs
@@ -0,0 +1,22 @@
+﻿using AcHelper.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThMEPWSS.Command
+{
+    class ThWaterSuplySystemDiagramCmd : IAcadCommand, IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
