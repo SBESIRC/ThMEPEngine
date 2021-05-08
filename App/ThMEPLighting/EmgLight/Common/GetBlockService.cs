bb3ec31eae28810624433657d6cdc60f631e4596
zxr201 <crystal_zxr@hotmail.com>
2021-05-08 12:51:54 +0800

    Feature/emg connect5 (#364)
    * add new blk info
    * fix group emg and evac light buffer. Not use fix value, using scale * size +buffer

--- App/ThMEPLighting/EmgLight/Common/GetBlockService.cs
+++ App/ThMEPLighting/EmgLight/Common/GetBlockService.cs
@@ -24,9 +24,27 @@ namespace ThMEPLighting.EmgLight.Common
         Dictionary<BlockReference, BlockReference> evacR2Ceiling = new Dictionary<BlockReference, BlockReference>();
         Dictionary<BlockReference, BlockReference> evacLR2Ceiling = new Dictionary<BlockReference, BlockReference>();
 
-        public void getBlocksData(Polyline bufferFrame, ThMEPOriginTransformer transformer,List<Polyline> holes = null)
+        Dictionary<BlockReference, BlockReference> enterN = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> enterNCeiling = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> floor = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> floorCeiling = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> floorEvac = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> floorEvacCeiling = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacUpCeiling = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacPost = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacSq = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacSqD = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacCir = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacCirD = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> refuge = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> refugeCeiling = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> refugeE = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> refugeECeiling = new Dictionary<BlockReference, BlockReference>();
+        Dictionary<BlockReference, BlockReference> evacLR = new Dictionary<BlockReference, BlockReference>();
+
+        public void getBlocksData(Polyline bufferFrame, ThMEPOriginTransformer transformer, List<Polyline> holes = null)
         {
-            emgLight = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EmgLightBlockName, transformer,holes);
+            emgLight = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EmgLightBlockName, transformer, holes);
             evacR = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacRBlockName, transformer, holes);
             evacRL = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacLRBlockName, transformer, holes);
 
@@ -42,8 +60,43 @@ namespace ThMEPLighting.EmgLight.Common
             evacR2Ceiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacR2LineCeilingBlockName, transformer, holes);
             evacLR2Ceiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacLR2LineCeilingBlockName, transformer, holes);
 
+
+            emgLight = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EmgLightBlockName, transformer, holes);
+            evacR = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacRBlockName, transformer, holes);
+            evacRL = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacLRBlockName, transformer, holes);
+
+            exitE = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.ExitEBlockName, transformer, holes);
+            exitS = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.ExitSBlockName, transformer, holes);
+            exitECeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.ExitECeilingBlockName, transformer, holes);
+            exitSCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.ExitSCeilingBlockName, transformer, holes);
+
+            enter = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EnterBlockName, transformer, holes);
+            enterCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EnterCeilingBlockName, transformer, holes);
+
+            evacCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacCeilingBlockName, transformer, holes);
+            evacR2Ceiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacR2LineCeilingBlockName, transformer, holes);
+            evacLR2Ceiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacLR2LineCeilingBlockName, transformer, holes);
+
+            enterN = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EnterNBlockName, transformer, holes);
+            enterNCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EnterNCeilingBlockName, transformer, holes);
+            floor = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.FloorBlockName, transformer, holes);
+            floorCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.FloorCeilingBlockName, transformer, holes);
+            floorEvac = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.FloorEvacBlockName, transformer, holes);
+            floorEvacCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.FloorEvacCeilingBlockName, transformer, holes);
+            evacUpCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacUpCeilingBlockName, transformer, holes);
+            evacPost = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacPostBlockName, transformer, holes);
+            evacSq = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacSqBlockName, transformer, holes);
+            evacSqD = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacSqDBlockName, transformer, holes);
+            evacCir = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacCirBlockName, transformer, holes);
+            evacCirD = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacCirDBlockName, transformer, holes);
+            refuge = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.RefugeBlockName, transformer, holes);
+            refugeCeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.RefugeCeilingBlockName, transformer, holes);
+            refugeE = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.RefugeEBlockName, transformer, holes);
+            refugeECeiling = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.RefugeECeilingBlockName, transformer, holes);
+            evacLR = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EmgLightLayerName, ThMEPLightingCommon.EvacLRCeilingBlockName, transformer, holes);
+
             //var ALE = GetSourceDataService.ExtractBlock(bufferFrame, ThMEPLightingCommon.EPowerLayerName, ThMEPLightingCommon.ALEBlockName, transformer);
-            
+
         }
 
         public void getBlockList(Dictionary<EmgBlkType.BlockType, List<BlockReference>> blockList)
@@ -52,62 +105,59 @@ namespace ThMEPLighting.EmgLight.Common
             blockList.Add(EmgBlkType.BlockType.evac, evacR.Select(x => x.Value).ToList());
             blockList[EmgBlkType.BlockType.evac].AddRange(evacRL.Select(x => x.Value).ToList());
 
-            blockList.Add(EmgBlkType.BlockType.exit, exitE.Select(x => x.Value).ToList());
-            blockList[EmgBlkType.BlockType.exit].AddRange(exitS.Select(x => x.Value).ToList());
-            blockList[EmgBlkType.BlockType.exit].AddRange(exitECeiling.Select(x => x.Value).ToList());
-            blockList[EmgBlkType.BlockType.exit].AddRange(exitSCeiling.Select(x => x.Value).ToList());
+            blockList.Add(EmgBlkType.BlockType.otherSecBlk, exitE.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(exitS.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(exitECeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(exitSCeiling.Select(x => x.Value).ToList());
 
-            blockList.Add(EmgBlkType.BlockType.enter, enter.Select(x => x.Value).ToList());
-            blockList[EmgBlkType.BlockType.enter].AddRange(enterCeiling.Select(x => x.Value).ToList());
+            //blockList.Add(EmgBlkType.BlockType.otherSecBlk, enter.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange( enter.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(enterCeiling.Select(x => x.Value).ToList());
 
-            blockList.Add(EmgBlkType.BlockType.evacCeiling, evacCeiling.Select(x => x.Value).ToList());
-            blockList[EmgBlkType.BlockType.evacCeiling].AddRange(evacR2Ceiling.Select(x => x.Value).ToList());
-            blockList[EmgBlkType.BlockType.evacCeiling].AddRange(evacLR2Ceiling.Select(x => x.Value).ToList());
+            //blockList.Add(EmgBlkType.BlockType.otherSecBlk, evacCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacR2Ceiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacLR2Ceiling.Select(x => x.Value).ToList());
+
+
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(enterN.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(enterNCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(floor.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(floorCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(floorEvac.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(floorEvacCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacUpCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacPost.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacSq.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacSqD.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacCir.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacCirD.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(refuge.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(refugeCeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(refugeE.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(refugeECeiling.Select(x => x.Value).ToList());
+            blockList[EmgBlkType.BlockType.otherSecBlk].AddRange(evacLR.Select(x => x.Value).ToList());
 
             //blockList.Add(EmgBlkType.BlockType.ale, ALE.Select(x => x.Value).ToList());
 
         }
 
-        //public static void projectToXY(ref Dictionary<EmgBlkType.BlockType, List<BlockReference>> blockList)
-        //{
-        //    var typeList = blockList.Select(x => x.Key).ToList();
-        //    foreach (var type in typeList)
-        //    {
-        //        blockList[type].ForEach(x => x.Position = new Point3d(x.Position.X, x.Position.Y, 0));
-
-        //    }
-        //}
-
         public static Dictionary<string, List<Point3d>> blkConnectDict()
         {
-            //public static readonly string EmgLightBlockName = "E-BFEL810";      //消防应急灯图块名
-            //public static readonly string EvacRBlockName = "E-BFEL200";     //疏散指示灯图块
-            //public static readonly string EvacLRBlockName = "E-BFEL210";     //疏散指示灯图块
+            Dictionary<string, List<Point3d>> blkConnectDict = new Dictionary<string, List<Point3d>>();//左，下，右，上
 
-            //public static readonly string ExitEBlockName = "E-BFEL100";           //紧急出口图块
-            //public static readonly string ExitSBlockName = "E-BFEL102";           //紧急出口图块
+            Point3d rightTopPt1 = new Point3d(2.5, 1.25, 0);//普通出口
 
-            //public static readonly string ExitECeilingBlockName = "E-BFEL101";//紧急出口吊装
-            //public static readonly string ExitSCeilingBlockName = "E-BFEL103";//紧急出口吊装
+            List<Point3d> type2 = new List<Point3d> { new Point3d(-2.5, 0, 0), new Point3d(0, -1.25, 0), new Point3d(2.5, 0, 0), new Point3d(0, 2.5, 0) }; //普通吊装
+            List<Point3d> type3 = new List<Point3d> { new Point3d(-1.25, 2.5, 0), new Point3d(0, 0, 0), new Point3d(1.25, 2.5, 0), new Point3d(0, 6.25, 0) };//落地灯柱
 
-            //public static readonly string EnterBlockName = "E-BFEL140";//出口/禁止
-            //public static readonly string EnterCeilingBlockName = "E-BFEL141";//出口/禁止吊装
-
-            //public static readonly string EvacCeilingBlockName = "E-BFEL201";//疏散指示灯吊装
-            //public static readonly string EvacR2LineCeilingBlockName = "E-BFEL201-1";//疏散指示灯吊装
-            //public static readonly string EvacLR2LineCeilingBlockName = "E-BFEL211-1";//疏散指示灯吊装
-
-
-            Dictionary<string, List<Point3d>> blkConnectDict = new Dictionary<string, List<Point3d>>();
-
-            Point3d rightTopPt1 = new Point3d(2.5, 1.25, 0);
-
-            List<Point3d> type2 = new List<Point3d> { new Point3d(-2.5, 0, 0), new Point3d(0, -1.25, 0), new Point3d(2.5, 0, 0), new Point3d(0, 2.5, 0) };
+            Point3d rightTopPt2 = new Point3d(2, 2, 0);//埋地
 
             blkConnectDict.Add("E-BFEL810", new List<Point3d> { new Point3d (-1.25,0,0),
                                                                 new Point3d (0,-2.25,0),
                                                                 new Point3d (1.25,0,0),
                                                                 new Point3d (0,1.25,0)});
+
             addBlkConnectDict(blkConnectDict, "E-BFEL200", rightTopPt1);
             addBlkConnectDict(blkConnectDict, "E-BFEL210", rightTopPt1);
             addBlkConnectDict(blkConnectDict, "E-BFEL100", rightTopPt1);
@@ -119,6 +169,24 @@ namespace ThMEPLighting.EmgLight.Common
             blkConnectDict.Add("E-BFEL201", type2);
             blkConnectDict.Add("E-BFEL201-1", type2);
             blkConnectDict.Add("E-BFEL211-1", type2);
+
+            addBlkConnectDict(blkConnectDict, "E-BFEL130", rightTopPt1);
+            blkConnectDict.Add("E-BFEL131", type2);
+            addBlkConnectDict(blkConnectDict, "E-BFEL110", rightTopPt1);
+            blkConnectDict.Add("E-BFEL111", type2);
+            blkConnectDict.Add("E-BFEL161", type2);
+            blkConnectDict.Add("E-BFEL161-1", type2);
+            blkConnectDict.Add("E-BFEL221", type2);
+            blkConnectDict.Add("E-BFEL223", type3);
+            addBlkConnectDict(blkConnectDict, "E-BFEL240", rightTopPt2);
+            addBlkConnectDict(blkConnectDict, "E-BFEL241", rightTopPt2);
+            addBlkConnectDict(blkConnectDict, "E-BFEL250", rightTopPt2);
+            addBlkConnectDict(blkConnectDict, "E-BFEL251", rightTopPt2);
+            addBlkConnectDict(blkConnectDict, "E-BFEL120", rightTopPt1);
+            blkConnectDict.Add("E-BFEL121", type2);
+            addBlkConnectDict(blkConnectDict, "E-BFEL122", rightTopPt1);
+            blkConnectDict.Add("E-BFEL123", type2);
+            blkConnectDict.Add("E-BFEL211", type2);
 
             return blkConnectDict;
 
@@ -134,10 +202,10 @@ namespace ThMEPLighting.EmgLight.Common
             blkConnectDict.Add(key, new List<Point3d> { leftPt, bottomPt, rightPt, topPt });
         }
 
-        public static void getBlkOutLine(BlockReference blk, Dictionary<string, List<Point3d>> blkSizeDict,out Polyline blkOutline, out List<Point3d> connectPt, BlockReference groupBlk = null)
+        public static void getBlkOutLine(BlockReference blk, Dictionary<string, List<Point3d>> blkSizeDict, out Polyline blkOutline, out List<Point3d> connectPt, BlockReference groupBlk = null)
         {
             var ptList = blkSizeDict[blk.Name].Select(x => x).ToList();
-             connectPt = ptList.Select(x => x.TransformBy(blk.BlockTransform)).ToList();
+            connectPt = ptList.Select(x => x.TransformBy(blk.BlockTransform)).ToList();
 
             if (groupBlk != null)
             {
@@ -153,7 +221,7 @@ namespace ThMEPLighting.EmgLight.Common
                 connectPt = ptList.Select(x => x.TransformBy(blk.BlockTransform)).ToList();
             }
 
-             blkOutline = new Polyline();
+            blkOutline = new Polyline();
 
             blkOutline.AddVertexAt(0, new Point2d(ptList[0].X, ptList[3].Y), 0, 0, 0);
             blkOutline.AddVertexAt(1, new Point2d(ptList[0].X, ptList[1].Y), 0, 0, 0);
@@ -162,17 +230,9 @@ namespace ThMEPLighting.EmgLight.Common
             blkOutline.TransformBy(blk.BlockTransform);
             blkOutline.Closed = true;
 
-           
+
         }
 
-        public static double getScale(BlockReference blk)
-        {
-            double scale = 100;
-
-            scale= blk.ScaleFactors.X;
-
-            return scale;
-        }
 
     }
 }
