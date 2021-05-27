ae540feb6f13db4117d3fcb4993026842ac57053
Jerry Dong <shichongdong80@163.com>
2021-05-27 18:17:31 +0800

    ThCADCoreNTSSpatialIndex support MPolygon

--- App/ThCADCore/NTS/ThCADCoreNTSSpatialIndex.cs
+++ App/ThCADCore/NTS/ThCADCoreNTSSpatialIndex.cs
@@ -108,6 +108,10 @@ namespace ThCADCore.NTS
                 {
                     return poly.ToNTSPolygon();
                 }
+                else if (obj is MPolygon mPolygon)
+                {
+                    return mPolygon.ToNTSPolygon();
+                }
                 else
                 {
                     throw new NotSupportedException();
@@ -139,51 +143,18 @@ namespace ThCADCore.NTS
             Geometries.Keys.ForEach(g => Engine.Insert(g.EnvelopeInternal, g));
         }
 
+
         /// <summary>
         /// Crossing selection
         /// </summary>
-        /// <param name="polyline"></param>
+        /// <param name="entity"></param>
         /// <returns></returns>
-        public DBObjectCollection SelectCrossingPolygon(Polyline polyline)
+        public DBObjectCollection SelectCrossingPolygon(Entity entity)
         {
-            var geometry = ToNTSPolygon(polyline);
+            var geometry = ToNTSPolygon(entity);
             return CrossingFilter(
                 Query(geometry.EnvelopeInternal),
                 ThCADCoreNTSService.Instance.PreparedGeometryFactory.Create(geometry));
-        }
-
-        public DBObjectCollection SelectCrossingPolygon(MPolygon mPolygon)
-        {
-            /*
-             * 线获取MPolygon外圈内所有的物体
-             * 减去洞内包括的物体
-             */
-            var loops = mPolygon.Loops();
-            var objs = SelectCrossingPolygon(loops[0]);
-            for (int i = 1; i < loops.Count; i++)
-            {
-                foreach (DBObject innerObj in SelectWindowPolygon(loops[i]))
-                {
-                    objs.Remove(innerObj);
-                }
-            }
-            return objs;
-        }
-
-        public DBObjectCollection SelectCrossingPolygon(Entity ent)
-        {
-            if (ent is Polyline poly)
-            {
-                return SelectCrossingPolygon(poly);
-            }
-            else if (ent is MPolygon mPolygon)
-            {
-                return SelectCrossingPolygon(mPolygon);
-            }
-            else
-            {
-                return new DBObjectCollection();
-            }
         }
 
         public DBObjectCollection SelectCrossingPolygon(Point3dCollection polygon)
@@ -211,47 +182,13 @@ namespace ThCADCore.NTS
         /// <summary>
         /// Window selection
         /// </summary>
-        /// <param name="polyline"></param>
+        /// <param name="entity"></param>
         /// <returns></returns>
-        public DBObjectCollection SelectWindowPolygon(Polyline polyline)
+        public DBObjectCollection SelectWindowPolygon(Entity entity)
         {
-            var geometry = ToNTSPolygon(polyline);
+            var geometry = ToNTSPolygon(entity);
             return WindowFilter(Query(geometry.EnvelopeInternal),
                 ThCADCoreNTSService.Instance.PreparedGeometryFactory.Create(geometry));
-        }
-
-        public DBObjectCollection SelectWindowPolygon(MPolygon mPolygon)
-        {
-            /*
-             * 线获取MPolygon里面的物体
-             * 减去洞内包括的物体
-             */
-            var loops = mPolygon.Loops();
-            var objs = SelectWindowPolygon(loops[0]);
-            for (int i = 1; i < loops.Count; i++)
-            {
-                foreach (DBObject innerObj in SelectCrossingPolygon(loops[i]))
-                {
-                    objs.Remove(innerObj);
-                }
-            }
-            return objs;
-        }
-
-        public DBObjectCollection SelectWindowPolygon(Entity ent)
-        {
-            if (ent is Polyline poly)
-            {
-                return SelectWindowPolygon(poly);
-            }
-            else if (ent is MPolygon mPolygon)
-            {
-                return SelectWindowPolygon(mPolygon);
-            }
-            else
-            {
-                return new DBObjectCollection();
-            }
         }
 
         /// <summary>
