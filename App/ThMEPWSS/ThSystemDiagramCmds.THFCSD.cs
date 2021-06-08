6c405fdaf64b7a9f33d7743c57acc8347eef90b3
Jerry Dong <shichongdong80@163.com>
2021-06-08 14:14:22 +0800

    Rename THFCFD command

--- App/ThMEPWSS/ThSystemDiagramCmds.THFCSD.cs
+++ App/ThMEPWSS/ThSystemDiagramCmds.THFCSD.cs
@@ -1,16 +1,11 @@
 ﻿using Autodesk.AutoCAD.Runtime;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ThMEPWSS.Command;
 
 namespace ThMEPWSS
 {
     public partial class ThSystemDiagramCmds
     {
-        [CommandMethod("TIANHUACAD", "THCRSD", CommandFlags.Modal)]
+        [CommandMethod("TIANHUACAD", "THFCFD", CommandFlags.Modal)]
         public void ThCreateFireControlSystemDiagram()
         {
             using (var cmd = new ThFireControlSystemDiagramCmd())
