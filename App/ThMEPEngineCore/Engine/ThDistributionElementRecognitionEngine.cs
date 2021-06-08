8246b928f241b424e1091f4856033ce102049587
jackwhoknow <trasure@126.com>
2021-06-08 18:19:41 +0800

    add  Recognize to ThDistributionElementRecognitionEngine

--- App/ThMEPEngineCore/Engine/ThDistributionElementRecognitionEngine.cs
+++ App/ThMEPEngineCore/Engine/ThDistributionElementRecognitionEngine.cs
@@ -20,5 +20,14 @@ namespace ThMEPEngineCore.Engine
 
         public abstract void Recognize(Database database, Point3dCollection polygon);
         public abstract void RecognizeMS(Database database, Point3dCollection polygon);
+        /// <summary>
+        /// 把ExtractionEngine提取的数据转成Ifc model
+        /// </summary>
+        /// <param name="datas"></param>
+        /// <param name="polygon"></param>
+        public virtual void Recognize(List<ThRawIfcDistributionElementData> datas, Point3dCollection polygon)
+        {
+            //
+        }
     }
 }
