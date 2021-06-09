a79abfef8d71aeb913fbb48534f994554b8dedb1
Jerry Dong <shichongdong80@163.com>
2021-06-09 13:43:58 +0800

    Fix build error

--- App/ThMEPElectrical/SecurityPlaneSystem/StructureHandleService/GetPrimitivesService.cs
+++ App/ThMEPElectrical/SecurityPlaneSystem/StructureHandleService/GetPrimitivesService.cs
@@ -149,7 +149,7 @@ namespace ThMEPElectrical.StructureHandleService
                 var ShearWallEngine = new ThShearWallRecognitionEngine();
                 ShearWallEngine.Recognize(ShearWallExtractEngine.Results, polyline.Vertices());
 
-                var archWallExtractEngine = new ThArchitectureWallExtractionEngine();
+                var archWallExtractEngine = new ThDB3ArchWallExtractionEngine();
                 archWallExtractEngine.Extract(acdb.Database);
                 archWallExtractEngine.Results.ForEach(x => originTransformer.Transform(x.Geometry));
                 var archWallEngine = new ThDB3ArchWallRecognitionEngine();
