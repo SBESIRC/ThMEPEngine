d1287b778f8f0d22660cd35e26d46af778093791
Jerry Dong <shichongdong80@163.com>
2021-06-10 18:35:21 +0800

    Implement ThAXISLineRecognitionEngine command (#445)

---
+++ App/ThMEPEngineCore/Engine/ThAXISLineRecognitionEngine.cs
@@ -0,0 +1,58 @@
+﻿using NFox.Cad;
+using DotNetARX;
+using System.Linq;
+using ThCADCore.NTS;
+using Dreambuild.AutoCAD;
+using Autodesk.AutoCAD.Geometry;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using ThMEPEngineCore.Model;
+
+namespace ThMEPEngineCore.Engine
+{
+    public class ThAXISLineExtractionEngine : ThBuildingElementExtractionEngine
+    {
+        public override void Extract(Database database)
+        {
+            var visitor = new ThAXISLineExtractionVisitor();
+            var extractor = new ThBuildingElementExtractor();
+            extractor.Accept(visitor);
+            extractor.Extract(database);
+            Results = visitor.Results;
+        }
+    }
+
+    public class ThAXISLineRecognitionEngine : ThBuildingElementRecognitionEngine
+    {
+        public override void Recognize(Database database, Point3dCollection polygon)
+        {
+            var engine = new ThAXISLineExtractionEngine();
+            engine.Extract(database);
+            Recognize(engine.Results, polygon);
+        }
+
+        public override void Recognize(List<ThRawIfcBuildingElementData> datas, Point3dCollection polygon)
+        {
+            List<Curve> curves = new List<Curve>();
+            var objs = datas.Select(o => o.Geometry).ToCollection();
+            if (polygon.Count > 0)
+            {
+                ThCADCoreNTSSpatialIndex columnCurveSpatialIndex = new ThCADCoreNTSSpatialIndex(objs);
+                var pline = new Polyline()
+                {
+                    Closed = true,
+                };
+                pline.CreatePolyline(polygon);
+                foreach (var filterObj in columnCurveSpatialIndex.SelectCrossingPolygon(pline))
+                {
+                    curves.Add(filterObj as Curve);
+                }
+            }
+            else
+            {
+                curves = objs.Cast<Curve>().ToList();
+            }
+            curves.Cast<Line>().ForEach(o => Elements.Add(ThAXISLine.Create(o)));
+        }
+    }
+}
