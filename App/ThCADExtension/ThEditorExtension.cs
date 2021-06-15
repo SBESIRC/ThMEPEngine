c5abc49bb8400dd4f5c8a69e33c115b82fb3a2b3
jackwhoknow <trasure@126.com>
2021-06-15 15:26:32 +0800

    move ZoomToObjects、PickFirstObjects  to ThCadExtension

--- App/ThCADExtension/ThEditorExtension.cs
+++ App/ThCADExtension/ThEditorExtension.cs
@@ -37,7 +37,7 @@ namespace ThCADExtension
                 ViewTableRecord view = ed.GetCurrentView();
 
                 // zoom到pline
-                Active.Editor.ZoomObject(plineObjId);
+                Active.Editor.ZoomToObject(plineObjId);
 
                 // 计算选择范围
                 // 由于绘图不规范，对于一些“奇异”的多段线，用它作为选择的轮廓线会导致选择失败。
@@ -79,7 +79,7 @@ namespace ThCADExtension
                 ViewTableRecord view = ed.GetCurrentView();
 
                 // zoom到pline
-                Active.Editor.ZoomObject(plineObjId);
+                Active.Editor.ZoomToObject(plineObjId);
 
                 // 选择
                 PromptSelectionResult result;
@@ -151,28 +151,6 @@ namespace ThCADExtension
             }
         }
 
-        public static void ZoomWindow(this Editor ed, Extents3d ext)
-        {
-            ext.TransformBy(ed.CurrentUserCoordinateSystem.Inverse());
-            COMTool.ZoomWindow(ext.MinPoint, ext.MaxPoint);
-        }
-
-        public static void ZoomObject(this Editor ed, ObjectId entId)
-        {
-            Database db = ed.Document.Database;
-            using (Transaction trans = db.TransactionManager.StartTransaction())
-            {
-                //获取实体对象
-                Entity ent = trans.GetObject(entId, OpenMode.ForRead) as Entity;
-                if (ent == null) return;
-                //根据实体的范围对视图进行缩放
-                Extents3d ext = ent.GeometricExtents;
-                ext.TransformBy(ed.CurrentUserCoordinateSystem.Inverse());
-                COMTool.ZoomWindow(ext.MinPoint, ext.MaxPoint);
-                trans.Commit();
-            }
-        }
-
         public static PromptSelectionResult SelectAtPickBox(this Editor ed, Point3d pickBoxCentre)
         {
             //Get pick box's size on screen
@@ -207,6 +185,67 @@ namespace ThCADExtension
             points.Add(pt);
 
             return ed.SelectCrossingPolygon(points);
+        }
+
+        public static void ZoomWindow(this Editor ed, Extents3d ext)
+        {
+            ext.TransformBy(ed.CurrentUserCoordinateSystem.Inverse());
+            COMTool.ZoomWindow(ext.MinPoint, ext.MaxPoint);
+        }
+
+        public static void ZoomToObject(this Editor ed, ObjectId entId)
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Use(entId.Database))
+            {
+                var obj = acadDatabase.Element<Entity>(entId);
+                ed.ZoomToObjects(new Entity[] { obj }, 1.0);
+            }
+        }
+
+        public static void ZoomToObjects(this Editor ed, Entity[] entities, double scale)
+        {
+            try
+            {
+                var extents = new Extents3d();
+                entities.ForEachDbObject(e =>
+                {
+                    Plane XYPlane = new Plane(Point3d.Origin, Vector3d.ZAxis);
+                    Matrix3d matrix = Matrix3d.Projection(XYPlane, XYPlane.Normal);
+                    var projectExtents = new Extents3d(
+                        e.GeometricExtents.MinPoint.TransformBy(matrix),
+                        e.GeometricExtents.MaxPoint.TransformBy(matrix));
+                    extents.AddExtents(projectExtents);
+                });
+                //wcs下的Extend中心点
+                var center = extents.CenterPoint();
+                //Extend的宽和高
+                double projExtendHeight = extents.Height();
+                double projExtendWidth = extents.Width();
+                //以Extentd中心为ocs坐标原点，获取ocs下的Extend的最大最小点坐标
+                Point3d ocsMinPoint = new Point3d(-0.5 * projExtendWidth, -0.5 * projExtendHeight, 0);
+                Point3d ocsMaxPoint = new Point3d(0.5 * projExtendWidth, 0.5 * projExtendHeight, 0);
+                var ocsExtents = new Extents3d(ocsMinPoint, ocsMaxPoint);
+                // 转换到WCS
+                Matrix3d rotationMat = Matrix3d.Identity;
+                Matrix3d scaleMat = Matrix3d.Scaling(scale, Point3d.Origin);
+                Matrix3d movementMat = Matrix3d.Displacement(center.GetAsVector());
+                ocsExtents.TransformBy(scaleMat.PreMultiplyBy(rotationMat).PreMultiplyBy(movementMat));
+                // 在WCS下缩放到范围内
+                COMTool.ZoomWindow(ocsExtents.MinPoint, ocsExtents.MaxPoint);
+            }
+            catch
+            {
+                // 在缺失字体的情况下，BlockReference.GeometricExtents会抛出eInvalidExtents异常
+                // 若异常发生，不执行任何操作
+            }
+        }
+
+        public static void PickFirstObjects(this Editor ed, ObjectId[] objs)
+        {
+            // 首先清空现有的PickFirst选择集
+            Active.Editor.SetImpliedSelection(new ObjectId[0]);
+            // 接着讲模型添加到PickFirst选择集
+            Active.Editor.SetImpliedSelection(objs);
         }
     }
 }
