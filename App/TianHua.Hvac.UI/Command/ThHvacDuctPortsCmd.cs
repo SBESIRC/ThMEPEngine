12ca71fbe202356e1c1f21de2a970f9fadb70851
qiyixuan <2298305439@qq.com>
2021-06-15 09:27:04 +0800

    add 1 comment (#450)

--- App/TianHua.Hvac.UI/Command/ThHvacDuctPortsCmd.cs
+++ App/TianHua.Hvac.UI/Command/ThHvacDuctPortsCmd.cs
@@ -42,6 +42,7 @@ namespace TianHua.Hvac.UI.Command
                     Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("选择错误起始点");
                     return;
                 }
+                // construct graph
                 var adjust_graph = new ThDuctPortsConstructor(graph_res, in_param);
                 var judger = new ThDuctPortsJudger(graph_res.merged_endlines, adjust_graph.endline_segs);
                 var painter = new ThDuctPortsDraw(in_param, judger.align_points);
