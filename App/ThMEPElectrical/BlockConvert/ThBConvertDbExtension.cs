eaec33b4fb68127691278a14d14053231c73126c
Jerry Dong <shichongdong80@163.com>
2021-06-16 14:51:25 +0800

    Implement BlockReference.GetBlockReferenceOBB

--- App/ThMEPElectrical/BlockConvert/ThBConvertDbExtension.cs
+++ App/ThMEPElectrical/BlockConvert/ThBConvertDbExtension.cs
@@ -1,4 +1,6 @@
-﻿using Linq2Acad;
+﻿using NFox.Cad;
+using Linq2Acad;
+using System.Linq;
 using ThCADExtension;
 using GeometryExtensions;
 using Autodesk.AutoCAD.Geometry;
@@ -63,6 +65,25 @@ namespace ThMEPElectrical.BlockConvert
                     }
                 }
                 return objs;
+            }
+        }
+
+        /// <summary>
+        /// 获取转换图块的OBB
+        /// </summary>
+        /// <param name="br"></param>
+        /// <param name="ecs2Wcs"></param>
+        /// <returns></returns>
+        public static Polyline GetBlockReferenceOBB(this BlockReference br, Matrix3d ecs2Wcs)
+        {
+            using (var acadDatabase = AcadDatabase.Use(br.Database))
+            {
+                // 业务需求: 去除在“DEFPOINTS”上的图元
+                var blockTableRecord = acadDatabase.Blocks.Element(br.BlockTableRecord);
+                var entities = blockTableRecord.GetEntities().Where(e => e.Layer != "DEFPOINTS");
+                var rectangle = entities.ToCollection().GeometricExtents().ToRectangle();
+                rectangle.TransformBy(ecs2Wcs);
+                return rectangle;
             }
         }
 
