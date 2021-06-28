664a764e312f614754d4df1ddb3261d2e5f8e27a
田开贺 <tiankaihe@thape.com.cn>
2021-06-28 18:44:54 +0800

    UI adjustment

--- App/ThMEPElectrical/SystemDiagram/Model/WireCircuit/ThFireWaterTankLevelVerticalWireCircuit.cs
+++ App/ThMEPElectrical/SystemDiagram/Model/WireCircuit/ThFireWaterTankLevelVerticalWireCircuit.cs
@@ -20,6 +20,7 @@ namespace ThMEPElectrical.SystemDiagram.Model.WireCircuit
             List<Entity> Result = new List<Entity>();
             int FireWaterTankMaxFloor = 0;// 消防水箱最高楼层
             int FireExtinguisherPoolMinFloor = 0;//消防水池最低楼层
+            int FireExtinguisherPoolMaxFloor = 0;//消防水池最高楼层
             int FireExtinguisherPoolCount =0;    //消防水池个数
             for (int FloorNum = 0; FloorNum < AllFireDistrictData.Count; FloorNum++)
             {
@@ -34,15 +35,27 @@ namespace ThMEPElectrical.SystemDiagram.Model.WireCircuit
                 {
                     FireExtinguisherPoolCount += AreaData.Data.BlockData.BlockStatistics["消防水池"];
                     if (FireExtinguisherPoolMinFloor == 0)
+                    {
                         FireExtinguisherPoolMinFloor = FloorNum + 1;
+                    }
+                    FireExtinguisherPoolMaxFloor = FloorNum + 1;
                     Result.AddRange(DrawFireExtinguisherPoolLine(currentIndex, FloorNum));
                 }
             }
             //都存在，才画
-            if (FireWaterTankMaxFloor > 0 && FireExtinguisherPoolMinFloor > 0)
+            if (FireWaterTankMaxFloor > 0 || FireExtinguisherPoolMaxFloor > 0)
             {
-                Line Endline1 = new Line(new Point3d(OuterFrameLength * (currentIndex - 1) + Offset, 0, 0), new Point3d(OuterFrameLength * (currentIndex - 1) + Offset, OuterFrameLength * (FireWaterTankMaxFloor-1) + 2350, 0));
-                Result.Add(Endline1);
+                if (FireWaterTankMaxFloor >= FireExtinguisherPoolMaxFloor)
+                {
+                    Line Endline1 = new Line(new Point3d(OuterFrameLength * (currentIndex - 1) + Offset, 0, 0), new Point3d(OuterFrameLength * (currentIndex - 1) + Offset, OuterFrameLength * (FireWaterTankMaxFloor - 1) + 2350, 0));
+                    Result.Add(Endline1);
+                }
+                else
+                {
+                    Line Endline1 = new Line(new Point3d(OuterFrameLength * (currentIndex - 1) + Offset, 0, 0), new Point3d(OuterFrameLength * (currentIndex - 1) + Offset, OuterFrameLength * (FireExtinguisherPoolMaxFloor - 1) + 2250, 0));
+                    Result.Add(Endline1);
+                }
+                
                 //设置线型
                 Result.ForEach(o =>
                 {
