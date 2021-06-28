7cb2a7a07f73bd8c85d76f961fbdef29d8abcbd4
第三次存在 <514940817@qq.com>
2021-06-28 15:59:20 +0800

    commit layout Guard Tour System step3 (#497)
    Co-authored-by: 汤永靖 <tangyongjing@thape.com.cn>

--- App/ThMEPElectrical/SecurityPlaneSystem/GuardTourSystem/HandleGuardTourRoomService.cs
+++ App/ThMEPElectrical/SecurityPlaneSystem/GuardTourSystem/HandleGuardTourRoomService.cs
@@ -10,14 +10,14 @@ namespace ThMEPElectrical.SecurityPlaneSystem.GuardTourSystem
     public static class HandleGuardTourRoomService
     {
         static string roomNameColumn = "房间名称";
-        static string spaceColumn = "布置间距";
-        static string stairRoomColumn = "楼梯间";
-        public static List<string> GTRoom = new List<string>()
+        static string insideRoomColumn = "房间内";
+        static string outsideRoomColumn = "房间外";
+        public static List<string> GTRooms = new List<string>()
         {
             "车库",
         };
 
-        public static List<string> StairRoom = new List<string>()
+        public static List<string> otherRooms = new List<string>()
         {
             "楼梯",
         };
@@ -25,26 +25,41 @@ namespace ThMEPElectrical.SecurityPlaneSystem.GuardTourSystem
         public static void HandleRoomInfo(DataTable table)
         {
             string columnName = null;
+            string insideRoom = null;
+            string outsideRoom = null;
             foreach (DataColumn column in table.Columns)
             {
                 if (column.ColumnName.Contains(roomNameColumn))
                 {
                     columnName = column.ColumnName;
                 }
+                else if (column.ColumnName.Contains(insideRoomColumn))
+                {
+                    insideRoom = column.ColumnName;
+                }
+                else if (column.ColumnName.Contains(outsideRoomColumn))
+                {
+                    outsideRoom = column.ColumnName;
+                }
             }
 
             if (columnName != null)
             {
-                GTRoom.Clear();
+                GTRooms.Clear();
+                otherRooms.Clear();
                 foreach (DataRow row in table.Rows)
                 {
                     var roomNames = CommonRoomHandleService.HandleRoom(row[columnName].ToString());
-                    GTRoom.AddRange(roomNames);
+                    if (row[insideRoom].ToString() == "是")
+                    {
+                        GTRooms.AddRange(roomNames);
+                    }
+                    if (row[outsideRoom].ToString() == "是")
+                    {
+                        otherRooms.AddRange(roomNames);
+                    }
                 }
             }
-
-            StairRoom.Clear();
-            StairRoom.AddRange(CommonRoomHandleService.HandleRoom(stairRoomColumn));
         }
     }
 }
