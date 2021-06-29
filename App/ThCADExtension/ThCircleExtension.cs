a14f49d949082403888a71d3696ecff7a79c24a9
王家 <wangjia7@thape.com.cn>
2021-06-29 14:30:52 +0800

    Implement ThCircleExtension.TessellateCircleWithChord

--- App/ThCADExtension/ThCircleExtension.cs
+++ App/ThCADExtension/ThCircleExtension.cs
@@ -30,5 +30,31 @@ namespace ThCADExtension
             poly.TransformBy(Matrix3d.Displacement(circle.Center.GetAsVector()));
             return poly;
         }
+
+        /// <summary>
+        /// 用弦长分割圆
+        /// </summary>
+        /// <param name="circle"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static Polyline TessellateCircleWithChord(this Circle circle, double length)
+        {
+            return TessellateCircleWithArc(circle, circle.ChordLengthToArcLength(length));
+        }
+
+        /// <summary>
+        /// 根据弦长求解对应较短弧的弧长
+        /// </summary>
+        /// <param name="circle"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static double ChordLengthToArcLength(this Circle circle, double length)
+        {
+            if(length > circle.Diameter)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return 2 * circle.Radius * Math.Asin(length / circle.Diameter);
+        }
     }
 }
