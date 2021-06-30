6b45959b71df2cf4d258cf6f07033abb9e30daa5
王家 <wangjia7@thape.com.cn>
2021-06-30 18:00:30 +0800

    add preprocess when build area for architecture wall

--- App/ThMEPEngineCore/Service/ThArchitectureWallSimplifier.cs
+++ App/ThMEPEngineCore/Service/ThArchitectureWallSimplifier.cs
@@ -1,4 +1,5 @@
-﻿using NFox.Cad;
+﻿using System;
+using NFox.Cad;
 using System.Linq;
 using ThCADCore.NTS;
 using Dreambuild.AutoCAD;
@@ -9,6 +10,7 @@ namespace ThMEPEngineCore.Service
 {
     public class ThArchitectureWallSimplifier
     {
+        private const double AREA_TOLERANCE = 1.0;
         private const double OFFSET_DISTANCE = 20.0;
         private const double DISTANCE_TOLERANCE = 1.0;
 
@@ -40,8 +42,8 @@ namespace ThMEPEngineCore.Service
 
         public static DBObjectCollection Normalize(DBObjectCollection walls)
         {
-            var objs = new DBObjectCollection();
-            foreach(AcPolygon wall in walls)
+            var objs = new DBObjectCollection();            
+            foreach (AcPolygon wall in walls)
             {
                 wall.Buffer(-OFFSET_DISTANCE)
                     .Cast<AcPolygon>()
@@ -50,14 +52,56 @@ namespace ThMEPEngineCore.Service
                         o.Buffer(OFFSET_DISTANCE)
                         .Cast<AcPolygon>()
                         .ForEach(e => objs.Add(e));
-                    });                
+                    });
             }
             return objs;
         }
 
         public static DBObjectCollection BuildArea(DBObjectCollection walls)
         {
-            return walls.BuildArea();
+            walls = Buffer(walls, -DISTANCE_TOLERANCE);
+            walls = walls.BuildArea();
+            walls = Buffer(walls, DISTANCE_TOLERANCE);
+            walls = FilterSmallAreaPolygon(walls);
+            return walls;
+        }
+        private static DBObjectCollection Buffer(DBObjectCollection objs, double length)
+        {
+            var results = new DBObjectCollection();
+            foreach (Entity obj in objs)
+            {
+                if (obj is AcPolygon polyline)
+                {
+                    polyline.Buffer(length).Cast<Entity>().ForEach(e => results.Add(e));
+                }
+                else if (obj is MPolygon mPolygon)
+                {
+                    mPolygon.Buffer(length).Cast<Entity>().ForEach(e => results.Add(e));
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+            }
+            return results;
+        }
+        private static DBObjectCollection FilterSmallAreaPolygon(DBObjectCollection polygons)
+        {
+            return polygons.Cast<Entity>().Where(o =>
+            {
+                if (o is AcPolygon polyline)
+                {
+                    return polyline.Area > AREA_TOLERANCE;
+                }
+                else if (o is MPolygon mPolygon)
+                {
+                    return mPolygon.Area > AREA_TOLERANCE;
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+            }).ToCollection();
         }
     }
 }
