47dd9b1cbac748261094ec6c3f21a006ee305563
刘光胜 <liuguangsheng@thape.com.cn>
2021-07-01 13:24:57 +0800

    add create hatch for MPolygon

--- App/ThMEPWSS/Hydrant/Service/ThHydrantPrintService.cs
+++ App/ThMEPWSS/Hydrant/Service/ThHydrantPrintService.cs
@@ -42,26 +42,41 @@ namespace ThMEPWSS.Hydrant.Service
             using (var acadDatabase = AcadDatabase.Use(Db))
             {
                 var layerId = acadDatabase.Database.CreateAILayer(LayerName, 30);
-                var ObjIds = new ObjectIdCollection();
-                var clone = entity.Clone() as Entity;
-                ObjIds.Add(acadDatabase.ModelSpace.Add(clone));
-                clone.ColorIndex = colorIndex;
-                clone.Layer = LayerName;
+                if(entity is Polyline)
+                {
+                    var ObjIds = new ObjectIdCollection();
+                    var clone = entity.Clone() as Entity;
+                    ObjIds.Add(acadDatabase.ModelSpace.Add(clone));
+                    clone.ColorIndex = colorIndex;
+                    clone.Layer = LayerName;
 
-                Hatch oHatch = new Hatch();
-                var normal = new Vector3d(0.0, 0.0, 1.0);
-                oHatch.Normal = normal;
-                oHatch.Elevation = 0.0;
-                oHatch.PatternScale = 2.0;
-                oHatch.SetHatchPattern(HatchPatternType.PreDefined, "SOLID");
-                oHatch.ColorIndex = colorIndex;
-                oHatch.Layer = LayerName;
-                oHatch.Transparency = new Autodesk.AutoCAD.Colors.Transparency(77); //30%
-                acadDatabase.ModelSpace.Add(oHatch);
-                //this works ok  
-                oHatch.Associative = true;
-                oHatch.AppendLoop((int)HatchLoopTypes.Default, ObjIds);
-                oHatch.EvaluateHatch(true);
+                    Hatch oHatch = new Hatch();
+                    var normal = new Vector3d(0.0, 0.0, 1.0);
+                    oHatch.Normal = normal;
+                    oHatch.Elevation = 0.0;
+                    oHatch.PatternScale = 2.0;
+                    oHatch.SetHatchPattern(HatchPatternType.PreDefined, "SOLID");
+                    oHatch.ColorIndex = colorIndex;
+                    oHatch.Layer = LayerName;
+                    oHatch.Transparency = new Autodesk.AutoCAD.Colors.Transparency(77); //30%
+                    acadDatabase.ModelSpace.Add(oHatch);
+                    //this works ok  
+                    oHatch.Associative = true;
+                    oHatch.AppendLoop((int)HatchLoopTypes.Default, ObjIds);
+                    oHatch.EvaluateHatch(true);
+                }
+                else if(entity is MPolygon mPolygon)
+                {
+                    mPolygon.Normal = new Vector3d(0.0, 0.0, 1.0);
+                    mPolygon.Elevation = 0.0;
+                    mPolygon.PatternScale = 2.0;
+                    mPolygon.SetPattern(HatchPatternType.PreDefined, "SOLID");
+                    mPolygon.Layer = LayerName;
+                    mPolygon.ColorIndex = colorIndex;
+                    mPolygon.Transparency = new Autodesk.AutoCAD.Colors.Transparency(77); //30%
+                    acadDatabase.ModelSpace.Add(mPolygon);
+                    mPolygon.EvaluateHatch(true);
+                }                
             }
         }
     }
