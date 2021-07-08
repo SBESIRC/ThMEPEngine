4f32dcbb49fee9c01ef947d3e7cdea552c5a3492
刘潘 <liupan1@thape.com.cn>
2021-07-08 17:18:59 +0800

    ThBConvertEngineStrongCurrent.Adjust, adjust for equipment base point

--- App/ThMEPElectrical/BlockConvert/ThBConvertEngineWeakCurrent.cs
+++ App/ThMEPElectrical/BlockConvert/ThBConvertEngineWeakCurrent.cs
@@ -49,7 +49,7 @@ namespace ThMEPElectrical.BlockConvert
             using (AcadDatabase acadDatabase = AcadDatabase.Active())
             {
                 var blockReference = acadDatabase.Element<BlockReference>(blkRef, true);
-                blockReference.TransformBy(srcBlockReference.BlockTransform.PreMultiplyBy(srcBlockReference.MCS2WCS));
+                blockReference.TransformBy(srcBlockReference.MCS2WCS);
             }
         }
 
