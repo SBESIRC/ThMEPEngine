42c79d3e1e349ceeee5fb18458e418401be2be80
鲍金昌 <baojinchang@thape.com.cn>
2021-07-20 11:07:50 +0800

    define ThFireAlarm command

---
+++ App/ThMEPElectrical/Command/ThFireAlarmCommand.cs
@@ -0,0 +1,49 @@
+﻿using AcHelper;
+using AcHelper.Commands;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Linq2Acad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThCADCore.NTS;
+using ThCADExtension;
+using ThMEPElectrical.SecurityPlaneSystem;
+using ThMEPElectrical.SecurityPlaneSystem.AccessControlSystem.LayoutService;
+using ThMEPElectrical.SecurityPlaneSystem.AccessControlSystem.Model;
+using ThMEPElectrical.SecurityPlaneSystem.StructureHandleService;
+using ThMEPElectrical.StructureHandleService;
+using ThMEPEngineCore.Algorithm;
+using ThMEPEngineCore.Model;
+
+namespace ThMEPElectrical.Command
+{
+    public class ThFireAlarmCommand : IAcadCommand, IDisposable
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Execute()
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            {
+                var per = Active.Editor.GetEntity("\n选择一个框线");
+                var pts = new Point3dCollection();
+                if (per.Status == PromptStatus.OK)
+                {
+                    var frame = acadDatabase.Element<Polyline>(per.ObjectId);
+                    var newFrame = ThMEPFrameService.NormalizeEx(frame);
+                    pts = newFrame.VerticesEx(100.0);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
