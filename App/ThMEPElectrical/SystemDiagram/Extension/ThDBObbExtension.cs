f205bc1b5b90a90427cf7dee8ea360edc558dedf
田开贺 <tiankaihe@thape.com.cn>
2021-07-22 11:39:32 +0800

    Add filter conditions

--- App/ThMEPElectrical/SystemDiagram/Extension/ThDBObbExtension.cs
+++ App/ThMEPElectrical/SystemDiagram/Extension/ThDBObbExtension.cs
@@ -36,7 +36,8 @@ namespace ThMEPElectrical.SystemDiagram.Extension
                 var blockTableRecord = acadDatabase.Blocks.Element(br.BlockTableRecord);
                 var entities = blockTableRecord.GetEntities().Where(e =>
                 {
-                    if (e is AttributeDefinition) return false;
+                    if (e is AttributeDefinition) return false;//业务：过滤属性定义
+                    if (e is BlockReference) return false;//业务：过滤块中块
                     var layerTableRecord = acadDatabase.Layers.Element(e.Layer);
                     return !layerTableRecord.IsFrozen & !layerTableRecord.IsOff;
                 });
