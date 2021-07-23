b8d31137e88070b24210dbb3453a4d283ab9b495
刘光胜 <liuguangsheng@thape.com.cn>
2021-07-23 19:43:27 +0800

    update ThDrainageDitchNearbyService find closed ditch logic

--- App/ThMEPWSS/FlushPoint/Service/ThDrainageDitchNearbyService.cs
+++ App/ThMEPWSS/FlushPoint/Service/ThDrainageDitchNearbyService.cs
@@ -1,6 +1,8 @@
 ﻿using NFox.Cad;
+using System.Linq;
 using ThCADCore.NTS;
 using ThCADExtension;
+using ThMEPEngineCore.CAD;
 using Autodesk.AutoCAD.Geometry;
 using System.Collections.Generic;
 using Autodesk.AutoCAD.DatabaseServices;
@@ -10,18 +12,47 @@ namespace ThMEPWSS.FlushPoint.Service
     public class ThDrainageDitchNearbyService
     {
         public double NearbyDistance { get; set; } //排水沟的中心线外扩4000范围内视为“附近”
-        private ThCADCoreNTSSpatialIndex SpatialIndex { get; set; }
-        public ThDrainageDitchNearbyService(List<Entity> ditches)
+        public List<Entity> Rooms { get; set; }
+        private ThCADCoreNTSSpatialIndex DitchSpatialIndex { get; set; }
+        private ThCADCoreNTSSpatialIndex RoomSpatialIndex { get; set; }
+        public ThDrainageDitchNearbyService(List<Entity> ditches,List<Entity> rooms)
         {
+            Rooms = rooms;
             NearbyDistance = 4000;
-            SpatialIndex = new ThCADCoreNTSSpatialIndex(ditches.ToCollection());
+            DitchSpatialIndex = new ThCADCoreNTSSpatialIndex(ditches.ToCollection());
+            RoomSpatialIndex = new ThCADCoreNTSSpatialIndex(rooms.ToCollection());
         }
         public bool Find(Point3d pt)
         {
+            var rooms = FindRooms(pt);
+            var collector = new DBObjectCollection();
+            rooms.ForEach(r =>
+            {
+                foreach(Entity ent in DitchSpatialIndex.SelectCrossingPolygon(r))
+                {
+                    collector.Add(ent);
+                }
+            });
+            var spatialIndex = new ThCADCoreNTSSpatialIndex(collector);
             var circle = new Circle(pt, Vector3d.ZAxis, NearbyDistance);
             var poly = circle.Tessellate(5.0);
-            var objs = SpatialIndex.SelectCrossingPolygon(poly);
+            var objs = spatialIndex.SelectCrossingPolygon(poly);
             return objs.Count > 0;
+        }
+        private List<Entity> FindRooms(Point3d pt)
+        {
+            var envelop = ThDrawTool.CreateSquare(pt, 5.0);
+            var objs = RoomSpatialIndex.SelectCrossingPolygon(envelop);
+            var containers = new List<Entity>();
+            if (objs.Count == 0)
+            {
+                containers = Rooms.Where(r => r.IsContains(pt)).ToList();
+            }
+            else
+            {
+                containers = objs.Cast<Entity>().ToList();
+            }
+            return containers.OrderBy(o => o.ToNTSPolygon().Distance(pt.ToNTSPoint())).ToList();
         }
     }
 }
--- App/ThMEPWSS/FlushPoint/Service/ThFilterWashPointsService.cs
+++ App/ThMEPWSS/FlushPoint/Service/ThFilterWashPointsService.cs
@@ -10,14 +10,16 @@ namespace ThMEPWSS.FlushPoint.Service
     {
         public ThDrainFacilityExtractor DrainFacilityExtractor { get; set; }
         public WashPtLayoutInfo LayoutInfo { get; set; }
+        public List<Entity> Rooms { get; set; }
         public ThFilterWashPointsService()
         {
+            Rooms = new List<Entity>();
             LayoutInfo = new WashPtLayoutInfo();
             DrainFacilityExtractor = new ThDrainFacilityExtractor();
         }
         public void Filter(List<Point3d> washPoints)
         {
-            var drainageDitchService = new ThDrainageDitchNearbyService(DrainFacilityExtractor.DrainageDitches);
+            var drainageDitchService = new ThDrainageDitchNearbyService(DrainFacilityExtractor.DrainageDitches, Rooms);
             var collectWellService = new ThCollectingWellNearbyService(
                 DrainFacilityExtractor.CollectingWells.Cast<Polyline>().ToList());
             LayoutInfo.NearbyPoints = washPoints.Where(o => drainageDitchService.Find(o) || collectWellService.Find(o)).ToList();
