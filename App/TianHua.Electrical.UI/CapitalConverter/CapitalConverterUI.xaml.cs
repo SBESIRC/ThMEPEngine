0184d49a964c4d306728d2d5ef54ea6ecebb5d42
刘光胜 <liuguangsheng@thape.com.cn>
2021-07-28 14:56:58 +0800

    finished THTZZH command UI

---
+++ App/TianHua.Electrical.UI/CapitalConverter/CapitalConverterUI.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ThMEPElectrical.Model;
+using ThControlLibraryWPF.CustomControl;
+
+namespace TianHua.Electrical.UI.CapitalConverter
+{
+    public partial class CapitalConverterUI : ThCustomWindow
+    {
+        public ThCapitalConverterModel Parameter { get; set; }
+        public bool GoOn { get; set; }
+        
+        public CapitalConverterUI()
+        {
+            Parameter = new ThCapitalConverterModel();
+            GoOn = false;            
+            InitializeComponent();
+            this.DataContext = Parameter;
+        }
+
+        private void btnCapitalConvert_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GoOn = true;
+            this.Close();
+        }
+
+        private void btnUpdateCompare_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        }
+    }
+}
