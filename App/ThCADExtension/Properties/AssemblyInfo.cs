5d19fb90adff4b609972fe481af48d8cec5b7b9a
董士崇 <dongshichong@thape.com.cn>
2021-07-30 18:39:55 +0800

    Revert "Bump version to 1.4.5.0"
    This reverts commit a564bfd4975403c5f9f70901b86d4b293bff27c1.

--- App/ThCADExtension/Properties/AssemblyInfo.cs
+++ App/ThCADExtension/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("2abcdb6e-f0c9-44c1-960f-31.4.5.0ef9738")]
+[assembly: Guid("2abcdb6e-f0c9-44c1-960f-31d494ef9738")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.5.0.1")]
-[assembly: AssemblyFileVersion("1.4.5.0.1")]
+[assembly: AssemblyVersion("1.4.4.1")]
+[assembly: AssemblyFileVersion("1.4.4.1")]
