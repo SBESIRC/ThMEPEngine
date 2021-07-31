4af5277963d55367f3ecf377b3a879515652197e
董士崇 <dongshichong@thape.com.cn>
2021-07-31 21:58:56 +0800

    Implement DataSet

---
+++ App/ThMEPEngineCore/DataSet/ThMEPDataSet.cs
@@ -0,0 +1,9 @@
+﻿using NetTopologySuite.Features;
+
+namespace ThMEPEngineCore.DataSet
+{
+    public class ThMEPDataSet
+    {
+        public FeatureCollection Container { get; set; }
+    }
+}
---
+++ App/ThMEPEngineCore/DataSet/ThMEPDataSetFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace ThMEPEngineCore.DataSet
+{
+    public abstract class ThMEPDataSetFactory
+    {
+        /// <summary>
+        /// 创建数据集
+        /// </summary>
+        /// <returns></returns>
+        public ThMEPDataSet Create()
+        {
+            // 获取原材料
+            GetSpatialElements();
+            GetBuildingElements();
+
+            // 加工原材料
+            return BuildDataSet();
+        }
+
+        /// <summary>
+        /// 获取空间元素
+        /// </summary>
+        protected abstract void GetSpatialElements();
+
+        /// <summary>
+        /// 获取建筑元素
+        /// </summary>
+        protected abstract void GetBuildingElements();
+
+        /// <summary>
+        /// 创建数据集
+        /// </summary>
+        protected abstract ThMEPDataSet BuildDataSet();
+    }
+}
