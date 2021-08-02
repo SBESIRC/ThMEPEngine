dbd1f91342b31735f475e44f5ec698269402b497
齐少华 <qishaohua@thape.com.cn>
2021-08-02 10:24:31 +0800

    fix a bug

--- App/ThMEPWSS/ViewModel/ThFireHydrantSystemViewModel.cs
+++ App/ThMEPWSS/ViewModel/ThFireHydrantSystemViewModel.cs
@@ -13,64 +13,52 @@ namespace ThMEPWSS.ViewModel
     {
         public static void InsertLoopMark()
         {
-            try
+            Common.Utils.FocusMainWindow();
+            using (Active.Document.LockDocument())
             {
-                Common.Utils.FocusMainWindow();
-                using (Active.Document.LockDocument())
+                //WaterSuplyUtils.ImportNecessaryBlocks();
+                ThMEPWSS.Pipe.Service.ThRainSystemService.ImportElementsFromStdDwg();
+                while (true)
                 {
-                    //WaterSuplyUtils.ImportNecessaryBlocks();
-                    ThMEPWSS.Pipe.Service.ThRainSystemService.ImportElementsFromStdDwg();
-                    while (true)
+                    var opt = new PromptPointOptions("请指定环管标记插入点: \n");
+                    var pt = Active.Editor.GetPoint(opt);
+                    if (pt.Status != PromptStatus.OK)
                     {
-                        var opt = new PromptPointOptions("请指定环管标记插入点: \n");
-                        var pt = Active.Editor.GetPoint(opt);
-                        if (pt.Status != PromptStatus.OK)
-                        {
-                            break;
-                        }
-                        using (var acadDatabase = AcadDatabase.Active())  //要插入图纸的空间
-                        {
-                            acadDatabase.ModelSpace.ObjectId.InsertBlockReference("W-FRPT-NOTE", WaterSuplyBlockNames.LoopMark,
-                                        pt.Value, new Scale3d(1, 1, 1), 0);
-                        }
+                        break;
                     }
+                    using (var acadDatabase = AcadDatabase.Active())  //要插入图纸的空间
+                    {
+                        acadDatabase.ModelSpace.ObjectId.InsertBlockReference("W-FRPT-NOTE", WaterSuplyBlockNames.LoopMark,
+                                    pt.Value, new Scale3d(1, 1, 1), 0);
+                    }   
                 }
-            }
-            catch(Exception ex)
-            {
             }
         }
 
         public static void InsertSubLoopMark()
         {
-            try
+            Common.Utils.FocusMainWindow();
+            using (Active.Document.LockDocument())
             {
-                Common.Utils.FocusMainWindow();
-                using (Active.Document.LockDocument())
+                //WaterSuplyUtils.ImportNecessaryBlocks();
+                ThMEPWSS.Pipe.Service.ThRainSystemService.ImportElementsFromStdDwg();
+                while (true)
                 {
-                    //WaterSuplyUtils.ImportNecessaryBlocks();
-                    ThMEPWSS.Pipe.Service.ThRainSystemService.ImportElementsFromStdDwg();
-                    while (true)
+                    var opt = new PromptPointOptions("请指定环管节点标记插入点: \n");
+                    var pt = Active.Editor.GetPoint(opt);
+                    if (pt.Status != PromptStatus.OK)
                     {
-                        var opt = new PromptPointOptions("请指定环管节点标记插入点: \n");
-                        var pt = Active.Editor.GetPoint(opt);
-                        if (pt.Status != PromptStatus.OK)
-                        {
-                            break;
-                        }
-                        using (var acadDatabase = AcadDatabase.Active())  //要插入图纸的空间
-                        {
-                            var valueDic = new Dictionary<string, string>();
-                            valueDic.Add("节点1", "A");
-                            valueDic.Add("节点2", "A'");
-                            var objID = acadDatabase.ModelSpace.ObjectId.InsertBlockReference("W-FRPT-NOTE", WaterSuplyBlockNames.LoopNodeMark,
-                                        pt.Value, new Scale3d(1, 1, 1), 0, valueDic);
-                        }
+                        break;
+                    }
+                    using (var acadDatabase = AcadDatabase.Active())  //要插入图纸的空间
+                    {
+                        var valueDic = new Dictionary<string, string>();
+                        valueDic.Add("节点1", "A");
+                        valueDic.Add("节点2", "A'");
+                        var objID = acadDatabase.ModelSpace.ObjectId.InsertBlockReference("W-FRPT-NOTE", WaterSuplyBlockNames.SubLoopMark,
+                                    pt.Value, new Scale3d(1, 1, 1), 0, valueDic);
                     }
                 }
-            }
-            catch(Exception ex)
-            {
             }
         }
     }
