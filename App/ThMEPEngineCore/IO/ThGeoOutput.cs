f7adbb00cfbe80cb265c66cb41eca9ac44d22e0b
董士崇 <dongshichong@thape.com.cn>
2021-08-12 15:44:15 +0800

    Revert "Delete unused code"
    This reverts commit 550472335a173e3fc33df484713d26233158f40c.

--- App/ThMEPEngineCore/IO/ThGeoOutput.cs
+++ App/ThMEPEngineCore/IO/ThGeoOutput.cs
@@ -1,11 +1,26 @@
 ﻿using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 using ThMEPEngineCore.Model;
 using ThMEPEngineCore.IO.GeoJSON;
 using System.Collections.Generic;
 
 namespace ThMEPEngineCore.IO
 {
+    public class ThGeoOutput
+    {
+        public static void Output(List<ThGeometry> geos,string path,string fileName)
+        {
+            var geoJsonWriter = new ThGeometryJsonWriter();
+            geoJsonWriter.Write(geos, Path.Combine(path, string.Format("{0}.Info.geojson", fileName)));
+        }
+
+        public static string Output(List<ThGeometry> geos)
+        {
+            var geoJsonWriter = new ThGeometryJsonWriter();
+            return geoJsonWriter.Write(geos);
+        }
+    }
     public sealed class ExtentedStringWriter : StringWriter
     {
         private readonly Encoding stringWriterEncoding;
@@ -21,14 +36,6 @@ namespace ThMEPEngineCore.IO
             {
                 return this.stringWriterEncoding;
             }
-        }
-    }
-    public class ThGeoOutput
-    {
-        public static void Output(List<ThGeometry> geos, string path, string fileName)
-        {
-            var geoJsonWriter = new ThGeometryJsonWriter();
-            geoJsonWriter.Write(geos, Path.Combine(path, string.Format("{0}.Info.geojson", fileName)));
         }
     }
 }
