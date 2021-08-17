0af90aac715e768f6599dd24ba3a5684ae0902ba
刘光胜 <liuguangsheng@thape.com.cn>
2021-08-17 09:49:32 +0800

    implement DB3 Room layer manager

---
+++ App/ThMEPEngineCore/Service/ThDB3RoomLayerManager.cs
@@ -0,0 +1,32 @@
+﻿using Linq2Acad;
+using System.Linq;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPEngineCore.Service
+{
+    public class ThDB3RoomLayerManager : ThDbLayerManager
+    {
+        public static List<string> CurveXrefLayers(Database database)
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Use(database))
+            {
+                return acadDatabase.Layers
+                    .Where(o => IsVisibleLayer(o))
+                    .Where(o => IsRoomLayer(o.Name))
+                    .Select(o => o.Name)
+                    .ToList();
+            }
+        }
+
+        private static bool IsRoomLayer(string name)
+        {
+            string[] patterns = ThStructureUtils.OriginalFromXref(name).ToUpper().Split('_').Reverse().ToArray();
+            if (patterns.Count() < 2)
+            {
+                return false;
+            }
+            return (patterns[0] == "ROOM") && (patterns[1] == "DEFPOINTS");
+        }
+    }
+}
