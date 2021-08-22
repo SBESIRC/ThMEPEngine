6e5da48eb618df27eb7f2eb6349fe5be40793347
zyk-work <81728112+zyk-work@users.noreply.github.com>
2021-08-22 14:47:31 +0800

    1、修改支持超远距离问题 (#644)
    2、将测试线支持超远距离
    
    Co-authored-by: 赵玉康 <zhaoyukang@thape.com.cn>

--- App/ThMEPElectrical/Business/Procedure/UcsInfoCalculator.cs
+++ App/ThMEPElectrical/Business/Procedure/UcsInfoCalculator.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Linq2Acad;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ThMEPElectrical.Model;
-using Linq2Acad;
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.Geometry;
-using ThMEPElectrical.Assistant;
+using ThMEPEngineCore.Algorithm;
 
 namespace ThMEPElectrical.Business.Procedure
 {
@@ -15,15 +12,17 @@ namespace ThMEPElectrical.Business.Procedure
     {
         public List<UcsInfo> UcsInfos = new List<UcsInfo>();
         private string m_layerName;
+        ThMEPOriginTransformer m_originTransformer;
 
-        public UcsInfoCalculator(string blockLayerName)
+        public UcsInfoCalculator(string blockLayerName, ThMEPOriginTransformer originTransformer)
         {
+            m_originTransformer = originTransformer;
             m_layerName = blockLayerName;
         }
 
-        public static List<UcsInfo> MakeUcsInfos(string blockLayerName)
+        public static List<UcsInfo> MakeUcsInfos(string blockLayerName, ThMEPOriginTransformer originTransformer)
         {
-            var ucsInfoCalculator = new UcsInfoCalculator(blockLayerName);
+            var ucsInfoCalculator = new UcsInfoCalculator(blockLayerName, originTransformer);
             ucsInfoCalculator.Do();
             return ucsInfoCalculator.UcsInfos;
         }
@@ -36,12 +35,15 @@ namespace ThMEPElectrical.Business.Procedure
                     .Where(p => p.Layer.ToUpper().Contains(m_layerName.ToUpper()))
                     .ToList();
 
-                 foreach (var block in blockRefs)
+                foreach (var block in blockRefs)
                 {
-                    var blockSystem = block.BlockTransform.CoordinateSystem3d;
+                    var copyBlock = (BlockReference)block.Clone();
+                    if (null != m_originTransformer)
+                        m_originTransformer.Transform(copyBlock);
+                    var blockSystem = copyBlock.BlockTransform.CoordinateSystem3d;
                     var transBlock = Matrix3d.AlignCoordinateSystem(Point3d.Origin, blockSystem.Xaxis, blockSystem.Yaxis, 
                         blockSystem.Zaxis, Point3d.Origin, Vector3d.XAxis, Vector3d.YAxis, Vector3d.ZAxis);
-                    UcsInfos.Add(new UcsInfo(block.Position, transBlock, block.Rotation, blockSystem.Xaxis, block.BlockTransform));
+                    UcsInfos.Add(new UcsInfo(copyBlock.Position, transBlock, copyBlock.Rotation, blockSystem.Xaxis, copyBlock.BlockTransform));
                     //DrawUtils.DrawProfile(GeometryTrans.MatrixSystemCurves(block.BlockTransform, 100), "drawMatrix");
                 }
             }
