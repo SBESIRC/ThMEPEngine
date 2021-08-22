9cab389a9a745b5e7a6e60994fd4a563af1fb7dd
董士崇 <dongshichong@thape.com.cn>
2021-08-22 16:08:47 +0800

    Implement ThIfcBeamExtension.SectionHeight and BottomElevation

--- App/ThMEPEngineCore/Extension/ThIfcBeamExtension.cs
+++ App/ThMEPEngineCore/Extension/ThIfcBeamExtension.cs
@@ -22,5 +22,32 @@ namespace ThMEPEngineCore.Extension
                 throw new NotSupportedException();
             }
         }
+
+        /// <summary>
+        /// 梁截面高度
+        /// </summary>
+        /// <param name="beam"></param>
+        /// <returns></returns>
+        public static double SectionHeight(this ThIfcBeam beam)
+        {
+            return beam.Height;
+        }
+
+        /// <summary>
+        /// 梁底标高
+        /// </summary>
+        /// <param name="beam"></param>
+        /// <returns></returns>
+        public static double BottomElevation(this ThIfcBeam beam)
+        {
+            if (beam.DistanceToFloor < 0)
+            {
+                return Math.Abs(beam.DistanceToFloor);
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
