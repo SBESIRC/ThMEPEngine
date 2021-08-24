53411bbbfc1b3a9878bdf39c4ec4282067a08769
董士崇 <dongshichong@thape.com.cn>
2021-08-24 12:45:14 +0800

    BlockReference.ToOBB, support non-uniform scale
    (cherry picked from commit 66ce45c7fd81fc728f31fd0d57b3f7ceaac1f956)

---
+++ App/ThCADExtension/ThRectangleExtension.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThCADExtension
+{
+    public static class ThRectangleExtension
+    {
+        /// <summary>
+        /// 矩形变换（支持non-uniform scale）
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public static Polyline GetTransformedRectangle(this Polyline rectangle, Matrix3d matrix)
+        {
+            var solid = rectangle.ToSolid();
+            solid.TransformBy(matrix);
+            return solid.ToPolyline();
+        }
+    }
+}
