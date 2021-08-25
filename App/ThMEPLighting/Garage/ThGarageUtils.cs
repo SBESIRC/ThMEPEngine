9023b12245c8ba3aaf411b75fbd17fadf0e067a0
刘潘 <liupan1@thape.com.cn>
2021-08-25 10:58:25 +0800

    Fix function IsLessThan45Degree to make two vectors endpoint to startpoint

--- App/ThMEPLighting/Garage/ThGarageUtils.cs
+++ App/ThMEPLighting/Garage/ThGarageUtils.cs
@@ -1,13 +1,49 @@
 ﻿using System;
 using Autodesk.AutoCAD.Geometry;
+using System.Collections.Generic;
 
 namespace ThMEPLighting.Garage
 {
     public static class ThGarageUtils
     {
-        public static bool IsLessThan45Degree(Vector3d first,Vector3d second)
+        public static bool IsLessThan45Degree(Point3d firstStart, Point3d firstEnd, Point3d secondStart, Point3d secondEnd)
         {
-            if (first.GetAngleTo(second) < Math.PI / 4 - 1e-5) 
+            var distance = new List<double>
+            {
+                firstStart.DistanceTo(secondStart),
+                firstStart.DistanceTo(secondEnd),
+                firstEnd.DistanceTo(secondStart),
+                firstEnd.DistanceTo(secondEnd)
+            };
+
+            var mindistance = double.MaxValue;
+            var index = 0;
+            for (int i = 0; i < distance.Count; i++)
+            {
+                if (distance[i] < mindistance)
+                {
+                    mindistance = distance[i];
+                    index = i;
+                }
+            }
+
+            var a = index / 2;
+            var b = index % 2;
+            var firstList = new List<Point3d>
+            {
+                firstStart,
+                firstEnd
+            };
+            var secondList = new List<Point3d>
+            {
+                secondStart,
+                secondEnd
+            };
+
+            var first = firstList[1 - a].GetVectorTo(firstList[a]);
+            var second = secondList[b].GetVectorTo(secondList[1 - b]);
+
+            if (first.GetAngleTo(second) < Math.PI / 4 - 1e-5)
             {
                 return true;
             }
