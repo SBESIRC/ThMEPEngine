f832ad91584fa16ccbcbfbd56556711d6a505bc4
董士崇 <dongshichong@thape.com.cn>
2021-08-31 17:53:00 +0800

    Use NTS 2D Coordinate

--- App/ThCADCore/NTS/ThCADCoreNTSGeExtension.cs
+++ App/ThCADCore/NTS/ThCADCoreNTSGeExtension.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using NetTopologySuite.Geometries;
-using NetTopologySuite.Algorithm;
 using Autodesk.AutoCAD.Geometry;
 
 namespace ThCADCore.NTS
@@ -9,21 +8,10 @@ namespace ThCADCore.NTS
     {
         public static Point3d ToAcGePoint3d(this Coordinate coordinate)
         {
-            if (!double.IsNaN(coordinate.Z))
-            {
-                return new Point3d(
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.X),
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.Y),
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.Z)
-                    );
-            }
-            else
-            {
-                return new Point3d(
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.X),
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.Y),
-                    0);
-            }
+            return new Point3d(
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.X),
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.Y),
+                0);
         }
 
         public static Point3d ToAcGePoint3d(this Point point)
@@ -34,9 +22,8 @@ namespace ThCADCore.NTS
         public static Point2d ToAcGePoint2d(this Coordinate coordinate)
         {
             return new Point2d(
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.X),
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.Y)
-                );
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.X),
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(coordinate.Y));
         }
 
         public static Point2d ToAcGePoint2d(this Point point)
@@ -45,7 +32,7 @@ namespace ThCADCore.NTS
         }
         public static Point ToNTSPoint(this Point3d point)
         {
-            return new Point(point.X, point.Y, point.Z);
+            return new Point(point.X, point.Y);
         }
         public static Point ToNTSPoint(this Point2d point)
         {
@@ -54,18 +41,15 @@ namespace ThCADCore.NTS
         public static Coordinate ToNTSCoordinate(this Point3d point)
         {
             return new Coordinate(
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.X),
-                    ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.Y)
-                    );
-
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.X),
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.Y));
         }
 
         public static Coordinate ToNTSCoordinate(this Point2d point)
         {
             return new Coordinate(
                 ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.X),
-                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.Y)
-                );
+                ThCADCoreNTSService.Instance.PrecisionModel.MakePrecise(point.Y));
         }
 
         public static Coordinate[] ToNTSCoordinates(this Point3dCollection points)
@@ -86,24 +70,6 @@ namespace ThCADCore.NTS
                 points.Add(coordinate.ToAcGePoint3d());
             }
             return points;
-        }
-
-        public static bool IsReflex(Point3d p1, Point3d p2, Point3d p3)
-        {
-            return (p3.Y - p1.Y) * (p2.X - p1.X) - (p3.X - p1.X) * (p2.Y - p1.Y) < 0;
-        }
-
-        public static bool IsConvex(Point3d p1, Point3d p2, Point3d p3)
-        {
-            return (p3.Y - p1.Y) * (p2.X - p1.X) - (p3.X - p1.X) * (p2.Y - p1.Y) > 0;
-        }
-
-        public static bool IsCollinear(Point3d p1, Point3d p2, Point3d p3)
-        {
-            var coordinate1 = p1.ToNTSCoordinate();
-            var coordinate2 = p2.ToNTSCoordinate();
-            var coordinate3 = p3.ToNTSCoordinate();
-            return Orientation.Index(coordinate1, coordinate2, coordinate3) == OrientationIndex.Collinear;
         }
     }
 }
