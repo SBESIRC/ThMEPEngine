9a2f2cdad174549abcbf3c5ca37dfd8ae7e40d48
explorer7777 <1750923@tongji.edu.cn>
2021-09-06 18:37:23 +0800

    block convert delete existing blocks (#737)
    Co-authored-by: 刘潘 <liupan1@thape.com.cn>

---
+++ App/ThMEPElectrical/BlockConvert/ThBConvertExcelExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ThMEPElectrical.BlockConvert
+{
+    public static class ThBConvertExcelExtension
+    {
+        /// <summary>
+        /// 获取Excel中的块转换映射表
+        /// </summary>
+        /// <param name="bConvertConfigUrl"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public static List<ThBConvertRule> Rules(this string bConvertConfigUrl, ConvertMode mode)
+        {
+            var engine = new ThBConvertRuleEngine();
+            return engine.Acquire(bConvertConfigUrl, mode);
+        }
+    }
+}
--- App/ThMEPElectrical/BlockConvert/ThBConvertRuleEngine.cs
+++ App/ThMEPElectrical/BlockConvert/ThBConvertRuleEngine.cs
@@ -2,6 +2,7 @@
 using Linq2Acad;
 using System.Linq;
 using System.Collections.Generic;
+using ThMEPEngineCore.IO.ExcelService;
 using Autodesk.AutoCAD.DatabaseServices;
 
 namespace ThMEPElectrical.BlockConvert
@@ -9,42 +10,39 @@ namespace ThMEPElectrical.BlockConvert
     public class ThBConvertRuleEngine
     {
         /// <summary>
-        /// 从图纸中获取转换规则
+        /// 从Excel表中获取转换规则
         /// </summary>
-        /// <param name="database"></param>
+        /// <param name="bConvertConfigUrl"></param>
+        /// <param name="mode"></param>
         /// <returns></returns>
-        public List<ThBConvertRule> Acquire(Database database, ConvertMode mode)
+        public List<ThBConvertRule> Acquire(string bConvertConfigUrl, ConvertMode mode)
         {
-            using (AcadDatabase acadDatabase = AcadDatabase.Use(database))
+            var rules = new List<ThBConvertRule>();
+            var excelSrevice = new ReadExcelService();
+            var dataSet = excelSrevice.ReadExcelToDataSet(bConvertConfigUrl, true);
+            if ((mode & ConvertMode.STRONGCURRENT) == ConvertMode.STRONGCURRENT)
             {
-                var rules = new List<ThBConvertRule>();
-                if ((mode & ConvertMode.STRONGCURRENT) == ConvertMode.STRONGCURRENT)
-                {
-                    var table = acadDatabase.ModelSpace
-                        .OfType<Table>()
-                        .First(o => o.Cells[0, 0].Text() == ThBConvertCommon.BLOCK_MAP_RULES_TABLE_TITLE_STRONG);
-                    rules.AddRange(Rules(table, ConvertMode.STRONGCURRENT));
-                }
-                if ((mode & ConvertMode.WEAKCURRENT) == ConvertMode.WEAKCURRENT)
-                {
-                    var table = acadDatabase.ModelSpace
-                        .OfType<Table>()
-                        .First(o => o.Cells[0, 0].Text() == ThBConvertCommon.BLOCK_MAP_RULES_TABLE_TITLE_WEAK);
-                    rules.AddRange(Rules(table, ConvertMode.WEAKCURRENT));
-                }
-                return rules;
+                var table = dataSet.Tables[ThBConvertCommon.STRONG_CURRENT];
+                rules.AddRange(Rules(table, ConvertMode.STRONGCURRENT));
             }
+            if ((mode & ConvertMode.WEAKCURRENT) == ConvertMode.WEAKCURRENT)
+            {
+                var table = dataSet.Tables[ThBConvertCommon.WEAK_CURRENT];
+                rules.AddRange(Rules(table, ConvertMode.WEAKCURRENT));
+            }
+            return rules;
         }
 
         /// <summary>
         /// 获取表中的块转换映射表
         /// </summary>
         /// <param name="table"></param>
+        /// <param name="mode"></param>
         /// <returns></returns>
-        public List<ThBConvertRule> Rules(Table table, ConvertMode mode)
+        public List<ThBConvertRule> Rules(System.Data.DataTable table, ConvertMode mode)
         {
             var rules = new List<ThBConvertRule>();
-            for (int row = 2; row < table.Rows.Count; row++)
+            for (int row = 0; row < table.Rows.Count; row++)
             {
                 var source = new ThBlockConvertBlock()
                 {
@@ -53,14 +51,14 @@ namespace ThMEPElectrical.BlockConvert
 
                 // 源块名
                 int column = 0;
-                source.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_NAME] = table.Cells[row, column].Text();
+                source.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_NAME] = StringFilter(table.Rows[row][column].ToString());
 
                 // 源块图示
                 column++;
 
                 // 可见性
                 column++;
-                source.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_VISIBILITY] = table.Cells[row, column].Text();
+                source.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_VISIBILITY] = StringFilter(table.Rows[row][column].ToString());
 
                 var target = new ThBlockConvertBlock()
                 {
@@ -69,18 +67,22 @@ namespace ThMEPElectrical.BlockConvert
 
                 // 目标块名
                 column++;
-                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_NAME] = table.Cells[row, column].Text();
+                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_NAME] = StringFilter(table.Rows[row][column].ToString());
 
                 // 目标块图示
                 column++;
 
                 // 是否炸开
                 column++;
-                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_EXPLODE] = table.Cells[row, column].Text();
+                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_EXPLODE] = StringFilter(table.Rows[row][column].ToString());
 
                 // 目标图层
                 column++;
-                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_LAYER] = table.Cells[row, column].Text();
+                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_LAYER] = StringFilter(table.Rows[row][column].ToString());
+
+                // 内含图块
+                column++;
+                target.Attributes[ThBConvertCommon.BLOCK_MAP_ATTRIBUTES_BLOCK_INTERNAL] = StringFilter(table.Rows[row][column].ToString());
 
                 // 创建映射规则
                 rules.Add(new ThBConvertRule()
@@ -90,6 +92,11 @@ namespace ThMEPElectrical.BlockConvert
                 });
             }
             return rules;
+        }
+
+        private string StringFilter(string str)
+        {
+            return str.Replace(" ", "").Replace("\n", ""); ;
         }
     }
 }
