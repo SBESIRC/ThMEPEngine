8de0f5bb31f5de07f21cce9b539295e44ac5fb15
萌新 <fengzuowei@thape.com.cn>
2021-09-15 19:23:59 +0800

    修改照明ui (#801)

--- App/ThMEPLighting.UI/Lighting/uiThLighting.xaml.cs
+++ App/ThMEPLighting.UI/Lighting/uiThLighting.xaml.cs
@@ -25,33 +25,128 @@ namespace TianHua.Lighting.UI
     /// </summary>
     public partial class uiThLighting : ThCustomWindow
     {
-        static LightingViewModel UIConfigs = null;
+        public static readonly LightingViewModel UIConfigs = new LightingViewModel();
+        static uiThLighting()
+        {
+            var items = UIConfigs.Items;
+            items.Add(new LightingViewModel.Item() { Text = "全选" });
+            items.Add(new LightingViewModel.Item() { Text = "AD-SIGN" });
+        }
+
         public uiThLighting()
         {
             InitializeComponent();
-             if(UIConfigs == null)
-            {
-                UIConfigs = new LightingViewModel();
-            }
-            DataContext = UIConfigs;
-
+            InitUI();
             //For single form instance
             MutexName = "Mutext_uiThLighting";
         }
 
-        private void btnLayout_Click(object sender, RoutedEventArgs e)
+        private void InitUI()
+        {
+            cbRatio.ItemsSource = new string[] { "1:100" };
+            cbSignLightSize.ItemsSource = new string[] { "中型", "大型" };
+            DataContext = UIConfigs;
+            SelectionChangedEventHandler f = null;
+            int lastSelCount = 0;
+            
+            //控制拾取车道线listbox 全选事件
+            f = (s, e) =>
+            {
+                try
+                {
+                    foreach (LightingViewModel.Item item in lstBox.SelectedItems)
+                    {
+                        if (item.Text == "全选")
+                        {
+                            try
+                            {
+                                lstBox.SelectionChanged -= f;
+                                if (lstBox.SelectedItems.Count + 1 == UIConfigs.Items.Count && lstBox.SelectedItems.Count < lastSelCount)
+                                {
+                                    lstBox.SelectedItems.Remove(item);
+                                    return;
+                                }
+                                lstBox.SelectedItems.Clear();
+                                foreach (var m in UIConfigs.Items)
+                                {
+                                    lstBox.SelectedItems.Add(m);
+                                }
+                            }
+                            finally
+                            {
+                                lstBox.SelectionChanged += f;
+                            }
+                            return;
+                        }
+                    }
+                }
+                finally
+                {
+                    lastSelCount = lstBox.SelectedItems.Count;
+                }
+
+                {
+                    var lst = UIConfigs.Items.Except(lstBox.SelectedItems.Cast<LightingViewModel.Item>()).ToList();
+                    if (lst.Count == 1)
+                    {
+                        if (lst[0].Text == "全选")
+                        {
+                            lstBox.SelectedItems.Clear();
+                        }
+                    }
+                }
+
+            };
+            lstBox.SelectionChanged += f;
+            lstBox.PreviewKeyDown += (s, e) => { e.Handled = true; };
+        }
+
+        private void btnPlace_Click(object sender, RoutedEventArgs e)
         {
             using (var cmd = new LightingLayoutCommand(UIConfigs))
             {
                 cmd.Execute();
             }
         }
-        private void bthRouting_Click(object sender, RoutedEventArgs e)
+        private void btnRouting_Click(object sender, RoutedEventArgs e)
         {
             using (var cmd = new LightingRouteCableCommand(UIConfigs))
             {
                 cmd.Execute();
             }
         }
+      
+
+        private void btnReGen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDrawLightCenter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDrawNonLightCenter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDrawSingleCenter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPickUp(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCalcPath(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
     }
 }
--- App/ThMEPLighting/Lighting/LightingViewModel.cs
+++ App/ThMEPLighting/Lighting/LightingViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,7 @@ using ThControlLibraryWPF.ControlUtils;
 
 namespace ThMEPLighting.Lighting.ViewModels
 {
-    public enum RadioLightOptions { L1, L2, L3, L4, L5}
-    public enum RadioDualUseOptions { Yes, No }
+
     public class EnumBooleanConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -23,86 +23,350 @@ namespace ThMEPLighting.Lighting.ViewModels
             return ((bool)value) ? parameter : Binding.DoNothing;
         }
     }
-    public class LightingViewModel : NotifyPropertyChangedBase
+    public class RadioUiValueConverter : IValueConverter
     {
-        private RadioLightOptions _LightOption = RadioLightOptions.L1;
-        public RadioLightOptions LightOption
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            get
+            if (value is null) return Equals(value, parameter);
+            if (value is bool b)
             {
-                return _LightOption;
+                return b == bool.Parse((string)parameter);
             }
-            set
+            if (value is string str)
             {
-                _LightOption = value;
-                OnPropertyChanged("LightOption");
+                return str == (string)parameter;
             }
+            if (value.GetType().IsEnum)
+            {
+                return value.ToString() == (string)parameter;
+            }
+            return Equals(value, parameter);
         }
 
-        private double _Radius = 1000; //mm
-
-        public double LightingRadius
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            get { return _Radius; }
-            set 
+            if ((bool)value)
             {
-                _Radius = value;
-                OnPropertyChanged("LightingRadius");
+                if (targetType == typeof(bool))
+                {
+                    return bool.Parse((string)parameter);
+                }
+                if (targetType == typeof(string))
+                {
+                    return parameter;
+                }
+                if (targetType.IsEnum)
+                {
+                    return Enum.Parse(targetType, (string)parameter);
+                }
             }
-        }
-
-        private double _EvacuationRadius = 1000; //mm
-
-        public double EvacuationRadius
-        {
-            get { return _EvacuationRadius; }
-            set 
-            {
-                _EvacuationRadius = value;
-                OnPropertyChanged("EvacuationRadius");
-            }
-        }
-
-        private RadioDualUseOptions _DualUseOption =  RadioDualUseOptions.No;
-
-        public RadioDualUseOptions DualUseOption
-        {
-            get { return _DualUseOption; }
-            set
-            {
-                _DualUseOption = value;
-                OnPropertyChanged("DualUseOption");
-            }
-        }
-
-        //块参照比例index
-        private int _BlockRatioIndex = 0;
-        public int BlockRatioIndex
-        {
-            get
-            {
-                return _BlockRatioIndex;
-            }
-            set
-            {
-                _BlockRatioIndex = value;
-                OnPropertyChanged("BlockRatioIndex");
-            }
-        }
-
-        //块参照比例string
-        private string _BlockRatio = string.Empty;
-        public string BlockRatio
-        {
-            get
-            {
-                return _BlockRatio;
-            }
-            set
-            {
-                _BlockRatio = value;
-                OnPropertyChanged("BlockRatio");
-            }
+            return Binding.DoNothing;
         }
     }
+    public class LightingViewModel : NotifyPropertyChangedBase
+    {
+        string _LightingLamps = "圆形吸顶灯";
+        public string LightingLamps
+        {
+            get => _LightingLamps;
+            set
+            {
+                if (value != _LightingLamps)
+                {
+                    _LightingLamps = value;
+                    OnPropertyChanged(nameof(LightingLamps));
+                }
+            }
+        }
+
+        double _LayoutRadiusOfNormalLightingLamps = 3000;
+        public double LayoutRadiusOfNormalLightingLamps
+        {
+            get => _LayoutRadiusOfNormalLightingLamps;
+            set
+            {
+                if (value != _LayoutRadiusOfNormalLightingLamps)
+                {
+                    _LayoutRadiusOfNormalLightingLamps = value;
+                    OnPropertyChanged(nameof(LayoutRadiusOfNormalLightingLamps));
+                }
+            }
+        }
+
+        double _LayoutRadiusOfEmergencyLightingLamps = 6000;
+        public double LayoutRadiusOfEmergencyLightingLamps
+        {
+            get => _LayoutRadiusOfEmergencyLightingLamps;
+            set
+            {
+                if (value != _LayoutRadiusOfEmergencyLightingLamps)
+                {
+                    _LayoutRadiusOfEmergencyLightingLamps = value;
+                    OnPropertyChanged(nameof(LayoutRadiusOfEmergencyLightingLamps));
+                }
+            }
+        }
+
+        bool _WhetherEmergencyLightingIsAlsoUsedAsOrdinaryLighting = false;
+        public bool WhetherEmergencyLightingIsAlsoUsedAsOrdinaryLighting
+        {
+            get => _WhetherEmergencyLightingIsAlsoUsedAsOrdinaryLighting;
+            set
+            {
+                if (value != _WhetherEmergencyLightingIsAlsoUsedAsOrdinaryLighting)
+                {
+                    _WhetherEmergencyLightingIsAlsoUsedAsOrdinaryLighting = value;
+                    OnPropertyChanged(nameof(WhetherEmergencyLightingIsAlsoUsedAsOrdinaryLighting));
+                }
+            }
+        }
+
+        string _IlluminanceControl = "单排布置";
+        public string IlluminanceControl
+        {
+            get => _IlluminanceControl;
+            set
+            {
+                if (value != _IlluminanceControl)
+                {
+                    _IlluminanceControl = value;
+                    OnPropertyChanged(nameof(IlluminanceControl));
+                }
+            }
+        }
+
+        string _InstallationMode = "线槽安装";
+        public string InstallationMode
+        {
+            get => _InstallationMode;
+            set
+            {
+                if (value != _InstallationMode)
+                {
+                    _InstallationMode = value;
+                    OnPropertyChanged(nameof(InstallationMode));
+                }
+            }
+        }
+
+        string _LayoutMode = "按柱跨布置";
+        public string LayoutMode
+        {
+            get => _LayoutMode;
+            set
+            {
+                if (value != _LayoutMode)
+                {
+                    _LayoutMode = value;
+                    OnPropertyChanged(nameof(LayoutMode));
+                }
+            }
+        }
+
+        string _NumberOfCircuits = "自动计算";
+        public string NumberOfCircuits
+        {
+            get => _NumberOfCircuits;
+            set
+            {
+                if (value != _NumberOfCircuits)
+                {
+                    _NumberOfCircuits = value;
+                    OnPropertyChanged(nameof(NumberOfCircuits));
+                }
+            }
+        }
+
+        int _NumberOfCircuitsAutomaticCalculationOfNLoop = 25;
+        public int NumberOfCircuitsAutomaticCalculationOfNLoop
+        {
+            get => _NumberOfCircuitsAutomaticCalculationOfNLoop;
+            set
+            {
+                if (value != _NumberOfCircuitsAutomaticCalculationOfNLoop)
+                {
+                    _NumberOfCircuitsAutomaticCalculationOfNLoop = value;
+                    OnPropertyChanged(nameof(NumberOfCircuitsAutomaticCalculationOfNLoop));
+                }
+            }
+        }
+
+        int _NumberOfCircuitsSpecifyTheNumberOfNPerCircuits = 4;
+        public int NumberOfCircuitsSpecifyTheNumberOfNPerCircuits
+        {
+            get => _NumberOfCircuitsSpecifyTheNumberOfNPerCircuits;
+            set
+            {
+                if (value != _NumberOfCircuitsSpecifyTheNumberOfNPerCircuits)
+                {
+                    _NumberOfCircuitsSpecifyTheNumberOfNPerCircuits = value;
+                    OnPropertyChanged(nameof(NumberOfCircuitsSpecifyTheNumberOfNPerCircuits));
+                }
+            }
+        }
+
+        string _StartingNumber = "01";
+        public string StartingNumber
+        {
+            get => _StartingNumber;
+            set
+            {
+                if (value != _StartingNumber)
+                {
+                    _StartingNumber = value;
+                    OnPropertyChanged(nameof(StartingNumber));
+                }
+            }
+        }
+
+        double _TrunkingWidth = 300;
+        public double TrunkingWidth
+        {
+            get => _TrunkingWidth;
+            set
+            {
+                if (value != _TrunkingWidth)
+                {
+                    _TrunkingWidth = value;
+                    OnPropertyChanged(nameof(TrunkingWidth));
+                }
+            }
+        }
+
+        double _DoubleRowSpacing = 2700;
+        public double DoubleRowSpacing
+        {
+            get => _DoubleRowSpacing;
+            set
+            {
+                if (value != _DoubleRowSpacing)
+                {
+                    _DoubleRowSpacing = value;
+                    OnPropertyChanged(nameof(DoubleRowSpacing));
+                }
+            }
+        }
+
+        double _LampSpacing = 2700;
+        public double LampSpacing
+        {
+            get => _LampSpacing;
+            set
+            {
+                if (value != _LampSpacing)
+                {
+                    _LampSpacing = value;
+                    OnPropertyChanged(nameof(LampSpacing));
+                }
+            }
+        }
+
+        string _EvacuationInstructions = "优先壁装";
+        public string EvacuationInstructions
+        {
+            get => _EvacuationInstructions;
+            set
+            {
+                if (value != _EvacuationInstructions)
+                {
+                    _EvacuationInstructions = value;
+                    OnPropertyChanged(nameof(EvacuationInstructions));
+                }
+            }
+        }
+
+        string _MarkerLampSize = "中型";
+        public string MarkerLampSize
+        {
+            get => _MarkerLampSize;
+            set
+            {
+                if (value != _MarkerLampSize)
+                {
+                    _MarkerLampSize = value;
+                    OnPropertyChanged(nameof(MarkerLampSize));
+                }
+            }
+        }
+
+        bool _DisplayEvacuationRoute;
+        public bool DisplayEvacuationRoute
+        {
+            get => _DisplayEvacuationRoute;
+            set
+            {
+                if (value != _DisplayEvacuationRoute)
+                {
+                    _DisplayEvacuationRoute = value;
+                    OnPropertyChanged(nameof(DisplayEvacuationRoute));
+                }
+            }
+        }
+
+        string _BasementEmergencyLighting = "疏散照明壁灯";
+        public string BasementEmergencyLighting
+        {
+            get => _BasementEmergencyLighting;
+            set
+            {
+                if (value != _BasementEmergencyLighting)
+                {
+                    _BasementEmergencyLighting = value;
+                    OnPropertyChanged(nameof(BasementEmergencyLighting));
+                }
+            }
+        }
+
+        string _LaneLayout = "车道单侧布置";
+        public string LaneLayout
+        {
+            get => _LaneLayout;
+            set
+            {
+                if (value != _LaneLayout)
+                {
+                    _LaneLayout = value;
+                    OnPropertyChanged(nameof(LaneLayout));
+                }
+            }
+        }
+
+        string _GlobalScale = "1:100";
+        public string GlobalScale
+        {
+            get => _GlobalScale;
+            set
+            {
+                if (value != _GlobalScale)
+                {
+                    _GlobalScale = value;
+                    OnPropertyChanged(nameof(GlobalScale));
+                }
+            }
+        }
+
+        public class Item : NotifyPropertyChangedBase
+        {
+            string _text;
+            public string Text
+            {
+                get => _text;
+                set
+                {
+                    if (value != _text)
+                    {
+                        _text = value;
+                        OnPropertyChanged(nameof(Text));
+                    }
+                }
+            }
+        }
+
+        ObservableCollection<Item> _items = new ObservableCollection<Item>();
+        public ObservableCollection<Item> Items => _items;
+    }
+
+
+
+
 }
