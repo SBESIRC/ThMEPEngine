7b13deaaed924fe471fd577cbab6abccadcecb1e
董士崇 <dongshichong@thape.com.cn>
2021-10-09 16:10:51 +0800

    DBObjectCollection.UnionGeometries uses OverlayNGRobust.Overlay

--- App/ThCADCore/NTS/ThCADCoreNTSDbObjCollectionExtension.cs
+++ App/ThCADCore/NTS/ThCADCoreNTSDbObjCollectionExtension.cs
@@ -42,7 +42,12 @@ namespace ThCADCore.NTS
 
         public static Geometry UnionGeometries(this DBObjectCollection curves)
         {
-            return OverlayNGRobust.Union(curves.ToNTSMultiPolygon().Geometries);
+            // UnaryUnionOp.Union()有Robust issue
+            // 会抛出"non-noded intersection" TopologyException
+            // OverlayNGRobust.Union()在某些情况下仍然会抛出TopologyException (NTS 2.2.0)
+            Geometry polygons = curves.ToNTSMultiPolygon();
+            Geometry polygon = ThCADCoreNTSService.Instance.GeometryFactory.CreatePolygon();
+            return OverlayNGRobust.Overlay(polygons, polygon, SpatialFunction.Union);
         }
 
         public static DBObjectCollection UnionPolygons(this DBObjectCollection curves)
