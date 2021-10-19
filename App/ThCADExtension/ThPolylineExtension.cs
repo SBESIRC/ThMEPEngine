7c4f6062312800c2bcd80479cf436b10abd8f89e
liupan-AI <1750923@tongji.edu.cn>
2021-10-19 11:58:50 +0800

    ThPolylineTessellate blockwise direction fixing (#958)
    * ThPolylineTessellate blockwise direction fixing
    
    * Optimize ThPolylineTessellate's code
    
    Co-authored-by: 刘潘 <liupan1@thape.com.cn>

--- App/ThCADExtension/ThPolylineExtension.cs
+++ App/ThCADExtension/ThPolylineExtension.cs
@@ -95,7 +95,7 @@ namespace ThCADExtension
             var segment = new PolylineSegment(
                 arc.StartPoint.ToPoint2D(),
                 arc.EndPoint.ToPoint2D(),
-                arc.BulgeFromCurve(false));
+                arc.BulgeFromCurve(arc.Normal.DotProduct(Vector3d.ZAxis) < 0));
             return segment.TessellateSegmentWithChord(chord).ToPolyline();
         }
 
@@ -156,7 +156,7 @@ namespace ThCADExtension
             var segment = new PolylineSegment(
                 arc.StartPoint.ToPoint2D(),
                 arc.EndPoint.ToPoint2D(),
-                arc.BulgeFromCurve(false));
+                arc.BulgeFromCurve(arc.Normal.DotProduct(Vector3d.ZAxis) < 0));
             return segment.TessellateSegmentWithArc(length).ToPolyline();
         }
 
