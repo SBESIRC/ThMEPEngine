547deb3879f8019049c5234ef846c239f222ad6e
liupan-AI <1750923@tongji.edu.cn>
2021-10-21 15:51:31 +0800

    Implement ThCADCoreNTSGeometryFixer (#965)
    Co-authored-by: 刘潘 <liupan1@thape.com.cn>

---
+++ App/ThCADCore/NTS/ThCADCoreNTSGeometryFixer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NFox.Cad;
+using System.Linq;
+using Dreambuild.AutoCAD;
+using NetTopologySuite.Geometries;
+using Autodesk.AutoCAD.DatabaseServices;
+using NetTopologySuite.Operation.Overlay;
+using NetTopologySuite.Operation.OverlayNG;
+using NetTopologySuite.Geometries.Utilities;
+using AcPolygon = Autodesk.AutoCAD.DatabaseServices.Polyline;
+
+namespace ThCADCore.NTS
+{
+    public static class ThCADCoreNTSGeometryFixer
+    {
+        public static DBObjectCollection MakeValid(this DBObjectCollection polygons)
+        {
+            return GeometryFixer.Fix(polygons.ToNTSMultiPolygon()).ToDbCollection();
+        }
+    }
+}
