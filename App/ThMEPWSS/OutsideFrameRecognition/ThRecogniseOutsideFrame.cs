ec513866de1de6bde2fe769a414083ade4790700
jackwhoknow <trasure@126.com>
2021-11-15 11:28:52 +0800

    Feature/file hydrant check details implement (#1053)
    * Connected  the function of identifying the outer contour of the room to the hydrant checking system and  added a test command.
    
    * Connected  the function of identifying the outer contour of the room to the hydrant checking system and  added a test command.
    
    * Fix a bug in pressure drainage system diagram.
    
    * update hydrant get external space with new logic
    
    * resolove rebase conflict
    
    * fixed rebase conflicted
    
    * add Offset for ThHydrantRoomExtractor
    
    Co-authored-by: 余忠晟 <yuzhongsheng@thape.com.cn>
    Co-authored-by: 鲍金昌 <baojinchang@thape.com.cn>
    Co-authored-by: 刘光胜 <liuguangsheng@thape.com.cn>

--- App/ThMEPWSS/OutsideFrameRecognition/ThRecogniseOutsideFrame.cs
+++ App/ThMEPWSS/OutsideFrameRecognition/ThRecogniseOutsideFrame.cs
@@ -1,10 +1,13 @@
 ﻿using AcHelper;
 using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
+using Dreambuild.AutoCAD;
 using Linq2Acad;
 using System.Collections.Generic;
 using System.Linq;
 using ThCADCore.NTS;
+using ThCADExtension;
 using ThMEPEngineCore.Algorithm;
 using ThMEPEngineCore.CAD;
 
@@ -12,6 +15,35 @@ namespace ThMEPWSS.OutsideFrameRecognition
 {
     public class ThRecogniseOutsideFrame
     {
+        public void DisplayOutsideFrameForTest()
+        {
+            using (AcadDatabase adb = AcadDatabase.Active())
+            {
+                var result = Active.Editor.GetSelection();
+                if (result.Status != PromptStatus.OK)
+                {
+                    return;
+                }
+                List<Polyline> plys = new List<Polyline>();
+                foreach (ObjectId obj in result.Value.GetObjectIds())
+                {
+                    try
+                    {
+                        var frame = adb.Element<Polyline>(obj);
+                        if (frame.Layer == "AI-房间框线")
+                            plys.Add(ThMEPFrameService.Normalize(frame.Clone() as Polyline));
+                    }
+                    catch { }
+                }
+                plys = GetOutsideFrame(plys);
+                foreach (var p in plys)
+                {
+                    p.ColorIndex = 13;
+                    p.AddToCurrentSpace();
+                }
+            }
+        }
+
         /// <summary>
         /// 获取房间框线集的外包络框
         /// roomframes:房间框线
@@ -24,11 +56,12 @@ namespace ThMEPWSS.OutsideFrameRecognition
         /// <returns></returns>
         public static List<Polyline> GetOutsideFrame(List<Polyline> roomframes, double tol_grouped = 1500, double concave_param = 1000)
         {
-            using (var Doclock = Active.Document.LockDocument())
             using (AcadDatabase adb = AcadDatabase.Active())
             {
                 var outsideframes = new List<Polyline>();
-                if (roomframes.Count <= 1) return roomframes;
+                if (roomframes.Count < 1) return roomframes;
+                roomframes = SortPolylineBasedArea(roomframes);
+                roomframes.Reverse();
                 var plys = new List<Polyline>();
                 roomframes.ForEach(p => plys.Add(p));
                 var grouped_plys = new List<List<Polyline>>();
@@ -117,6 +150,46 @@ namespace ThMEPWSS.OutsideFrameRecognition
                     }
                     tmp_plys = new();
                 }
+            }
+        }
+
+        public static MPolygon CreatMpolygonFromBoundaryToInfinity(List<Polyline> plys, double buffer_distance = 30000)
+        {
+            List<Point3d> oripoints = new List<Point3d>();
+            plys.ForEach(p => oripoints.AddRange(p.EntityVertices().Cast<Point3d>().ToList()));
+            List<Point3d> convex_points = Algorithms.GetConvexHull(oripoints);
+            Polyline ply = new Polyline();
+            for (int i = 0; i < convex_points.Count; i++)
+            {
+                ply.AddVertexAt(i, convex_points[i].ToPoint2d(), 0, 0, 0);
+            }
+            ply.AddVertexAt(ply.Vertices().Count, ply.Vertices()[0].ToPoint2d(), 0, 0, 0);
+            if (ply.BufferPL(buffer_distance).Cast<Polyline>().ToList().Count > 0)
+            {
+                ply = ply.BufferPL(buffer_distance).Cast<Polyline>().First();
+            }
+            List<Curve> crvs = plys.Cast<Curve>().ToList();
+            var m= ThMPolygonTool.CreateMPolygon(ply, crvs);
+            return ThMPolygonTool.CreateMPolygon(ply, crvs);
+        }
+
+        private static List<Polyline> SortPolylineBasedArea(List<Polyline> plys)
+        {
+            var comparer = new PolyComparer();
+            plys.Sort(comparer);
+            return plys;
+        }
+
+        private class PolyComparer : IComparer<Polyline>
+        {
+            public PolyComparer()
+            {
+            }
+            public int Compare(Polyline a, Polyline b)
+            {
+                if (a.Area == b.Area) return 0;
+                else if (a.Area < b.Area) return -1;
+                else return 1;
             }
         }
     }
