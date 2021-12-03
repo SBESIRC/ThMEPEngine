c7b155bb45b9ce7938528367b271108ea35e053e
abysee <87347167+abysee@users.noreply.github.com>
2021-12-03 18:43:18 +0800

    Tower seperation (#1164)
    * THGA command
    
    * tower separation
    
    * Revert "THGA command"
    
    This reverts commit 98e4e31061cff95a52a097dff2680a016b98c307.
    
    * update code for tower seperation
    
    * Revert "THGA command"
    
    This reverts commit 989e4b02be6acbc7343e43e4e799ec9698d74f8c.
    
    Co-authored-by: 王文广 <wangwenguang@thape.com.cn>

---
+++ App/ThMEPWSS/TowerSeparation/DBScanPoints.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using Autodesk.AutoCAD.Geometry;
+
+namespace ThMEPWSS.TowerSeparation.DBScan
+{
+    public class DBScanCluster
+    {
+        public Point3dCollection sampleSet { get; set; } = new Point3dCollection();
+        public double epsilon { get; set; }
+        public int MinPts { get; set; }
+        private int clusterNum { get; set; }
+        private Point3dCollection centerPoints = new Point3dCollection();
+        private Point3dCollection notVisited = new Point3dCollection();
+        private Dictionary<Point3d, Point3dCollection> neighbors = new Dictionary<Point3d, Point3dCollection>();
+        private void FindCenterPoints()
+        {
+            FindNeighbor();
+            foreach(Point3d pt in sampleSet)
+            {
+                if(neighbors.ContainsKey(pt) && neighbors[pt].Count >= MinPts)
+                {
+                    centerPoints.Add(pt);
+                }
+            }
+        }
+
+        private void FindNeighbor()
+        {
+            for(int i = 0; i< sampleSet.Count; ++i)
+            {
+                for(int j = i + 1; j < sampleSet.Count; ++j)
+                {
+                    Point3d currentPt1 = sampleSet[i];
+                    Point3d currentPt2 = sampleSet[j];
+                    if(currentPt1.DistanceTo(currentPt2) <= epsilon)
+                    {
+                        if (neighbors.ContainsKey(currentPt1))
+                        {
+                            neighbors[currentPt1].Add(currentPt2);
+                        }
+                        else
+                        {
+                            neighbors.Add(currentPt1, new Point3dCollection { currentPt2 });
+                        }
+                        if (neighbors.ContainsKey(currentPt2))
+                        {
+                            neighbors[currentPt2].Add(currentPt1);
+                        }
+                        else
+                        {
+                            neighbors.Add(currentPt2, new Point3dCollection { currentPt1 });
+                        }
+                    }
+                }
+            }
+        }
+        private void Initialize(Point3dCollection pointSet)
+        {
+            sampleSet = pointSet;
+            FindCenterPoints();
+            clusterNum = 0;
+            foreach(Point3d pt in sampleSet)
+            {
+                notVisited.Add(pt);
+            }
+        }
+
+        public List<Point3dCollection> getClusters(Point3dCollection pointSet, double Epsilon, int minPts)
+        {
+            epsilon = Epsilon;
+            MinPts = minPts;
+            List<Point3dCollection> Clusters = new List<Point3dCollection>();
+            Initialize(pointSet);
+            while(centerPoints.Count > 0)
+            {
+                Point3dCollection currentNotVisit = new Point3dCollection();
+                foreach(Point3d pt in notVisited)
+                {
+                    currentNotVisit.Add(pt);
+                }
+                Point3d firstCorePt = centerPoints[0];
+                List<Point3d> queue = new List<Point3d> { firstCorePt };
+                notVisited.Remove(firstCorePt);
+                while (queue.Count > 0)
+                {
+                    Point3d q = queue[0];
+                    queue.RemoveAt(0);
+                    if(neighbors[q].Count >= MinPts)
+                    {
+                        Point3dCollection temp = neighbors[q];
+                        foreach(Point3d pt in temp)
+                        {
+                            if(notVisited.Contains(pt))
+                            {
+                                queue.Add(pt);
+                                notVisited.Remove(pt);
+                            }
+                        }
+                    }
+                }
+                Point3dCollection Ck = new Point3dCollection();
+                foreach(Point3d pt in currentNotVisit)
+                {
+                    if (!notVisited.Contains(pt))
+                    {
+                        Ck.Add(pt);
+                        if (centerPoints.Contains(pt))
+                        {
+                            centerPoints.Remove(pt);
+                        }
+                    }
+                }
+                if(Ck.Count > 0)
+                {
+                    ++clusterNum;
+                    Clusters.Add(Ck);
+                }
+                
+            }
+            return Clusters;
+        }
+
+    }
+}
---
+++ App/ThMEPWSS/TowerSeparation/TowerSeparationTest.cs
@@ -0,0 +1,69 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Linq2Acad;
+using ThCADExtension;
+using ThCADCore.NTS;
+using ThMEPEngineCore.Command;
+using ThMEPWSS.SprinklerConnect.Service;
+using ThMEPWSS.SprinklerConnect.Data;
+using ThMEPWSS.TowerSeparation.TowerExtract;
+using ThMEPWSS.DrainageSystemDiagram;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace ThMEPWSS.SprinklerConnect.Cmd
+{
+    public partial class ThSprinklerConnectNoUICmd
+    {
+        [CommandMethod("TIANHUACAD", "THSeparateTowerTest", CommandFlags.Modal)]
+        public void SeparateTower()
+        {
+            var cmd = new TowerSeparationTest();
+            cmd.Execute();
+        }
+    }
+
+    public class TowerSeparationTest : ThMEPBaseCommand
+    {
+        public TowerSeparationTest()
+        {
+        }
+        public override void SubExecute()
+        {
+            TowerSeparateExecute();
+        }
+
+        public void TowerSeparateExecute()
+        {
+            using (var doclock = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.LockDocument())
+            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            {
+                var frame = ThSprinklerDataService.GetFrame();
+                if (frame == null || frame.Area < 10)
+                {
+                    return;
+                }
+
+
+                var dataset = new ThSprinklerConnectDataFactory();
+                var geos = dataset.Create(acadDatabase.Database, frame.Vertices()).Container;
+                var dataQuery = new ThSprinklerDataQueryService(geos);
+                dataQuery.ClassifyData();
+                var TowerExtractor = new TowerExtractor();
+
+                var shearWalls = TowerExtractor.Extractor(dataQuery.ShearWallList, frame);
+                DrawUtils.ShowGeometry(shearWalls, "testForExtractor", 1);
+
+                //foreach(Polyline l in shearWalls)
+                //{
+                //    l.ColorIndex = 5;
+                //}
+                //acadDatabase.ModelSpace.Add(shearWalls);
+
+
+
+            }
+        }
+
+
+    }
+}
