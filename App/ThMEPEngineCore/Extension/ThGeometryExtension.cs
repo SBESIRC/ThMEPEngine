037f80ff36038046abb99d29579eeb03021f53fc
董士崇 <dongshichong@thape.com.cn>
2021-12-06 14:42:02 +0800

    Fix typo

--- App/ThMEPEngineCore/Extension/ThGeometryExtension.cs
+++ App/ThMEPEngineCore/Extension/ThGeometryExtension.cs
@@ -15,7 +15,7 @@ namespace ThMEPEngineCore.Extension
                     // Reference:
                     // https://knowledge.autodesk.com/support/autocad/learn-explore/caas/sfdcarticles/sfdcarticles/how-to-flatten-a-drawing-in-autocad.html
                     g.Boundary.TransformBy(Matrix3d.Displacement(new Vector3d(0, 0, 1E99)));
-                    g.Boundary.TransformBy(Matrix3d.Displacement(new Vector3d(0, 0, 1E-99)));
+                    g.Boundary.TransformBy(Matrix3d.Displacement(new Vector3d(0, 0, -1E99)));
                 }
             });
         }
