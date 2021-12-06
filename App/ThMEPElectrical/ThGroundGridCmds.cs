755f89fdca854bc5680e0744f94dcfdc0638b8e8
第三次存在 <514940817@qq.com>
2021-12-06 20:24:41 +0800

    commit ucs division step2 and roll back branch connecting (#1174)
    Co-authored-by: 汤永靖 <tangyongjing@thape.com.cn>

--- App/ThMEPElectrical/ThGroundGridCmds.cs
+++ App/ThMEPElectrical/ThGroundGridCmds.cs
@@ -1,5 +1,15 @@
 ﻿using ThMEPElectrical.Command;
 using Autodesk.AutoCAD.Runtime;
+using Linq2Acad;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+using ThCADExtension;
+using AcHelper;
+using System.Collections.Generic;
+using ThMEPElectrical.StructureHandleService;
+using ThMEPEngineCore.Algorithm;
+using Autodesk.AutoCAD.Geometry;
+using ThMEPEngineCore.UCSDivisionService;
 
 namespace ThMEPElectrical
 {
@@ -11,6 +21,56 @@ namespace ThMEPElectrical
             using (var cmd = new ThGroundGridCommand())
             {
                 cmd.Execute();
+            }
+        }
+
+        /// <summary>
+        /// ucs分区
+        /// </summary>
+        [CommandMethod("TIANHUACAD", "THUCSDIV", CommandFlags.Modal)]
+        public void ThUcsDisivision()
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            {
+                // 获取框线
+                PromptSelectionOptions options = new PromptSelectionOptions()
+                {
+                    AllowDuplicates = false,
+                    MessageForAdding = "选择区域",
+                    RejectObjectsOnLockedLayers = true,
+                };
+                var dxfNames = new string[]
+                {
+                    RXClass.GetClass(typeof(Polyline)).DxfName,
+                };
+                var filter = ThSelectionFilterTool.Build(dxfNames);
+                var result = Active.Editor.GetSelection(options, filter);
+                if (result.Status != PromptStatus.OK)
+                {
+                    return;
+                }
+
+                List<Polyline> frameLst = new List<Polyline>();
+                foreach (ObjectId obj in result.Value.GetObjectIds())
+                {
+                    var frame = acadDatabase.Element<BlockReference>(obj);
+                    frameLst.Add(frame.Clone() as Polyline);
+                }
+
+                ThMEPOriginTransformer originTransformer = new ThMEPOriginTransformer(Point3d.Origin);
+                foreach (var frame in frameLst)
+                {
+                    GetPrimitivesService getPrimitivesService = new GetPrimitivesService(originTransformer);
+                    getPrimitivesService.GetStructureInfo(frame, out List<Polyline> columns, out List<Polyline> walls);
+
+                    //区域分割
+                    UCSService uCSService = new UCSService();
+                    var ucsInfo = uCSService.UcsDivision(columns, frame);
+                    foreach (var item in ucsInfo)
+                    {
+                        acadDatabase.ModelSpace.Add(item.Key);
+                    }
+                }
             }
         }
     }
---
+++ App/ThMEPEngineCore/UCSDivisionService/Utils/GeoUtils.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThCADCore.NTS;
+
+namespace ThMEPEngineCore.UCSDivisionService.Utils
+{
+    public static class GeoUtils
+    {
+        public static Polyline GetLinesOutBox(List<Polyline> triangles)
+        {
+            var allLines = triangles.SelectMany(x => StructUtils.GetLinesByPolyline(x)).ToList();
+            var outLines = allLines.Where(x => triangles.Where(y => y.Distance(x.StartPoint) < 1 && y.Distance(x.EndPoint) < 1).Count() < 2).ToList();
+            var firLine = outLines.First();
+            var sP = firLine.StartPoint;
+            var eP = firLine.EndPoint;
+            Polyline outBox = new Polyline() { Closed = true };
+            int index = 0;
+            while (outLines.Count > 0 && !firLine.EndPoint.IsEqualTo(sP, new Tolerance(1, 1)))
+            {
+                outLines = outLines.Where(x => !x.IsEqualLine(firLine)).ToList();
+                firLine = outLines.Where(x => x.StartPoint.IsEqualTo(eP, new Tolerance(1, 1)) || x.EndPoint.IsEqualTo(eP, new Tolerance(1, 1))).FirstOrDefault();
+                if (firLine == null) return outBox;
+
+                var firEndPt = firLine.StartPoint.IsEqualTo(eP, new Tolerance(1, 1)) ? firLine.EndPoint : firLine.StartPoint;
+                firLine = new Line(eP, firEndPt);
+                outBox.AddVertexAt(index, eP.ToPoint2D(), 0, 0, 0);
+                index++;
+                eP = firEndPt;
+            }
+            outBox.AddVertexAt(index, sP.ToPoint2D(), 0, 0, 0);
+
+            return outBox;
+        }
+
+        public static bool IsEqualLine(this Line line, Line otherLine)
+        {
+            return (line.StartPoint.IsEqualTo(otherLine.StartPoint) && line.EndPoint.IsEqualTo(otherLine.EndPoint)) ||
+                (line.StartPoint.IsEqualTo(otherLine.EndPoint) && line.EndPoint.IsEqualTo(otherLine.StartPoint));
+        }
+    }
+}
