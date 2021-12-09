be4d678f11b63498a0cb99b7bf393105dcd61b9f
董士崇 <dongshichong@thape.com.cn>
2021-12-09 13:26:56 +0800

    ThCADCoreNTSPolygonExtension supprt far away hatches

--- App/ThCADCore/NTS/ThCADCoreNTSHatchExtension.cs
+++ App/ThCADCore/NTS/ThCADCoreNTSHatchExtension.cs
@@ -3,6 +3,7 @@ using NFox.Cad;
 using System.Linq;
 using ThCADExtension;
 using Dreambuild.AutoCAD;
+using Autodesk.AutoCAD.Geometry;
 using System.Collections.Generic;
 using NetTopologySuite.Geometries;
 using Autodesk.AutoCAD.DatabaseServices;
@@ -35,13 +36,33 @@ namespace ThCADCore.NTS
         public static DBObjectCollection Normalize(DBObjectCollection loops)
         {
             var objs = new DBObjectCollection();
+            // NTS Buffer对于非常远的坐标（WCS下，>10E10)处理的不好
+            // Workaround就是将位于非常远的图元临时移动到WCS原点附近，参与运算
+            // 运算结束后将运算结果再按相同的偏移从WCS原点附近移动到其原始位置
+            var transform = GetToNearOriginMatrix(loops);
+            loops.OfType<AcPolygon>().ForEach(l => l.TransformBy(transform));
             loops.OfType<AcPolygon>().ForEach(l =>
             {
-                l = l.Buffer(-OFFSET_DISTANCE).OfType<AcPolygon>().OrderByDescending(o => o.Area).First();
-                l = l.Buffer(OFFSET_DISTANCE).OfType<AcPolygon>().OrderByDescending(o => o.Area).First();
+                l = Offset(l, -OFFSET_DISTANCE);
+                l = Offset(l, OFFSET_DISTANCE);
                 objs.Add(l);
             });
+            // 将结果恢复到原始位置
+            var inverse = transform.Inverse();
+            objs.OfType<AcPolygon>().ForEach(l => l.TransformBy(inverse));
             return objs;
+        }
+
+        private static AcPolygon Offset(AcPolygon polygon, double distance)
+        {
+            return polygon.Buffer(distance).OfType<AcPolygon>().OrderByDescending(o => o.Area).First();
+        }
+
+        private static Matrix3d GetToNearOriginMatrix(DBObjectCollection loops)
+        {
+            var center = loops.GeometricExtents().CenterPoint();
+            var vector = center.GetVectorTo(Point3d.Origin);
+            return Matrix3d.Displacement(vector);
         }
 
         private static MultiPolygon ToNTSMultiPolygon(this Hatch hatch)
