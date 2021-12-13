4b2cfba8551e34585307e16621434f0943993569
董士崇 <dongshichong@thape.com.cn>
2021-12-13 11:36:22 +0800

    Set up TianHua.Architecture.WPI.UI project

---
+++ App/TianHua.Architecture.WPI.UI/ArchitectureWPFUIApp.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace TianHua.Architecture.WPI.UI
+{
+    public class ArchitectureWPFUIApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //
+        }
+
+        public void Terminate()
+        {
+            //
+        }
+    }
+}
