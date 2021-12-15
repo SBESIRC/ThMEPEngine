1a7394a7c1cb400ad5120ad436ba71a749138ab8
刘光胜 <liuguangsheng@thape.com.cn>
2021-12-15 11:03:05 +0800

    define ThDCLDataSetFactory

---
+++ App/ThMEPElectrical/DCL/Data/ThDCLDataSetFactory.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThMEPEngineCore.Data;
+
+namespace ThMEPElectrical.DCL.Data
+{
+    internal class ThDCLDataSetFactory : ThMEPDataSetFactory
+    {
+        public ThDCLDataSetFactory()
+        {
+        }
+        protected override ThMEPDataSet BuildDataSet()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void GetElements(Database database, Point3dCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
