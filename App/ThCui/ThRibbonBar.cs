ad881dd0444ad2de1abf5cc7b20971d51bd9ab10
董士崇 <dongshichong@thape.com.cn>
2021-12-21 20:10:34 +0800

    Enable structure ribbon

--- App/ThCui/ThRibbonBar.cs
+++ App/ThCui/ThRibbonBar.cs
@@ -15,7 +15,7 @@ namespace TianHua.AutoCAD.ThCui
                 CreateWSSPanel(tab);
                 CreateHVACPanel(tab);
                 CreateElectricPanel(tab);
-                //CreateStructurePanel(tab);
+                CreateStructurePanel(tab);
                 CreateArchitecturePanel(tab);
             }
         }
@@ -93,19 +93,19 @@ namespace TianHua.AutoCAD.ThCui
                     RibbonButtonStyle.LargeWithText);
             }
 
-            //{
-            //    var subPanel = row.AddNewPanel();
+            {
+                var subPanel = row.AddNewPanel();
 
-            //    // 地库风平面
-            //    var subRow = subPanel.AddNewRibbonRow();
-            //    subRow.AddNewButton("地库风平面",
-            //        "天华地库风平面",
-            //        "THDKFPM",
-            //        "天华地库风平面",
-            //        "IDI_THCAD_THDKFPM_SMALL",
-            //        "IDI_THCAD_THDKFPM_LARGE",
-            //        RibbonButtonStyle.LargeWithText);
-            //}
+                // 地库风平面
+                var subRow = subPanel.AddNewRibbonRow();
+                subRow.AddNewButton("地库风平面",
+                    "天华地库风平面",
+                    "THDKFPM",
+                    "天华地库风平面",
+                    "IDI_THCAD_THDKFPM_SMALL",
+                    "IDI_THCAD_THDKFPM_LARGE",
+                    RibbonButtonStyle.LargeWithText);
+            }
 
             {
                 var subPanel = row.AddNewPanel();
