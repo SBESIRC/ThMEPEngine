ed3fb60c5fb97bfa5b13df500cad59f5aecdd87f
刘光胜 <liuguangsheng@thape.com.cn>
2021-12-29 13:44:26 +0800

    implement ThFirstSecondFactory

---
+++ App/ThMEPLighting/Garage/Factory/ThFirstSecondFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using ThMEPLighting.Common;
+
+namespace ThMEPLighting.Garage.Factory
+{
+    public abstract class ThFirstSecondFactory
+    {
+        /// <summary>
+        /// 记录边线是1号线，还是2号线
+        /// </summary>
+        protected Dictionary<Line, EdgePattern> SideLineNumberDict { get; set; }
+        public Dictionary<Line, Tuple<List<Line>, List<Line>>> CenterSideDict { get; private set; }
+        public List<Line> FirstLines
+        {
+            get
+            {
+                return SideLineNumberDict.Where(o => o.Value == EdgePattern.First).Select(o => o.Key).ToList();
+            }
+        }
+        public List<Line> SecondLines
+        {
+            get
+            {
+                return SideLineNumberDict.Where(o => o.Value == EdgePattern.Second).Select(o => o.Key).ToList();
+            }
+        }
+        public abstract void Produce();
+    }
+}
