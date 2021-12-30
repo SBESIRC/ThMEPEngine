66bdad17fa845507b1ee6b88b8a179e7ac2fda36
刘光胜 <liuguangsheng@thape.com.cn>
2021-12-30 10:50:17 +0800

    define ThFirstwayArrangeService,ThSecondwayArrangeService

---
+++ App/ThMEPLighting/Garage/Service/Arrange/ThSecondwayArrangeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThMEPLighting.Garage.Service.Arrange
+{
+    public class ThSecondwayArrangeService : ThArrangeService
+    {
+        public override void Arrange()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
