bb785529fe6be05398aef67a31f43b2f6b63dc11
刘光胜 <liuguangsheng@thape.com.cn>
2021-12-31 17:13:04 +0800

    implement three class

---
+++ App/ThMEPLighting/Garage/Service/Arrange/ThLightEdgeDistributePointService.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Dreambuild.AutoCAD;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.DatabaseServices;
+using ThMEPLighting.Common;
+using ThMEPLighting.Garage.Model;
+using ThMEPLighting.Garage.Service.LayoutPoint;
+
+namespace ThMEPLighting.Garage.Service.Arrange
+{
+    /// <summary>
+    /// 给1、2号灯线的边布点
+    /// </summary>
+    internal class ThLightEdgeDistributePointService
+    {
+        #region ---------- input ----------
+        public List<Line> FirstLines { get; set; } = new List<Line>();
+        public List<Line> SecondLines { get; set; } = new List<Line>();
+        public DBObjectCollection Beams { get; set; } = new DBObjectCollection();
+        public DBObjectCollection Columns { get; set; } = new DBObjectCollection();
+        public ThLightArrangeParameter ArrangeParameter { get; set; } = new ThLightArrangeParameter();
+        #endregion
+        public ThLightEdgeDistributePointService()
+        {
+        }
+
+        public Tuple<List<ThLightEdge>, List<ThLightEdge>> Distribute()
+        {
+            // 创建边
+            var firstLightEdges = BuildEdges(FirstLines, EdgePattern.First);
+            var secondLightEdges = BuildEdges(SecondLines, EdgePattern.Second);
+
+            // 布点
+            var linePoints = new Dictionary<Line, List<Point3d>>();
+            var points = LayoutPoints();
+            linePoints = ThQueryPointService.Query(points, Union(FirstLines, SecondLines));
+            linePoints = Sort(linePoints);
+
+            // 优化布置的点
+            var optimizer = new ThLayoutPointOptimizeService(linePoints, FilterPointDistance);
+            optimizer.Optimize();
+
+            firstLightEdges.ForEach(f =>
+            {
+                linePoints[f.Edge].ForEach(p =>
+                {
+                    f.LightNodes.Add(new ThLightNode() { Position = p });
+                });
+            });
+            secondLightEdges.ForEach(f =>
+            {
+                linePoints[f.Edge].ForEach(p =>
+                {
+                    f.LightNodes.Add(new ThLightNode() { Position = p });
+                });
+            });
+            return Tuple.Create(firstLightEdges, secondLightEdges);
+        }
+
+        private List<Point3d> LayoutPoints()
+        {
+            // Curve 仅支持Line，和Line组成的多段线
+            var results = new List<Point3d>();
+            ThLayoutPointService layoutPointService = null;
+            switch (ArrangeParameter.LayoutMode)
+            {
+                case LayoutMode.AvoidBeam:
+                    layoutPointService = new ThAvoidBeamLayoutPointService(Beams);
+                    break;
+                case LayoutMode.ColumnSpan:
+                    layoutPointService = new ThColumnSpanLayoutPointService(Columns,
+                        ArrangeParameter.NearByDistance);
+                    break;
+                case LayoutMode.SpanBeam:
+                    layoutPointService = new ThSpanBeamLayoutPointService(Beams);
+                    break;
+                default:
+                    layoutPointService = new ThEqualDistanceLayoutPointService();
+                    break;
+            }
+            if (layoutPointService != null)
+            {
+                layoutPointService.Margin = ArrangeParameter.Margin;
+                layoutPointService.Interval = ArrangeParameter.Interval;
+                layoutPointService.LampLength = ArrangeParameter.LampLength;
+                layoutPointService.DoubleRowOffsetDis = ArrangeParameter.DoubleRowOffsetDis;
+                results = layoutPointService.Layout(FirstLines, SecondLines);
+            }
+            return results;
+        }
+
+        private Dictionary<Line, List<Point3d>> Sort(Dictionary<Line, List<Point3d>> linePoints)
+        {
+            var result = new Dictionary<Line, List<Point3d>>();
+            linePoints.ForEach(o =>
+            {
+                var pts = o.Value.OrderBy(p => p.DistanceTo(o.Key.StartPoint)).ToList();
+                result.Add(o.Key, pts);
+            });
+            return result;
+        }
+
+        private List<Line> Union(List<Line> firstLines, List<Line> secondLines)
+        {
+            var results = new List<Line>();
+            results.AddRange(firstLines);
+            results.AddRange(secondLines);
+            return results;
+        }
+        private List<ThLightEdge> BuildEdges(List<Line> lines, EdgePattern edgePattern)
+        {
+            var edges = new List<ThLightEdge>();
+            lines.ForEach(o => edges.Add(new ThLightEdge(o) { EdgePattern = edgePattern }));
+            return edges;
+        }
+        private double FilterPointDistance
+        {
+            get
+            {
+                return 0.4 * ArrangeParameter.Interval;
+            }
+        }
+    }
+}
---
+++ App/ThMEPLighting/Garage/Service/ThDirectionLightGraphService.cs
@@ -0,0 +1,173 @@
+﻿using System.Linq;
+using ThMEPEngineCore.CAD;
+using ThMEPLighting.Common;
+using Autodesk.AutoCAD.Geometry;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPLighting.Garage.Service
+{
+    /// <summary>
+    /// 根据方向来遍历,不支持支路
+    /// 用于车道线双排布置
+    /// </summary>
+    public class ThDirectionLightGraphService : ThLightGraphService
+    {
+        public List<ThLightGraphService> Graphs { get; private set; } // 当Model使用
+        public ThDirectionLightGraphService(List<ThLightEdge> edges) 
+            : base(edges,Point3d.Origin)
+        {
+            Graphs = new List<ThLightGraphService>();
+        }
+
+        public override void Build()
+        {
+            while(Edges.Count>0)
+            {
+                var first = Edges.First();
+                var links = new List<ThLightEdge> { first };
+                Find(links, first.EndPoint);
+                Find(links, first.StartPoint);
+                Update(links);
+
+                // 获取起点
+                var poly = ThGarageUtils.ToPolyline(links.Select(o => o.Edge).ToList(),
+                    ThGarageLightCommon.RepeatedPointDistance);
+
+                // 创建Model
+                var linkPath = new ThLinkPath
+                {
+                    Edges = links,
+                    IsMain = true,
+                    Start = poly.StartPoint,
+                    PreEdge = new ThLightEdge(),
+                };
+                var graph = new ThLightGraphService(links, poly.StartPoint);
+                graph.Links.Add(linkPath);
+                Graphs.Add(graph);
+
+                // 更新Edges 
+                Edges = Edges.Where(o => !o.IsTraversed).ToList();
+            }
+        }
+
+        /// <summary>
+        /// 查找相连的线
+        /// </summary>
+        /// <param name="links"></param>
+        /// <param name="start"></param>
+        private void FindPrev(List<ThLightEdge> links, Point3d portPt)
+        {
+            var portEdges = GetPortEdges(links, portPt);
+            if (portEdges.Count == 0)
+            {
+                return;
+            }
+            var neighbourEdge = FindNeighbourEdge(links[links.Count - 1], portEdges);
+            if (neighbourEdge == null)
+            {
+                return;
+            }
+            links.Insert(0,neighbourEdge);
+            FindPrev(links, neighbourEdge.StartPoint);
+        }
+
+        /// <summary>
+        /// 查找相连的线
+        /// </summary>
+        /// <param name="links"></param>
+        /// <param name="start"></param>
+        private void FindNext(List<ThLightEdge> links, Point3d portPt)
+        {
+            //当Degree为零，或碰到已遍历的边结束
+            var portEdges = GetPortEdges(links,portPt);
+            if (portEdges.Count == 0)
+            {
+                return;
+            }
+            var neighbourEdge = FindNeighbourEdge(links[links.Count - 1], portEdges);
+            if (neighbourEdge == null)
+            {
+                return;
+            }
+            links.Add(neighbourEdge);
+            FindNext(links, neighbourEdge.EndPoint);
+        }
+
+        private List<ThLightEdge> GetPortEdges(List<ThLightEdge> links, Point3d portPt)
+        {
+            //当Degree为零，或碰到已遍历的边结束
+            var portEdges = SearchEdges(portPt, ThGarageLightCommon.RepeatedPointDistance);
+            return portEdges
+                .Where(o => !links.Select(l => l.Id).Contains(o.Id))
+                .Where(o => !o.IsTraversed)
+                .ToList();
+        }
+
+        protected override ThLightEdge FindNeighbourEdge(ThLightEdge currentEdge, List<ThLightEdge> linkEdges)
+        {
+            // 先选择方向一致性的边
+            var dirConsistentEdges = linkEdges.Where(o => IsDirectionConsistent(currentEdge, o)).ToList();
+
+            // 筛选共线的边
+            var collinearEdges = dirConsistentEdges.Where(o => IsCollinear(currentEdge, o)).ToList();
+            if (collinearEdges.Count > 0)
+            {
+                return collinearEdges.OrderByDescending(o => o.Edge.Length).First();
+            }
+
+            // 获取不共线的边
+            var unCollinearEdges = dirConsistentEdges.Where(o => !IsCollinear(currentEdge, o)).ToList();
+            if (unCollinearEdges.Count == 1)
+            {
+                return unCollinearEdges[0];
+            }
+            else
+            {
+                // 对于多个分支，优先选择外角小于45度且夹角最小的通道
+                var angleRangedEdges = dirConsistentEdges
+                    .Where(o => IsLessThan45Degree(currentEdge, o))
+                    .OrderBy(o=> currentEdge.Direction.GetAngleTo(o.Direction))
+                    .ToList();
+                return angleRangedEdges.Count()>0? angleRangedEdges.First():null;
+            }
+        }
+
+        private void Update(List<ThLightEdge> edges)
+        {
+            edges.ForEach(o => o.IsTraversed = true);
+        }
+        private bool IsDirectionConsistent(ThLightEdge first, ThLightEdge second)
+        {
+            var firstSp = first.StartPoint;
+            var firstEp = first.EndPoint;
+
+            var secondSp = second.StartPoint;
+            var secondEp = second.EndPoint;
+
+            if (firstEp.DistanceTo(secondSp) <= ThGarageLightCommon.RepeatedPointDistance)
+            {
+                return true;
+            }
+            if (firstSp.DistanceTo(secondEp) <= ThGarageLightCommon.RepeatedPointDistance)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsCollinear(ThLightEdge first, ThLightEdge second)
+        {
+            return ThGeometryTool.IsCollinearEx(
+                  first.Edge.StartPoint, first.Edge.EndPoint,
+                  second.Edge.StartPoint, second.Edge.EndPoint);
+        }
+
+        private bool IsLessThan45Degree(ThLightEdge first, ThLightEdge second)
+        {
+            return ThGarageUtils.IsLessThan45Degree(
+                first.Edge.StartPoint, first.Edge.EndPoint,
+                second.Edge.StartPoint, second.Edge.EndPoint);
+        }
+    }
+}
