d8dd58136973b5d63223821928cff3291acc84cc
刘光胜 <liuguangsheng@thape.com.cn>
2022-01-08 14:26:58 +0800

    update ThBreakLineService

--- App/ThMEPLighting/Garage/Service/LayoutResult/ThBreakLineService.cs
+++ App/ThMEPLighting/Garage/Service/LayoutResult/ThBreakLineService.cs
@@ -1,4 +1,5 @@
-﻿using NFox.Cad;
+﻿using System;
+using NFox.Cad;
 using System.Linq;
 using ThCADCore.NTS;
 using ThCADExtension;
@@ -19,6 +20,7 @@ namespace ThMEPLighting.Garage.Service.LayoutResult
         private Matrix3d wcsToUcs;
         private double Length { get; set; } = 200.0;
         private Matrix3d WcsToUcs { get; set; }
+        private double TesslateLength = 100.0;
 
         public ThBreakLineService(Matrix3d currentUserCoordinateSystem, double length)
         {
@@ -31,43 +33,51 @@ namespace ThMEPLighting.Garage.Service.LayoutResult
             {
                 return wires;
             }
-            var results = new DBObjectCollection();
-            var breakLines = wires.OfType<Line>().Where(l => IsQualified(l.LineDirection())).ToList();
-            var otherLines = wires.OfType<Line>().Where(l => !breakLines.Contains(l)).ToCollection();
-            var spatialIndex = new ThCADCoreNTSSpatialIndex(otherLines);
-            breakLines.Where(l => l.Length > Length).ForEach(l =>
+            using (var ov = new ThCADCoreNTSArcTessellationLength(TesslateLength))
+            {
+                var results = new DBObjectCollection();
+                var breakLines = wires.OfType<Line>().Where(l => IsQualified(l.LineDirection())).ToList();
+                var otherCurves = wires
+                    .OfType<Curve>()
+                    .Where(o=>o is Line || o is Arc)
+                    .Where(l => !breakLines.Contains(l)).ToCollection();
+                var spatialIndex = new ThCADCoreNTSSpatialIndex(otherCurves);
+                breakLines.Where(l => l.Length > Length).ForEach(l =>
                 {
-                    var onLines = Query(l, spatialIndex);
-                    var res = Break(l, onLines, Length);
+                    var onCurves = Query(l, spatialIndex);
+                    var res = Break(l, onCurves, Length);
                     res.ForEach(o => results.Add(o));
                 });
-
-            results = results.Union(otherLines);
-            return results;
+                results = results.Union(otherCurves);
+                return results;
+            }
         }
-        private List<Line> Break(Line line,DBObjectCollection onLines,double length)
+
+        private List<Line> Break(Line line,DBObjectCollection onCurves,double length)
         {
             var overlapLines = new List<Line>();
-            onLines.OfType<Line>().ForEach(e =>
+            var direction = line.StartPoint.GetVectorTo(line.EndPoint).GetNormal();
+            onCurves.OfType<Curve>().ForEach(e =>
             {
-                var inters = GetValidIntersPt(line, e);
-                if (inters.HasValue)
+                var inters = GetIntersPtNotOnPort(line, e);
+                inters.OfType<Point3d>().ForEach(p =>
                 {
-                   var overlapLine = CalculateSplitLine(line, inters.Value, length);
-                    if(overlapLine.Length>1e-6)
+                    var ptPair = BuildBreakLineSP(p, direction, length);
+                    if(IsInLine(ptPair.Item1,line,1.0) && IsInLine(ptPair.Item2, line, 1.0))
                     {
-                        overlapLines.Add(overlapLine);
+                        overlapLines.Add(new Line(ptPair.Item1, ptPair.Item2));
                     }
-                }
+                });
             });
-
-            if(overlapLines.Count==0)
+            if (overlapLines.Count==0)
             {
                 return new List<Line> { line };
             }
             else
             {
-                return line.Difference(overlapLines);
+                var results = line.Difference(overlapLines).Where(o => o.Length > 1.0).ToList();
+                overlapLines.ForEach(o => o.Dispose());
+                return results;
             }
         }
 
@@ -77,48 +87,45 @@ namespace ThMEPLighting.Garage.Service.LayoutResult
             var ang = newVec.GetAngleTo(Vector3d.XAxis).RadToAng();
             return (ang >= 45 && ang <= 135) || (ang >= 225 && ang <= 315);
         }
-        private Line CalculateSplitLine(Line line,Point3d pt, double length)
+
+        private bool IsInLine(Point3d pt,Line line,double tolerance=0.0)
         {
-            var vec = line.LineDirection();
-            var pt1 = pt - vec.MultiplyBy(length / 2.0);
-            var pt2 = pt + vec.MultiplyBy(length / 2.0);
-            if(ThGeometryTool.IsPointInLine(line.StartPoint, line.EndPoint, pt1,1.0) &&
-                ThGeometryTool.IsPointInLine(line.StartPoint, line.EndPoint, pt2, 1.0))
-            {
-                return new Line(pt1, pt2);
-            }  
-            else
-            {
-                return new Line();
-            }
+            return ThGeometryTool.IsPointInLine(line.StartPoint, line.EndPoint, pt, tolerance);
         }
-        private Point3d? GetValidIntersPt(Line first,Line second)
+
+        private Tuple<Point3d,Point3d> BuildBreakLineSP(Point3d pt,Vector3d direction, double length)
+        {
+            // direction is normal
+            var pt1 = pt - direction.MultiplyBy(length / 2.0);
+            var pt2 = pt + direction.MultiplyBy(length / 2.0);
+            return Tuple.Create(pt1, pt2);
+        }
+
+        private Point3dCollection GetIntersPtNotOnPort(Curve first, Curve second)
         {
             var inters = first.IntersectWithEx(second);
             if(inters.Count==0)
             {
-                return null;
+                return new Point3dCollection();
             }
             var ports = new List<Point3d>() { first.StartPoint, first.EndPoint, 
                 second.StartPoint, second.EndPoint };
-            if(IsCloseToPorts(inters[0], ports))
-            {
-                return null;
-            }
-            return inters[0];
+            return inters
+                .OfType<Point3d>()
+                .Where(o => !IsCloseToPorts(o, ports, ThGarageLightCommon.RepeatedPointDistance))
+                .ToCollection();
         }
-        private bool IsCloseToPorts(Point3d intersPt,List<Point3d> portPts)
+
+        private bool IsCloseToPorts(Point3d intersPt,List<Point3d> portPts,double tolerance)
         {
-            return portPts.Where(p => intersPt.DistanceTo(p) <= ThGarageLightCommon.RepeatedPointDistance).Any();
+            return portPts.Where(p => intersPt.DistanceTo(p) <= tolerance).Any();
         }
         private DBObjectCollection Query(Line line, ThCADCoreNTSSpatialIndex spatialIndex)
         {
             var rec = ThDrawTool.ToRectangle(line.StartPoint, line.EndPoint, ThGarageLightCommon.RepeatedPointDistance);
             var objs = spatialIndex.SelectCrossingPolygon(rec);
-            return objs
-                .OfType<Line>()
-                .Where(l => !ThGeometryTool.IsCollinearEx(line.StartPoint, line.EndPoint, l.StartPoint, l.EndPoint))
-                .ToCollection();
+            objs.Remove(line);
+            return objs;
         }
     }
 }
