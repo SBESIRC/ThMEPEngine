6e9ca8f9f36fee0f5e45a3e47de389a2eb0c1128
董士崇 <dongshichong@thape.com.cn>
2022-01-13 09:40:18 +0800

    Revert "Polyline.ToNTSLineString validate with WashClone() (#1529)"
    This reverts commit 02e5f4c125b75dec1a82b8d98eae340ea8a44e67.

--- App/ThCADCore/NTS/ThCADCoreNTSDbExtension.cs
+++ App/ThCADCore/NTS/ThCADCoreNTSDbExtension.cs
@@ -171,73 +171,71 @@ namespace ThCADCore.NTS
             return objs;
         }
 
+        private static bool IsClosed(Polyline frame, double tolerance)
+        {
+            // 支持真实闭合或视觉闭合
+            return frame.Closed || (frame.StartPoint.DistanceTo(frame.EndPoint) <= tolerance);
+        }
+
         public static LineString ToNTSLineString(this Polyline poly)
         {
-            using(var clone = poly.WashClone() as Polyline)
+            var points = new List<Coordinate>();
+            var arcLength = ThCADCoreNTSService.Instance.ArcTessellationLength;
+            var polyLine = poly.HasBulges ? poly.TessellatePolylineWithArc(arcLength) : poly;
+            for (int i = 0; i < polyLine.NumberOfVertices; i++)
             {
-                if (clone == null)
+                points.Add(polyLine.GetPoint3dAt(i).ToNTSCoordinate());
+            }
+
+            // 支持真实闭合或视觉闭合
+            // 对于处于“闭合”状态的多段线，要保证其首尾点一致
+            if (points[0].Equals2D(points[points.Count - 1], ThCADCoreNTSService.Instance.AcadGlobalTolerance))
+            {
+                points.RemoveAt(points.Count - 1);
+                points.Add(points[0]);
+            }
+            else
+            {
+                if (polyLine.Closed)
+                {
+                    points.Add(points[0]);
+                }
+            }
+
+            if (points[0].Equals(points[points.Count - 1]))
+            {
+                // 三个点，其中起点和终点重合
+                // 多段线退化成一根线段
+                if (points.Count == 3)
+                {
+                    return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString(points.ToArray());
+                }
+
+                // 二个点，其中起点和终点重合
+                // 多段线退化成一个点
+                if (points.Count == 2)
                 {
                     return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString();
                 }
 
-                var points = new List<Coordinate>();
-                var arcLength = ThCADCoreNTSService.Instance.ArcTessellationLength;
-                var polyLine = poly.HasBulges ? poly.TessellatePolylineWithArc(arcLength) : poly;
-                for (int i = 0; i < polyLine.NumberOfVertices; i++)
+                // 一个点
+                // 多段线退化成一个点
+                if (points.Count == 1)
                 {
-                    points.Add(polyLine.GetPoint3dAt(i).ToNTSCoordinate());
+                    return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString();
                 }
 
-                // 支持真实闭合或视觉闭合
-                // 对于处于“闭合”状态的多段线，要保证其首尾点一致
-                if (points[0].Equals2D(points[points.Count - 1], ThCADCoreNTSService.Instance.AcadGlobalTolerance))
-                {
-                    points.RemoveAt(points.Count - 1);
-                    points.Add(points[0]);
-                }
-                else
-                {
-                    if (polyLine.Closed)
-                    {
-                        points.Add(points[0]);
-                    }
-                }
-
-                if (points[0].Equals(points[points.Count - 1]))
-                {
-                    // 三个点，其中起点和终点重合
-                    // 多段线退化成一根线段
-                    if (points.Count == 3)
-                    {
-                        return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString(points.ToArray());
-                    }
-
-                    // 二个点，其中起点和终点重合
-                    // 多段线退化成一个点
-                    if (points.Count == 2)
-                    {
-                        return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString();
-                    }
-
-                    // 一个点
-                    // 多段线退化成一个点
-                    if (points.Count == 1)
-                    {
-                        return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString();
-                    }
-
-                    // 首尾端点一致的情况
-                    // LinearRings are the fundamental building block for Polygons.
-                    // LinearRings may not be degenerate; that is, a LinearRing must have at least 3 points.
-                    // Other non-degeneracy criteria are implied by the requirement that LinearRings be simple. 
-                    // For instance, not all the points may be collinear, and the ring may not self - intersect.
-                    return ThCADCoreNTSService.Instance.GeometryFactory.CreateLinearRing(points.ToArray());
-                }
-                else
-                {
-                    // 首尾端点不一致的情况
-                    return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString(points.ToArray());
-                }
+                // 首尾端点一致的情况
+                // LinearRings are the fundamental building block for Polygons.
+                // LinearRings may not be degenerate; that is, a LinearRing must have at least 3 points.
+                // Other non-degeneracy criteria are implied by the requirement that LinearRings be simple. 
+                // For instance, not all the points may be collinear, and the ring may not self - intersect.
+                return ThCADCoreNTSService.Instance.GeometryFactory.CreateLinearRing(points.ToArray());
+            }
+            else
+            {
+                // 首尾端点不一致的情况
+                return ThCADCoreNTSService.Instance.GeometryFactory.CreateLineString(points.ToArray());
             }
         }
 
