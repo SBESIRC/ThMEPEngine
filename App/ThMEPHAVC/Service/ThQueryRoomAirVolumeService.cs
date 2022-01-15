9c556a2c0899e750a28a1e2c456086a2de7f312e
jackwhoknow <trasure@126.com>
2022-01-15 12:16:12 +0800

    fixed cant' read table data if table position not in room (#1556)
    Co-authored-by: 刘光胜 <liuguangsheng@thape.com.cn>

--- App/ThMEPHAVC/Service/ThQueryRoomAirVolumeService.cs
+++ App/ThMEPHAVC/Service/ThQueryRoomAirVolumeService.cs
@@ -2,11 +2,8 @@
 using System.Linq;
 using NFox.Cad;
 using Linq2Acad;
-using ThCADCore.NTS;
-using ThCADExtension;
 using Autodesk.AutoCAD.DatabaseServices;
 using ThMEPEngineCore.Model;
-using ThMEPEngineCore.Algorithm;
 using ThMEPHVAC.LoadCalculation.Model;
 
 namespace ThMEPHVAC.Service
@@ -15,23 +12,11 @@ namespace ThMEPHVAC.Service
     {
         private DBObjectCollection Tables { get; set; }
         private DBObjectCollection MarkLines { get; set; }
-        private ThMEPOriginTransformer Transformer { get; set; }
-        private ThCADCoreNTSSpatialIndex TableSpatialIndex { get; set; }
-        private ThCADCoreNTSSpatialIndex MarkLineSpatialIndex { get; set; }
-
         public ThQueryRoomAirVolumeService()
         {
             // 建议支持一次性对单个房间查询
-            Init();
-        }
-
-        private void Init()
-        {
             Tables = GetTables(AcHelper.Active.Database);
             MarkLines = GetMarkLines(AcHelper.Active.Database);
-            BuildTransformer();
-            Transform();
-            BuildSpatialIndex();
         }
 
         public object Query(ThIfcRoom room,string keyWord)
@@ -41,13 +26,9 @@ namespace ThMEPHVAC.Service
             {
                 return null;
             }
-            Transformer.Transform(room.Boundary);
-            var tables = GetTables(room.Boundary); // 获取房间内的风量表
-            var marklines = GetMarkLines(room.Boundary);// 获取房间内的线
             var table = room.GetLoadCalculationTable(
-                marklines.OfType<Curve>().ToList(),
-                tables.OfType<Table>().ToList());
-            Transformer.Reset(room.Boundary);
+                MarkLines.OfType<Curve>().ToList(),
+                Tables.OfType<Table>().ToList());
             if (table != null)
             {
                 var cellPos = Read(table, keyWord);
@@ -110,20 +91,6 @@ namespace ThMEPHVAC.Service
             return null;
         }
 
-        private void BuildTransformer()
-        {
-            var objs = new DBObjectCollection();
-            objs = objs.Union(Tables);
-            objs = objs.Union(MarkLines);
-            Transformer = new ThMEPOriginTransformer(objs);
-        }
-
-        private void BuildSpatialIndex()
-        {
-            TableSpatialIndex = new ThCADCoreNTSSpatialIndex(Tables);
-            MarkLineSpatialIndex = new ThCADCoreNTSSpatialIndex(MarkLines);
-        }
-
         private DBObjectCollection GetTables(Database database)
         {
             using (var acadDb = AcadDatabase.Use(database))
@@ -148,27 +115,6 @@ namespace ThMEPHVAC.Service
                 .Select(o=>o.Clone() as Curve)
                 .ToCollection();
             }
-        }
-
-        private DBObjectCollection GetMarkLines(Entity room)
-        {
-            return MarkLineSpatialIndex.SelectCrossingPolygon(room);
-        }
-        private DBObjectCollection GetTables(Entity room)
-        {
-            return TableSpatialIndex.SelectCrossingPolygon(room);
-        }
-
-        private void Transform()
-        {
-            Transformer.Transform(MarkLines);
-            Transformer.Transform(Tables);
-        }        
-
-        private void Reset()
-        {
-            Transformer.Reset(Tables);
-            Transformer.Reset(MarkLines);
         }
     }
 }
