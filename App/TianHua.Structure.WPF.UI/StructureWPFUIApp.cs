f76c25084bba5f77fcbf1f329f2374befeb9372a
董士崇 <dongshichong@thape.com.cn>
2022-01-19 17:30:53 +0800

    Setup Structure UI project

---
+++ App/TianHua.Structure.WPF.UI/StructureWPFUIApp.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace TianHua.Structure.WPF.UI
+{
+    public class StructureWPFUIApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
