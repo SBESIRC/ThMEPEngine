5de76f3b68761d92f69e505d48f33129a248a752
liupan-AI <1750923@tongji.edu.cn>
2022-02-10 11:12:37 +0800

    Code ThPDSCircuit and ThPDSLoad class (#1684)
    Co-authored-by: 刘潘 <liupan1@thape.com.cn>

---
+++ App/TianHua.Electrical.PDS/Model/ThPDSCircuit.cs
@@ -0,0 +1,121 @@
+﻿using Autodesk.AutoCAD.Geometry;
+
+namespace TianHua.Electrical.PDS.Model
+{
+    /// <summary>
+    /// 回路类型
+    /// </summary>
+    public enum ThPDSCircuitType
+    {
+        /// <summary>
+        /// 照明
+        /// </summary>
+        Lighting,
+
+        /// <summary>
+        /// 动力
+        /// </summary>
+        PowerEquipment,
+
+        /// <summary>
+        /// 应急照明
+        /// </summary>
+        EmergencyLighting,
+
+        /// <summary>
+        /// 消防动力
+        /// </summary>
+        EmergencyPowerEquipment,
+
+        /// <summary>
+        /// 消防应急照明
+        /// </summary>
+        FireEmergencyLighting,
+
+        /// <summary>
+        /// 控制
+        /// </summary>
+        Control
+    }
+
+    /// <summary>
+    /// 回路
+    /// </summary>
+    public class ThPDSCircuit
+    {
+        /// <summary>
+        /// 回路ID
+        /// </summary>
+        public string UID { get; set; }
+
+        /// <summary>
+        /// 回路编号
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// 回路类型
+        /// </summary>
+        public ThPDSCircuitType Type { get; set; }
+
+        /// <summary>
+        /// 额定电压
+        /// </summary>
+        public double KV { get; set; }
+
+        /// <summary>
+        /// 上级配电箱编号
+        /// </summary>
+        public string SourcePanelID { get; set; }
+
+        /// <summary>
+        /// 负载编号
+        /// </summary>
+        public string LoadID { get; set; }
+
+        /// <summary>
+        /// 用户自定义描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 安装功率
+        /// </summary>
+        public double InstalledCapacity { get; set; }
+
+        /// <summary>
+        /// 相数
+        /// </summary>
+        public double Phase { get; set; }
+
+        /// <summary>
+        /// 需要系数
+        /// </summary>
+        public double DemandFactor { get; set; }
+
+        /// <summary>
+        /// 功率因数
+        /// </summary>
+        public double PowerFactor { get; set; }
+
+        /// <summary>
+        /// 所属DWG
+        /// </summary>
+        public string ReferenceDWG { get; set; }
+
+        /// <summary>
+        /// 楼层
+        /// </summary>
+        public int FloorNumber { get; set; }
+
+        /// <summary>
+        /// 房间
+        /// </summary>
+        public string RoomType { get; set; }
+
+        /// <summary>
+        /// 基点坐标
+        /// </summary>
+        public Point3d BasePoint { get; set; }
+    }
+}
---
+++ App/TianHua.Electrical.PDS/Model/ThPDSLoad.cs
@@ -0,0 +1,100 @@
+﻿using Autodesk.AutoCAD.Geometry;
+
+namespace TianHua.Electrical.PDS.Model
+{
+    /// <summary>
+    /// 负载
+    /// </summary>
+    public class ThPDSLoad
+    {
+        /// <summary>
+        /// 负载编号
+        /// </summary>
+        public string LoadID { get; set; }
+
+        /// <summary>
+        /// 额定电压
+        /// </summary>
+        public double KV { get; set; }
+
+        /// <summary>
+        /// 负载类型
+        /// </summary>
+        public string LoadType { get; set; }
+
+        /// <summary>
+        /// 是否是消防设备
+        /// </summary>
+        public string FireLoad { get; set; }
+
+        /// <summary>
+        /// 上级配电箱编号
+        /// </summary>
+        public string SourcePanelID { get; set; }
+
+        /// <summary>
+        /// 回路ID
+        /// </summary>
+        public string CircuitID { get; set; }
+
+        /// <summary>
+        /// 回路编号
+        /// </summary>
+        public string CircuitNumber { get; set; }
+
+        /// <summary>
+        /// 用户自定义描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 主用设备数量
+        /// </summary>
+        public int PrimaryAvail { get; set; }
+
+        /// <summary>
+        /// 备用设备数量
+        /// </summary>
+        public int SpareAvail { get; set; }
+
+        /// <summary>
+        /// 安装功率
+        /// </summary>
+        public double InstalledCapacity { get; set; }
+
+        /// <summary>
+        /// 相数
+        /// </summary>
+        public double Phase { get; set; }
+
+        /// <summary>
+        /// 需要系数
+        /// </summary>
+        public double DemandFactor { get; set; }
+
+        /// <summary>
+        /// 功率因数
+        /// </summary>
+        public double PowerFactor { get; set; }
+
+        /// <summary>
+        /// 所属DWG
+        /// </summary>
+        public string ReferenceDWG { get; set; }
+
+        /// <summary>
+        /// 楼层
+        /// </summary>
+        public int FloorNumber { get; set; }
+
+        /// <summary>
+        /// 房间
+        /// </summary>
+        public string RoomType { get; set; }
+
+        /// <summary>
+        /// 基点坐标
+        /// </summary>
+        public Point3d BasePoint { get; set; }
+    }
+}
