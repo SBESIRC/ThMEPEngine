8c8719e414b879b5f5301fe46c8c0c41f04f7d44
董士崇 <dongshichong@thape.com.cn>
2022-02-11 13:11:43 +0800

    Implement ThPDSCircuitGraph (#1695)

---
+++ App/TianHua.Electrical.PDS/Model/ThPDSCircuitGraph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using QuickGraph;
+
+namespace TianHua.Electrical.PDS.Model
+{
+    public class ThPDSCircuitGraphNode : IEquatable<ThPDSCircuitGraphNode>
+    {
+        public bool Equals(ThPDSCircuitGraphNode other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ThPDSCircuitGraphEdge<T> : Edge<T> where T : ThPDSCircuitGraphNode
+    {
+        public ThPDSCircuitGraphEdge(T source, T target) : base(source, target)
+        {
+            //
+        }
+    }
+
+    public class ThPDSCircuitGraph
+    {
+        private AdjacencyGraph<ThPDSCircuitGraphNode, ThPDSCircuitGraphEdge<ThPDSCircuitGraphNode>> Graph { get; set; }
+    }
+}
