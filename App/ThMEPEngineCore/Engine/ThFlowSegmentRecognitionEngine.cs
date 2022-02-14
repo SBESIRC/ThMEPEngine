b9df48ca4c3a0d0cc10faad81068c07d95d9801a
董士崇 <dongshichong@thape.com.cn>
2022-02-14 13:25:53 +0800

    Define ThFlowSegmentRecognitionEngine

---
+++ App/ThMEPEngineCore/Engine/ThFlowSegmentRecognitionEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ThMEPEngineCore.Model;
+using Autodesk.AutoCAD.Geometry;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPEngineCore.Engine
+{
+    public abstract class ThFlowSegmentRecognitionEngine : IDisposable
+    {
+        public List<ThIfcFlowSegment> Elements { get; set; }
+        public ThFlowSegmentRecognitionEngine()
+        {
+            Elements = new List<ThIfcFlowSegment>();
+        }
+
+        public void Dispose()
+        {
+            //
+        }
+
+        public abstract void Recognize(Database database, Point3dCollection polygon);
+
+        public abstract void RecognizeMS(Database database, Point3dCollection polygon);
+
+        public abstract void Recognize(List<ThRawIfcAnnotationElementData> datas, Point3dCollection polygon);
+    }
+}
