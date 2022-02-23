83fc631c0ddc434f6396bd61425f16ad13101660
刘光胜 <liuguangsheng@thape.com.cn>
2022-02-23 15:33:07 +0800

    add five models

---
+++ App/ThMEPStructure/HuaRunPeiJin/Model/ThRectangleEdgeComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace ThMEPStructure.HuaRunPeiJin.Model
+{
+    public class ThRectangleEdgeComponent: ThEdgeComponent
+    {
+        public double Bw { get; set; }
+        public double Hc { get; set; }        
+        
+        /// <summary>
+        /// 拉筋
+        /// 参见"2021-ZZ-技术-总部-JG-CKTJ.pdf Page65",对应数字2
+        /// </summary>
+        public string Link2 { get; set; }
+        /// <summary>
+        /// 拉筋
+        /// 参见"2021-ZZ-技术-总部-JG-CKTJ.pdf Page65",对应数字3
+        /// </summary>
+        public string Link3 { get; set; }
+    }
+}
---
+++ App/ThMEPStructure/HuaRunPeiJin/Model/ThWallColumnReinforceSet.cs
@@ -0,0 +1,43 @@
+﻿namespace ThMEPStructure.HuaRunPeiJin.Model
+{
+    public class ThWallColumnReinforceSet
+    {
+        /// <summary>
+        /// 砼强度等级
+        /// </summary>
+        public string ConcreteStrengthGrade { get; set; }
+        /// <summary>
+        /// 抗震等级
+        /// </summary>
+        public string AntiSeismicGrade { get; set; }
+        /// <summary>
+        /// 保护层厚度
+        /// </summary>
+        public double C { get; set; }
+        /// <summary>
+        /// 自适应主表
+        /// 取值为：A0、A1、A2...
+        /// </summary>
+        public string Frame { get; set; }
+        /// <summary>
+        /// 字符行高
+        /// </summary>
+        public double TableRowHeight { get; set; }
+        /// <summary>
+        /// 墙柱标高
+        /// </summary>
+        public string Elevation { get; set; }
+        /// <summary>
+        /// 绘图比例
+        /// </summary>
+        public string DrawScale { get; set; }
+        /// <summary>
+        /// 点筋线宽
+        /// </summary>
+        public double PointReinforceLineWeight { get; set; }
+        /// <summary>
+        /// 箍线线宽
+        /// </summary>
+        public double StirrupLineWeight { get; set; }
+    }
+}
