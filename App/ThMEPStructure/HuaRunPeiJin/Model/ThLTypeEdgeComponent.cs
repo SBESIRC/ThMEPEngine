12fe24b1331a444bf47784135d1408204accee1f
刘光胜 <liuguangsheng@thape.com.cn>
2022-02-23 15:33:07 +0800

    add five models

---
+++ App/ThMEPStructure/HuaRunPeiJin/Model/ThLTypeEdgeComponent.cs
@@ -0,0 +1,37 @@
+﻿namespace ThMEPStructure.HuaRunPeiJin.Model
+{
+    public class ThLTypeEdgeComponent:ThEdgeComponent
+    {
+        // 请参见"2021-ZZ-技术-总部-JG-CKTJ.pdf Page70"
+        public double Bw { get; set; }
+        public double Hc1 { get; set; }
+        public double Bf { get; set; }
+        public double Hc2 { get; set; }
+        /// <summary>
+        /// 拉筋
+        /// 对应标注图中的数字2
+        /// </summary>
+        public string Link2 { get; set; }
+        /// <summary>
+        /// 拉筋
+        /// 对应标注图中的数字3
+        /// </summary>
+        public string Link3 { get; set; }
+        /// <summary>
+        /// 拉筋
+        /// 对应标注图中的数字4
+        /// </summary>
+        public string Link4 { get; set; }
+    }
+    public class ThLTypeCalEdgeComponent: ThLTypeEdgeComponent
+    {
+        /// <summary>
+        /// 迭代步数
+        /// </summary>
+        public int X { get; set; }
+        /// <summary>
+        /// 迭代增大后的纵筋规格
+        /// </summary>
+        public string EnhancedReinforce { get; set; }
+    }
+}
