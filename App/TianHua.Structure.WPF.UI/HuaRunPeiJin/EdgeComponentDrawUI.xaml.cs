1e38d753e9cdbd4a08faa422475ad148d44c1a09
刘光胜 <liuguangsheng@thape.com.cn>
2022-02-24 19:37:22 +0800

    finish design component draw UI

---
+++ App/TianHua.Structure.WPF.UI/HuaRunPeiJin/EdgeComponentDrawUI.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using ThControlLibraryWPF.CustomControl;
+
+namespace TianHua.Structure.WPF.UI.HuaRunPeiJin
+{
+    /// <summary>
+    /// 边缘构件绘制交互逻辑
+    /// </summary>
+    public partial class EdgeComponentDrawUI : ThCustomWindow
+    {
+        public EdgeComponentDrawUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
