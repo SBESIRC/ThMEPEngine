3fbe6a8072a0e7706c8589e159e40659180b54a1
董士崇 <dongshichong@thape.com.cn>
2022-02-25 19:23:49 +0800

    Define ThDownConductorWireRecognitionEngine

---
+++ App/ThMEPElectrical/EarthingGrid/Model/ThDownConductorWire.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThMEPEngineCore.Model;
+
+namespace ThMEPElectrical.EarthingGrid.Model
+{
+    public class ThDownConductorWire : ThIfcFlowSegment
+    {
+    }
+}
