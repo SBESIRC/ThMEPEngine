bb7eb54174896c0529d60359dbf3aaad55d7ce83
董士崇 <dongshichong@thape.com.cn>
2022-03-05 18:13:02 +0800

    Define ThPDSProjectGraphService

---
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TianHua.Electrical.PDS.Project.Module.Circuit;
+
+namespace TianHua.Electrical.PDS.Project.Module
+{
+    public static class ThPDSProjectGraphService
+    {
+        public static void AddCircuit(ThPDSProjectGraph graph, PDSBaseCircuit circuit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void UpdateFormType(ThPDSProjectGraph graph, PDSBaseCircuit circuit, string formType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
