81592489c339473b14633334b533fef54b1e58bd
董士崇 <dongshichong@thape.com.cn>
2022-03-05 19:13:28 +0800

    Define methods for ThPDSProjectGraphService

--- App/TianHua.Electrical.PDS/Project/Module/ProjectEnumCollection.cs
+++ App/TianHua.Electrical.PDS/Project/Module/ProjectEnumCollection.cs
@@ -1,51 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace TianHua.Electrical.PDS.Project.Module
 {
-    ///// <summary>
-    ///// 出线回路类型
-    ///// </summary>
-    //public enum CircuitFormOutType
-    //{
-    //    常规,
-    //    漏电,
-    //    接触器控制,
-    //    热继电器保护,
-    //    配电计量_上海CT,
-    //    配电计量_上海直接表,
-    //    配电计量_CT表在前,
-    //    配电计量_直接表在前,
-    //    配电计量_CT表在后,
-    //    电动机_分立元件,
-    //    电动机_CPS,
-    //    电动机_分立元件星三角启动,
-    //    电动机_CPS星三角启动,
-    //    双速电动机_分立元件detailYY,
-    //    双速电动机_分立元件YY,
-    //    双速电动机_CPSdetailYY,
-    //    双速电动机_CPSYY,
-    //    消防应急照明回路WFEL,
-    //    SPD,
-    //    小母排,
-    //    None
-    //}
+    /// <summary>
+    /// 出线回路类型
+    /// </summary>
+    public enum CircuitFormOutType
+    {
+        常规,
+        漏电,
+        接触器控制,
+        热继电器保护,
+        配电计量_上海CT,
+        配电计量_上海直接表,
+        配电计量_CT表在前,
+        配电计量_直接表在前,
+        配电计量_CT表在后,
+        电动机_分立元件,
+        电动机_CPS,
+        电动机_分立元件星三角启动,
+        电动机_CPS星三角启动,
+        双速电动机_分立元件detailYY,
+        双速电动机_分立元件YY,
+        双速电动机_CPSdetailYY,
+        双速电动机_CPSYY,
+        消防应急照明回路WFEL,
+        SPD,
+        小母排,
+        None
+    }
 
-    ///// <summary>
-    ///// 进线回路类型
-    ///// </summary>
-    //public enum CircuitFormInType
-    //{
-    //    一路进线,
-    //    二路进线ATSE,
-    //    三路进线,
-    //    集中电源,
-    //    None
-    //}
+    /// <summary>
+    /// 进线回路类型
+    /// </summary>
+    public enum CircuitFormInType
+    {
+        [Description("None")]
+        None,
+        [Description("1路进线")]
+        一路进线,
+        [Description("2路进线ATSE")]
+        二路进线ATSE,
+        [Description("3路进线")]
+        三路进线,
+        [Description("集中电源")]
+        集中电源,
+    }
 
+    /// <summary>
+    /// 错误信息
+    /// </summary>
     public enum PDSProjectErrorType
     {
         Alarm = 0,//报警，需要用户去关注并确认掉
--- App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphService.cs
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphService.cs
@@ -1,16 +1,60 @@
 ﻿using System;
-using TianHua.Electrical.PDS.Project.Module.Circuit;
 
 namespace TianHua.Electrical.PDS.Project.Module
 {
     public static class ThPDSProjectGraphService
     {
-        public static void AddCircuit(ThPDSProjectGraph graph, PDSBaseCircuit circuit)
+        /// <summary>
+        /// 新建回路
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="circuit"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void AddCircuit(ThPDSProjectGraph graph, CircuitFormOutType type)
         {
             throw new NotImplementedException();
         }
 
-        public static void UpdateFormType(ThPDSProjectGraph graph, PDSBaseCircuit circuit, string formType)
+        /// <summary>
+        /// 切换进线形式
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="type"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void UpdateFormInType(ThPDSProjectGraph graph, CircuitFormInType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 切换回路样式
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="node"></param>
+        /// <param name="type"></param>
+        public static void SwitchFormOutType(ThPDSProjectGraph graph, ThPDSProjectGraphNode node, CircuitFormOutType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 锁定回路（解锁回路）
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="node"></param>
+        /// <param name="doLock"></param>
+        public static void Lock(ThPDSProjectGraph graph, ThPDSProjectGraphNode node, bool doLock)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 删除回路
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="node"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void Delete(ThPDSProjectGraph graph, ThPDSProjectGraphNode node)
         {
             throw new NotImplementedException();
         }
