03e17c13e81a7df96ed4b6405a32f0121398e335
CaoShusai <911479142@qq.com>
2022-03-11 18:36:41 +0800

    添加一字型纵筋不带C筋版本位置计算

---
+++ App/ThMEPStructure/Reinforcement/Draw/Helper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThMEPStructure.Reinforcement.Draw
+{
+    class Helper
+    {
+        /// <summary>
+        /// 将纵筋规格解析出每个不同大小的规格的钢筋各有多少个
+        /// </summary>
+        /// <param name="str"></param>
+        public void AnalyseZongJinStr(string str)
+        {
+
+        }
+
+        /// <summary>
+        /// 计算方差，平方的均值减去均值的平方
+        /// </summary>
+        /// <param name="numbers"></param>
+        public static double calVariance(List<double> numbers)
+        {
+            double squareSum = 0;
+            double sum = 0;
+            foreach(var num in numbers)
+            {
+                squareSum += num * num;
+                sum += num;
+            }
+
+            return (squareSum / numbers.Count) - (sum / numbers.Count) * (sum / numbers.Count);
+        }
+
+    }
+}
