62cc86e9631eb386da190a341dbab01ae4b6d352
胡思超 <543813567@qq.com>
2022-03-14 13:25:40 +0800

    outline1

--- App/ThMEPStructure/Reinforcement/Draw/DrawObjectTType.cs
+++ App/ThMEPStructure/Reinforcement/Draw/DrawObjectTType.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.AutoCAD.Geometry;
+using ThMEPStructure.Reinforcement.Model;
+using ThMEPEngineCore.CAD;
 
 namespace ThMEPStructure.Reinforcement.Draw
 {
-    class DrawObjectTType:DrawObjectBase
+    class DrawObjectTType : DrawObjectBase
     {
-        
+        ThTTypeEdgeComponent thTTypeEdgeComponent;
+        public override void DrawOutline(string drawingScale)
+        {
+            int scale = 100 / int.Parse(drawingScale.Substring(2));
+            var pts = new Point3dCollection
+            {
+                TableStartPt + new Vector3d(200, -1500, 0)* scale,
+                TableStartPt + new Vector3d(200, -1500 - thTTypeEdgeComponent.Bw, 0) * scale,
+                //TableStartPt + new Vector3d(200, -1500 - thTTypeEdgeComponent.Bw, 0),
+            }
+        }
     }
 }
