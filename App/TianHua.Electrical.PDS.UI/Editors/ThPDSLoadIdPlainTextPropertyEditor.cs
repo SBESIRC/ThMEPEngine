e011747e79dca9b14fb7c8941171f11e47f51f4a
董士崇 <dongshichong@thape.com.cn>
2022-03-25 13:22:44 +0800

    Implement ThPDSLoadIdPlainTextPropertyEditor (#1963)
    * Part 1
    
    * Implement ThPDSLoadIdPlainTextPropertyEditor

---
+++ App/TianHua.Electrical.PDS.UI/Editors/ThPDSLoadIdPlainTextPropertyEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using HandyControl.Controls;
+using TianHua.Electrical.PDS.UI.Project.Module.Component;
+
+namespace TianHua.Electrical.PDS.UI.Editors
+{
+    public class ThPDSLoadIdPlainTextPropertyEditor : PropertyEditorBase
+    {
+        public override FrameworkElement CreateElement(PropertyItem propertyItem) => new System.Windows.Controls.TextBox
+        {
+            IsReadOnly = GetIsReadOnly(propertyItem),
+        };
+
+        public override DependencyProperty GetDependencyProperty() => System.Windows.Controls.TextBox.TextProperty;
+
+        private bool GetIsReadOnly(PropertyItem propertyItem)
+        {
+            var model = propertyItem.Value as ThPDSCircuitModel;
+            if (model != null)
+            {
+                return !string.IsNullOrEmpty(model.LoadId);
+            }
+            else
+            {
+                return propertyItem.IsReadOnly;
+            }
+        }
+    }
+}
