fcbf96b978874ec58cf0114c0e96d434a38de9cd
董士崇 <dongshichong@thape.com.cn>
2022-04-04 10:46:06 +0800

    Use EquatableTaggedEdge (#2039)

--- App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraph.cs
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraph.cs
@@ -24,11 +24,11 @@ namespace TianHua.Electrical.PDS.Project.Module
         }
     }
 
-    public class ThPDSProjectGraphEdge<T> : EquatableEdge<T> where T : ThPDSProjectGraphNode
+    public class ThPDSProjectGraphEdge<T> : EquatableTaggedEdge<T, ThPDSProjectGraphEdgeTag> where T : ThPDSProjectGraphNode
     {
         public ThPDSCircuit Circuit { get; set; }
         public CircuitDetails Details { get; set; }
-        public ThPDSProjectGraphEdge(T source, T target) : base(source, target)
+        public ThPDSProjectGraphEdge(T source, T target) : base(source, target, null)
         {
             //
         }
