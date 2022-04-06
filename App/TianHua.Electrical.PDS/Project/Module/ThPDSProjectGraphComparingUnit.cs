7dc17a3c662a0ba42a64b2fedbe62e1046f092f6
董士崇 <dongshichong@thape.com.cn>
2022-04-06 20:40:54 +0800

    Implement ThPDSProjectGraphComparingUnit.NodeEquals and EdgeEquals

--- App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphComparingUnit.cs
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphComparingUnit.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using PDSGraph = QuikGraph.AdjacencyGraph<
+﻿using PDSGraph = QuikGraph.AdjacencyGraph<
     TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphNode,
     TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphEdge>;
 
@@ -8,5 +7,15 @@ namespace TianHua.Electrical.PDS.Project.Module
     public abstract class ThPDSProjectGraphComparingUnit
     {
         public abstract void DoCompare(PDSGraph source, PDSGraph target);
+
+        protected bool NodeEquals(ThPDSProjectGraphNode node, ThPDSProjectGraphNode other)
+        {
+            return node.Load.ID.LoadID == other.Load.ID.LoadID;
+        }
+
+        protected bool EdgeEquals(ThPDSProjectGraphEdge edge, ThPDSProjectGraphEdge other)
+        {
+            return edge.Circuit.ID.CircuitID.Equals(other.Circuit.ID.CircuitID);
+        }
     }
 }
