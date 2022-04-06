15a9fc604942aa2b67e3955bd1f663ee2dd31d5c
董士崇 <dongshichong@thape.com.cn>
2022-04-06 18:27:49 +0800

    Define ThPDSProjectGraphComparingUnit

---
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphComparingUnit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PDSGraph = QuikGraph.AdjacencyGraph<
+    TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphNode,
+    TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphEdge>;
+
+namespace TianHua.Electrical.PDS.Project.Module
+{
+    public abstract class ThPDSProjectGraphComparingUnit
+    {
+        public abstract void DoCompare(PDSGraph source, PDSGraph target);
+    }
+}
---
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphStructuralComparingUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PDSGraph = QuikGraph.AdjacencyGraph<
+    TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphNode,
+    TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphEdge>;
+
+namespace TianHua.Electrical.PDS.Project.Module
+{
+    /// <summary>
+    /// 结构分析比较器
+    /// </summary>
+    public class ThPDSProjectGraphStructuralComparingUnit : ThPDSProjectGraphComparingUnit
+    {
+        public override void DoCompare(PDSGraph source, PDSGraph target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
