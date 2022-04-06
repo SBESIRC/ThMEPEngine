e39bb3281d9bcc1b2a4ca6a91092abcf8b2ce88c
董士崇 <dongshichong@thape.com.cn>
2022-04-06 18:27:49 +0800

    Define ThPDSProjectGraphComparingUnit

--- App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphCompareService.cs
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphCompareService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using PDSGraph = QuikGraph.AdjacencyGraph<
     TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphNode, 
     TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphEdge>;
@@ -7,9 +8,21 @@ namespace TianHua.Electrical.PDS.Project.Module
 {
     public class ThPDSProjectGraphCompareService
     {
-        public PDSGraph Compare(PDSGraph source,PDSGraph target)
+        private readonly List<ThPDSProjectGraphComparingUnit> _units;
+
+        public ThPDSProjectGraphCompareService()
         {
-            throw new NotImplementedException();
+            _units = new List<ThPDSProjectGraphComparingUnit>()
+            {
+                new ThPDSProjectGraphStructuralComparingUnit(),
+                new ThPDSProjectGraphQuantitativeComparingUnit(),
+                new ThPDSProjectGraphQualitativeComparingUnit(),
+            };
+        }
+
+        public void Compare(PDSGraph source,PDSGraph target)
+        {
+            _units.ForEach(o => o.DoCompare(source, target));
         }
     }
 }
---
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphQualitativeComparingUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PDSGraph = QuikGraph.AdjacencyGraph<
+    TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphNode,
+    TianHua.Electrical.PDS.Project.Module.ThPDSProjectGraphEdge>;
+
+namespace TianHua.Electrical.PDS.Project.Module
+{
+    /// <summary>
+    /// 定性分析比较器
+    /// </summary>
+    public class ThPDSProjectGraphQualitativeComparingUnit : ThPDSProjectGraphComparingUnit
+    {
+        public override void DoCompare(PDSGraph source, PDSGraph target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
