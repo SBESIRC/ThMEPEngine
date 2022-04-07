547e17a229ff4bcb7d827f5eef39b229228851d6
董士崇 <dongshichong@thape.com.cn>
2022-04-07 15:02:50 +0800

    Implement ThPDSComponentBlockInfoService

---
+++ App/TianHua.Electrical.PDS.UI/Services/ThPDSComponentBlockInfoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TianHua.Electrical.PDS.UI.WpfServices;
+
+namespace TianHua.Electrical.PDS.UI.Services
+{
+    public static class ThPDSComponentBlockInfoService
+    {
+        public static bool IsRCBreaker(BlockInfo info)
+        {
+            return info.BlockName is "RCD";
+        }
+
+        public static bool IsBreaker(BlockInfo info)
+        {
+            return info.BlockName is "CircuitBreaker" or "RCD";
+        }
+    }
+}
