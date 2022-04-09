88e288fd635cf7e4c492f6ec998738232ca2de4d
董士崇 <dongshichong@thape.com.cn>
2022-04-09 16:30:44 +0800

    Implement ThPDSBreakerTypeDescriptionProvider

---
+++ App/TianHua.Electrical.PDS.UI/Helpers/ThPDSBreakerCustomTypeDescriptor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using TianHua.Electrical.PDS.UI.Project.Module.Component;
+
+namespace TianHua.Electrical.PDS.UI.Helpers
+{
+    public class ThPDSBreakerCustomTypeDescriptor : CustomTypeDescriptor
+    {
+        private readonly ThPDSBreakerExModel _breaker;
+
+        public ThPDSBreakerCustomTypeDescriptor(ICustomTypeDescriptor parent, object instance): base(parent)
+        {
+            _breaker = (ThPDSBreakerExModel)instance;
+        }
+
+        public override PropertyDescriptorCollection GetProperties()
+        {
+            return base.GetProperties();
+        }
+    }
+}
