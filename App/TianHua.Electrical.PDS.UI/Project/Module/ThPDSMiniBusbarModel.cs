50f45a3d794ae8181353b3dd101879f2efced288
董士崇 <dongshichong@thape.com.cn>
2022-04-13 13:44:38 +0800

    Implement ThPDSMiniBusbarModel

---
+++ App/TianHua.Electrical.PDS.UI/Project/Module/ThPDSMiniBusbarModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using ThControlLibraryWPF.ControlUtils;
+using TianHua.Electrical.PDS.Project.Module;
+
+namespace TianHua.Electrical.PDS.UI.Project.Module
+{
+    public class ThPDSMiniBusbarModel : NotifyPropertyChangedBase
+    {
+        private readonly MiniBusbar _miniBusbar;
+
+        public ThPDSMiniBusbarModel(MiniBusbar busbar)
+        {
+            _miniBusbar = busbar;
+        }
+
+        [DisplayName("功率")]
+        [Category("小母排参数")]
+        public double Power
+        {
+            get => _miniBusbar.Power;
+            set
+            {
+                _miniBusbar.Power = value;
+                OnPropertyChanged(nameof(Power));
+            }
+        }
+    }
+}
