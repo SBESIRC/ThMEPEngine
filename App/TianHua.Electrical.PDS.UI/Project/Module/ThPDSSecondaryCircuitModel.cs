38b3e5621c853a17e3370af1d25fa8a86f457ab4
董士崇 <dongshichong@thape.com.cn>
2022-04-14 13:59:31 +0800

    Update ThPDSSecondaryCircuitModel

--- App/TianHua.Electrical.PDS.UI/Project/Module/ThPDSSecondaryCircuitModel.cs
+++ App/TianHua.Electrical.PDS.UI/Project/Module/ThPDSSecondaryCircuitModel.cs
@@ -4,18 +4,22 @@ using TianHua.Electrical.PDS.Project.Module;
 
 namespace TianHua.Electrical.PDS.UI.Project.Module
 {
-    //二级回路跟控制回路是同一个东西
-    public class ThPDSSecondaryCircuitModel
+    /// <summary>
+    /// 二级回路（控制回路）
+    /// </summary>
+    public class ThPDSSecondaryCircuitModel : NotifyPropertyChangedBase
     {
         private readonly SecondaryCircuit _sc;
-
         public ThPDSSecondaryCircuitModel(SecondaryCircuit sc)
         {
             _sc = sc;
         }
+
+        [ReadOnly(true)]
         [Category("控制回路参数")]
         [DisplayName("回路编号")]
         public string CircuitID => _sc.CircuitID;
+
         [Category("控制回路参数")]
         [DisplayName("功能描述")]
         public string CircuitDescription => _sc.CircuitDescription;
