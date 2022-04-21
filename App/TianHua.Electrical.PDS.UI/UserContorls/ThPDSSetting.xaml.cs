340d8780f1162e34691634e538b32621ba4b7125
萌新 <fengzuowei@thape.com.cn>
2022-04-21 14:29:07 +0800

    1002705 (#2196)

--- App/TianHua.Electrical.PDS.UI/UserContorls/ThPDSSetting.xaml.cs
+++ App/TianHua.Electrical.PDS.UI/UserContorls/ThPDSSetting.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +27,28 @@ namespace TianHua.Electrical.PDS.UI.UserContorls
         public ThPDSSetting()
         {
             InitializeComponent();
+            Loaded += ThPDSSetting_Loaded;
+        }
+
+        Setting setting;
+        const string file = "THPDSCONFIG.JSON";
+        private void ThPDSSetting_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (File.Exists(file))
+                {
+                    setting = JsonConvert.DeserializeObject<Setting>(File.ReadAllText(file));
+                }
+            }
+            catch { }
+            setting ??= new();
+            DataContext = setting;
         }
 
         private void btnSaveClick(object sender, RoutedEventArgs e)
         {
+            File.WriteAllText(file, JsonConvert.SerializeObject(setting));
             Close();
         }
 
@@ -41,8 +62,36 @@ namespace TianHua.Electrical.PDS.UI.UserContorls
             using var dlg = new FolderBrowserDialog();
             if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-
+                setting.Path = dlg.SelectedPath;
             }
+        }
+        class Setting : ObservableObject
+        {
+            private string path;
+            private int interval = 10;
+            private bool individual;
+            private string dwgRatio = "1:100";
+            public string Path
+            {
+                get => path;
+                set => SetProperty(ref path, value);
+            }
+            public int Interval
+            {
+                get => interval;
+                set => SetProperty(ref interval, value);
+            }
+            public bool Individual
+            {
+                get => individual;
+                set => SetProperty(ref individual, value);
+            }
+            public string DwgRatio
+            {
+                get => dwgRatio;
+                set => SetProperty(ref dwgRatio, value);
+            }
+            public string[] DwgRatioItemsSouce { get; } = new string[] { "1:100", "1:150" };
         }
     }
 }
