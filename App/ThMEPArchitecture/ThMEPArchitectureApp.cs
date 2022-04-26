fec0c8da5e0b173c300c5a1930e52011e4a9127d
张文宣 <62444072+bobbyzwx@users.noreply.github.com>
2022-04-26 15:09:16 +0800

    RedirectAssembly (#2246)
    Co-authored-by: 张文宣 <zhangwenxuan@thape.com.cn>

--- App/ThMEPArchitecture/ThMEPArchitectureApp.cs
+++ App/ThMEPArchitecture/ThMEPArchitectureApp.cs
@@ -1,4 +1,6 @@
 ﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Reflection;
 
 namespace ThMEPArchitecture
 {
@@ -7,8 +9,30 @@ namespace ThMEPArchitecture
         public void Initialize()
         {
             //throw new System.NotImplementedException();
+            RedirectAssembly("System.Buffers", new Version(4, 0, 3, 0));
         }
+        // from TianHua.AutoCAD.ThCui.ThCuiAssemblyService
+        public void RedirectAssembly(string shortName, Version targetVersion)
+        {
+            Assembly handler(object sender, ResolveEventArgs args)
+            {
+                // 判断是否是指定的模块
+                var assemblyRef = new AssemblyName(args.Name);
+                if (assemblyRef.Name != shortName)
+                    return null;
 
+                // 指定版本
+                assemblyRef.Version = targetVersion;
+
+                // 释放事件handler
+                AppDomain.CurrentDomain.AssemblyResolve -= handler;
+
+                // 装载模块
+                return Assembly.Load(assemblyRef);
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += handler;
+        }
         public void Terminate()
         {
             //throw new System.NotImplementedException();
