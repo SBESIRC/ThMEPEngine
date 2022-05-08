47ce500bdd8802e48914c8951087dbe37fdbde5c
董士崇 <dongshichong@thape.com.cn>
2022-05-08 17:21:31 +0800

    Implement ThTCHArchWallRecognitionEngine

---
+++ App/ThMEPTCH/Engine/ThTCHArchWallRecognitionEngine.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using ThMEPEngineCore.Engine;
+using Autodesk.AutoCAD.DatabaseServices;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.Geometry;
+using ThMEPTCH.Model;
+
+namespace ThMEPTCH.Engine
+{
+    public class ThTCHArchWallExtractionEngine : ThBuildingElementExtractionEngine
+    {
+        public override void Extract(Database database)
+        {
+            var visitor = new ThTCHArchWallExtractionVisitor();
+            var extractor = new ThBuildingElementExtractor();
+            extractor.Accept(visitor);
+            extractor.Extract(database);
+            Results = new List<ThRawIfcBuildingElementData>();
+            Results.AddRange(visitor.Results);
+        }
+    }
+
+    public class ThTCHArchWallRecognitionEngine : ThBuildingElementRecognitionEngine
+    {
+        public override void Recognize(Database database, Point3dCollection polygon)
+        {
+            var engine = new ThTCHArchWallExtractionEngine();
+            engine.Extract(database);
+            Recognize(engine.Results, polygon);
+        }
+
+        public override void Recognize(List<ThRawIfcBuildingElementData> objs, Point3dCollection polygon)
+        {
+            Elements.AddRange(objs.Select(o => ThTCHWall.Create(o.Geometry)));
+        }
+    }
+}
