297b0cdfd879878df669a142812e58c3d9efadc6
董士崇 <dongshichong@thape.com.cn>
2022-05-21 17:34:45 +0800

    Set up ThMEPIFC project

---
+++ App/ThMEPIFC/ThMEPIFCApp.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace ThMEPIFC
+{
+    class ThMEPIFCApp : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            //add code to run when the ExtApp initializes. Here are a few examples:
+            //  Checking some host information like build #, a patch or a particular Arx/Dbx/Dll;
+            //  Creating/Opening some files to use in the whole life of the assembly, e.g. logs;
+            //  Adding some ribbon tabs, panels, and/or buttons, when necessary;
+            //  Loading some dependents explicitly which are not taken care of automatically;
+            //  Subscribing to some events which are important for the whole session;
+            //  Etc.
+        }
+
+        public void Terminate()
+        {
+            //add code to clean up things when the ExtApp terminates. For example:
+            //  Closing the log files;
+            //  Deleting the custom ribbon tabs/panels/buttons;
+            //  Unloading those dependents;
+            //  Un-subscribing to those events;
+            //  Etc.
+        }
+    }
+}
