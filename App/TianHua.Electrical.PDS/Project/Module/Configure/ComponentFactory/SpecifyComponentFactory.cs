c07267313d7ef568864f433e95e45b2c04104ccb
KomaHe <khthds@163.com>
2022-05-27 08:40:42 +0800

    fix bug (#2580)
    Co-authored-by: THAPE\tiankaihe <1204973192@qq.com>

--- App/TianHua.Electrical.PDS/Project/Module/Configure/ComponentFactory/SpecifyComponentFactory.cs
+++ App/TianHua.Electrical.PDS/Project/Module/Configure/ComponentFactory/SpecifyComponentFactory.cs
@@ -364,7 +364,7 @@ namespace TianHua.Electrical.PDS.Project.Module.Configure.ComponentFactory
 
         private Conductor CreatConductor(string config)
         {
-            return new Conductor(_conductorConfig, _highPower, _edge.Target.Load.Phase, _edge.Target.Load.CircuitType, _edge.Target.Load.LoadTypeCat_1, _edge.Target.Load.FireLoad, _edge.Circuit.ViaConduit, _edge.Circuit.ViaCableTray, _edge.Target.Load.Location.FloorNumber, _edge.Target.Load.CableLayingMethod1, _edge.Target.Load.CableLayingMethod2, _edge.Target.Load.LoadTypeCat_2 == ThPDSLoadTypeCat_2.ResidentialDistributionPanel);
+            return new Conductor(config, _highPower, _edge.Target.Load.Phase, _edge.Target.Load.CircuitType, _edge.Target.Load.LoadTypeCat_1, _edge.Target.Load.FireLoad, _edge.Circuit.ViaConduit, _edge.Circuit.ViaCableTray, _edge.Target.Load.Location.FloorNumber, _edge.Target.Load.CableLayingMethod1, _edge.Target.Load.CableLayingMethod2, _edge.Target.Load.LoadTypeCat_2 == ThPDSLoadTypeCat_2.ResidentialDistributionPanel);
         }
 
         /// <summary>
