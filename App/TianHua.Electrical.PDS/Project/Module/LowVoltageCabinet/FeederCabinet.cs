67a24a84c6c5b2ae10aaa427e958184f4b8897c4
KomaHe <khthds@163.com>
2022-05-31 18:22:10 +0800

    Feature/thpds substation (#2590)
    * Low Voltage Cabinet
    
    * commit
    
    Co-authored-by: THAPE\tiankaihe <1204973192@qq.com>

---
+++ App/TianHua.Electrical.PDS/Project/Module/LowVoltageCabinet/FeederCabinet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TianHua.Electrical.PDS.Project.Module.LowVoltageCabinet
+{
+    /// <summary>
+    /// 馈线柜
+    /// </summary>
+    public class FeederCabinet : PDSBaseLowVoltageCabinet
+    {
+        //馈线柜的柜子个数在[1,9]区间内，不同样式的柜子内含的规格是不一样的，具体逻辑待后续补充给
+
+        /// <summary>
+        /// 馈线柜
+        /// </summary>
+        public FeederCabinet()
+        {
+
+        }
+    }
+}
---
+++ App/TianHua.Electrical.PDS/Project/Module/LowVoltageCabinet/PDSBaseLowVoltageCabinet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TianHua.Electrical.PDS.Project.Module.LowVoltageCabinet
+{
+    /// <summary>
+    /// 低压柜
+    /// </summary>
+    public abstract class PDSBaseLowVoltageCabinet
+    {
+
+    }
+}
