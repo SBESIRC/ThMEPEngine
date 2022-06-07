4c2f82b7b40dfc6282b770f77c53f4611f87a864
KomaHe <khthds@163.com>
2022-06-07 14:19:33 +0800

    同步前后文的误差：不同步的情况下可能导致A*跑出来的连线异常 (#2632)
    Co-authored-by: THAPE\tiankaihe <1204973192@qq.com>

--- App/ThMEPEngineCore/Algorithm/AStarAlgorithm_New/GeometryHelper.cs
+++ App/ThMEPEngineCore/Algorithm/AStarAlgorithm_New/GeometryHelper.cs
@@ -14,10 +14,10 @@ namespace ThMEPEngineCore.Algorithm.AStarAlgorithm_New
         public static List<Point3d> GetAdjacentPoint(Point3d current, List<Line> mapLines)
         {
             List<Point3d> adjacentPts = new List<Point3d>();
-            var resLines = mapLines.Where(x => x.StartPoint.IsEqualTo(current, new Tolerance(1, 1)) || x.EndPoint.IsEqualTo(current, new Tolerance(1, 1))).ToList();
+            var resLines = mapLines.Where(x => x.StartPoint.IsEqualTo(current, new Tolerance(0.001, 0.001)) || x.EndPoint.IsEqualTo(current, new Tolerance(0.001, 0.001))).ToList();
             foreach (var line in resLines)
             {
-                adjacentPts.Add(line.EndPoint.IsEqualTo(current, new Tolerance(1, 1)) ? line.StartPoint : line.EndPoint);
+                adjacentPts.Add(line.EndPoint.IsEqualTo(current, new Tolerance(0.001, 0.001)) ? line.StartPoint : line.EndPoint);
             }
 
             return adjacentPts;
