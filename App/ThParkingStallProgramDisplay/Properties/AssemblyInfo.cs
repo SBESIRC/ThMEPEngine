e1f73148449a7cae39452ebe2c0f441b34eb8398
齐少华 <2316187741@qq.com>
2022-06-09 15:28:45 +0800

    change folder name (#2671)

--- App/ThParkingStallProgramDisplay/Properties/AssemblyInfo.cs
+++ App/ThParkingStallProgramDisplay/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ThParkingStallProgramDisplsy")]
+[assembly: AssemblyTitle("ThParkingStallProgramDisplay")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ThParkingStallProgramDisplsy")]
+[assembly: AssemblyProduct("ThParkingStallProgramDisplay")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
