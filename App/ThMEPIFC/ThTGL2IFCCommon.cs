bc45f9fbdbb4606b2e95d2a7341f3a70774a8b40
董士崇 <dongshichong@thape.com.cn>
2022-06-13 12:39:36 +0800

    Define ThTGL2IFCCommon

---
+++ App/ThMEPIFC/ThTGL2IFCCommon.cs
@@ -0,0 +1,12 @@
+﻿using Autodesk.AutoCAD.Geometry;
+
+namespace ThMEPIFC
+{
+    public class ThTGL2IFCCommon
+    {
+        /// <summary>
+        /// 全局精度
+        /// </summary>
+        public Tolerance Global = new Tolerance(1e-3, 1e-5);
+    }
+}
