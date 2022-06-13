1ef052ca43959eb6ced7d5d57be283b8b9bb7e80
KomaHe <khthds@163.com>
2022-06-13 20:16:13 +0800

    Feature/mepio build (#2708)

---
+++ App/ThMEPIO/DB/THMEPDBCoreServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThMEPIO.DB
+{
+    public interface THMEPDBCoreServices
+    {
+        //查
+        DataTable ExecuteDataTable(string tableName);
+        
+        //查
+        DataTable GetTable(string sql);
+
+        //增删改
+        int ExecuteNonQuery(string sql);
+    }
+}
