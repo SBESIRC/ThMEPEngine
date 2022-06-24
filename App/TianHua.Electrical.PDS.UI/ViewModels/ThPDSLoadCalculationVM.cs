ebb9f9d36dafdf7371a57caa204e0165a9ba8cae
董士崇 <dongshichong@thape.com.cn>
2022-06-24 23:18:14 +0800

    Define ThPDSLoadCalculationVM and data binding with ThPDSLoadCalculation

---
+++ App/TianHua.Electrical.PDS.UI/ViewModels/ThPDSLoadCalculationVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace TianHua.Electrical.PDS.UI.ViewModels
+{
+    public sealed class ThPDSLoadCalculationVM : ObservableObject
+    {
+        public ThPDSLoadCalculationVM()
+        {
+            //
+        }
+    }
+}
