2b4cb16b01c69e8a33ea3f6ab15c567645e5a402
董士崇 <dongshichong@thape.com.cn>
2022-06-26 10:17:12 +0800

    Define ThPDSTransformerVM

---
+++ App/TianHua.Electrical.PDS.UI/Models/ThPDSTransformer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TianHua.Electrical.PDS.UI.Models
+{
+    /// <summary>
+    /// 变压器
+    /// </summary>
+    public class ThPDSTransformer
+    {
+        /// <summary>
+        /// 变压器编号
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// 负载率
+        /// </summary>
+        public double LoadRate { get; set; }
+    }
+}
---
+++ App/TianHua.Electrical.PDS.UI/ViewModels/ThPDSTransformerVM.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using TianHua.Electrical.PDS.UI.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace TianHua.Electrical.PDS.UI.ViewModels
+{
+    public sealed class ThPDSTransformerVM : ObservableObject
+    {
+        private readonly IList<ThPDSTransformer> _transformers;
+
+        public IList<ThPDSTransformer> DataList => _transformers;
+
+        public ThPDSTransformerVM()
+        {
+            _transformers = new List<ThPDSTransformer>();
+        }
+
+        internal ObservableCollection<ThPDSTransformer> GetCardDataList()
+        {
+            return new ObservableCollection<ThPDSTransformer>
+            {
+                new ThPDSTransformer()
+                {
+                    Number = "1T1",
+                    LoadRate = 0.8,
+                },
+                new ThPDSTransformer()
+                {
+                    Number = "1T2",
+                    LoadRate = 0.8,
+                },
+                new ThPDSTransformer()
+                {
+                    Number = "1T3",
+                    LoadRate = 0.75,
+                },
+                new ThPDSTransformer()
+                {
+                    Number = "1T4",
+                    LoadRate = 0.75,
+                }
+            };
+        }
+    }
+}
