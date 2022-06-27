567a4cdb1a514dea646e514ec94d91b3e971574e
董士崇 <dongshichong@thape.com.cn>
2022-06-27 14:57:51 +0800

    THLCKX support UCS

--- App/ThMEPWSS/Command/ThPipeInsertFloorFrameCmd.cs
+++ App/ThMEPWSS/Command/ThPipeInsertFloorFrameCmd.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using Linq2Acad;
 using AcHelper;
 using AcHelper.Commands;
-using System.Collections.Generic;
 using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Geometry;
 using ThMEPWSS.Pipe.Service;
 
 namespace ThMEPWSS.Command
 {
-  public  class ThPipeInsertFloorFrameCmd : IAcadCommand, IDisposable
+    public class ThPipeInsertFloorFrameCmd : IAcadCommand, IDisposable
     {
         public void Dispose()
         {
@@ -17,20 +14,16 @@ namespace ThMEPWSS.Command
         }
         public void Execute()
         {
-            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            PromptPointResult result;
+            do
             {
-                PromptPointResult result;               
-                do
+                result = Active.Editor.GetPoint("\n选择要插入的基点位置");
+                if (result.Status == PromptStatus.OK)
                 {
-                    var tpipe = new List<Point3d>();
-                    result = Active.Editor.GetPoint("\n选择要插入的基点位置");
-                    if (result.Status == PromptStatus.OK)
-                    {
-                        tpipe.Add(result.Value);
-                        ThInsertStoreyFrameService.Insert(tpipe);
-                    }
-                } while (result.Status == PromptStatus.OK);              
-            }
+                    ThInsertStoreyFrameService.ImportBlock();
+                    ThInsertStoreyFrameService.Insert(result.Value);
+                }
+            } while (result.Status == PromptStatus.OK);
         }
     }
 }
--- App/ThMEPWSS/Pipe/Service/ThInsertStoreyFrameService.cs
+++ App/ThMEPWSS/Pipe/Service/ThInsertStoreyFrameService.cs
@@ -1,6 +1,8 @@
-﻿using Linq2Acad;
+﻿using AcHelper;
+using Linq2Acad;
 using DotNetARX;
 using ThCADExtension;
+using GeometryExtensions;
 using Autodesk.AutoCAD.Geometry;
 using System.Collections.Generic;
 using Autodesk.AutoCAD.DatabaseServices;
@@ -9,61 +11,44 @@ namespace ThMEPWSS.Pipe.Service
 {
     public static class ThInsertStoreyFrameService
     {
-        public static void Insert(List<Point3d> positions)
+        public static void ImportBlock()
         {
             using (AcadDatabase acadDatabase = AcadDatabase.Active())
-            {     
-                acadDatabase.Database.ImportBlock(ThWPipeCommon.STOREY_BLOCK_NAME, ThWPipeCommon.STOREY_LAYER_NAME);
-            }
-            using (AcadDatabase acadDatabase = AcadDatabase.Active())
             {
-                var attNameValues = new Dictionary<string, string>()
-                {
-                    { ThWPipeCommon.STOREY_ATTRIBUTE_VALUE_NUMBER, string.Empty }
-                };
-                positions.ForEach(o =>
-                {
-                    acadDatabase.Database.InsertBlock(
-                        ThWPipeCommon.STOREY_LAYER_NAME,
-                        ThWPipeCommon.STOREY_BLOCK_NAME,
-                        o,
-                        new Scale3d(1),
-                        0,
-                        attNameValues);
-                });
+                acadDatabase.Database.ImportBlock(ThWPipeCommon.STOREY_BLOCK_NAME, ThWPipeCommon.STOREY_LAYER_NAME);
             }
         }
 
-        /// <summary>
-        /// 插入图块
-        /// </summary>
-        /// <param name="database"></param>
-        /// <param name="layer"></param>
-        /// <param name="name"></param>
-        /// <param name="position"></param>
-        /// <param name="scale"></param>
-        /// <param name="angle"></param>
-        /// <param name="attNameValues"></param>
-        private static void InsertBlock(this Database database, string layer, string name, Point3d position, Scale3d scale, double angle, Dictionary<string, string> attNameValues)
+        public static void Insert(Point3d position)
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Active())
+            {
+                var obj = acadDatabase.Database.InsertBlock(
+                    ThWPipeCommon.STOREY_LAYER_NAME,
+                    ThWPipeCommon.STOREY_BLOCK_NAME,
+                    position,
+                    new Scale3d(1),
+                    0);
+                var frame = acadDatabase.Element<BlockReference>(obj, true);
+                frame.TransformBy(Active.Editor.UCS2WCS());
+            }
+        }
+
+        private static ObjectId InsertBlock(this Database database, string layer, string name, Point3d position, Scale3d scale, double angle)
         {
             using (AcadDatabase acadDatabase = AcadDatabase.Use(database))
             {
-                acadDatabase.ModelSpace.ObjectId.InsertBlockReference(
-                    layer, 
-                    name, 
-                    position, 
-                    scale, 
-                    angle, 
+                var attNameValues = new Dictionary<string, string>();
+                return acadDatabase.ModelSpace.ObjectId.InsertBlockReference(
+                    layer,
+                    name,
+                    position,
+                    scale,
+                    angle,
                     attNameValues);
             }
         }
 
-        /// <summary>
-        /// 导入图块
-        /// </summary>
-        /// <param name="database"></param>
-        /// <param name="name"></param>
-        /// <param name="layer"></param>
         private static void ImportBlock(this Database database, string name, string layer)
         {
             using (AcadDatabase acadDatabase = AcadDatabase.Use(database))
