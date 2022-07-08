f83f4cc38870f1a080368bb52b5ee587e03bf9b7
luoyun7 <935592870@qq.com>
2022-07-08 12:32:40 +0800

    no message

--- App/ThMEPWSS/HydrantLayout/Model/SecondaryModel.cs
+++ App/ThMEPWSS/HydrantLayout/Model/SecondaryModel.cs
@@ -46,6 +46,7 @@ namespace ThMEPWSS.HydrantLayout.Model
         //单柱子新晋指标
         public double PositionScore = 0;
         public double DoorScore = 0;
+        public int BasePointPosition = -2;
 
         //绘图属性
         public double ShortSide = Info.ShortSide;
