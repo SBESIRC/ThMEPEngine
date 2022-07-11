222d15104dd81474836386fd9775feba8016885b
董士崇 <dongshichong@thape.com.cn>
2022-07-11 15:40:25 +0800

    Define View Model ThPDSCreateLoadVM

---
+++ App/TianHua.Electrical.PDS.UI/ViewModels/ThPDSCreateLoadVM.cs
@@ -0,0 +1,33 @@
+﻿using TianHua.Electrical.PDS.Project.Module;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace TianHua.Electrical.PDS.UI.ViewModels
+{
+    public sealed class ThPDSCreateLoadVM : ObservableObject
+    {
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public ImageLoadType Type { get; set; }
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// 功率
+        /// </summary>
+        public double Power { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 楼层
+        /// </summary>
+        public string Storey { get; set; }
+    }
+}
