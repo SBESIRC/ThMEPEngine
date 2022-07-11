72e75ebfd74529a552062eac5409db6e4792dfc1
董士崇 <dongshichong@thape.com.cn>
2022-07-11 20:19:50 +0800

    Implement ThPDSCreateLoadVM

--- App/TianHua.Electrical.PDS.UI/UserContorls/ThPDSCreateLoad.xaml.cs
+++ App/TianHua.Electrical.PDS.UI/UserContorls/ThPDSCreateLoad.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Controls;
-using System.Collections.Generic;
-using TianHua.Electrical.PDS.Service;
-using TianHua.Electrical.PDS.Project.Module;
-using TianHua.Electrical.PDS.UI.Models;
-using Microsoft.Toolkit.Mvvm.Messaging;
+﻿using System.Windows;
+using TianHua.Electrical.PDS.UI.ViewModels;
 
 namespace TianHua.Electrical.PDS.UI.UserContorls
 {
@@ -15,81 +8,29 @@ namespace TianHua.Electrical.PDS.UI.UserContorls
         public ThPDSCreateLoad()
         {
             InitializeComponent();
-            defaultKV.SelectedItem = "0.38";
-            defaultKV.ItemsSource = new List<string>() { "0.38", "0.22" };
-            defaultPower.Text = "0";
-            var array = typeof(PDSImageSources).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            for (int i = 0; i < array.Length; i++)
-            {
-                var fi = array[i];
-                var v = (ImageSource)fi.GetValue(null);
-                var img = new Image() { Source = v };
-                cbx.Items.Add(img);
-                if (i == 0) cbx.SelectedItem = img;
-            }
+            this.DataContext = new ThPDSCreateLoadVM();
         }
-        static class PDSImageSources
-        {
-            static readonly ImageSourceConverter cvt = new();
-            public static readonly ImageSource RD = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Residential Distribution Panel.png"));
-            public static readonly ImageSource RS = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Roller Shutter.png"));
-            public static readonly ImageSource Socket = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Socket.png"));
-            public static readonly ImageSource ACB = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/AC Charger.png"));
-            public static readonly ImageSource DC = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/DC Charger.png"));
-            public static readonly ImageSource AC = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Electrical Control Panel.png"));
-            public static readonly ImageSource AW = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Electrical Meter Panel.png"));
-            public static readonly ImageSource ALE = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Emergency Lighting Distribution Panel.png"));
-            public static readonly ImageSource APE = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Emergency Power Distribution Panel.png"));
-            public static readonly ImageSource FEL = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Fire Emergency Lighting Distribution Panel.png"));
-            public static readonly ImageSource INT = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Isolation Switch Panel.png"));
-            public static readonly ImageSource AL = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Lighting Distribution Panel.png"));
-            public static readonly ImageSource Light = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Luminaire.png"));
-            public static readonly ImageSource Motor = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Motor.png"));
-            public static readonly ImageSource AP = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Power Distribution Panel.png"));
-            public static readonly ImageSource Pump = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Pump.png"));
-        }
-        ImageLoadType ImageLoadType
-        {
-            get
-            {
-                var array = typeof(PDSImageSources).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-                for (int i = 0; i < array.Length; i++)
-                {
-                    var fi = array[i];
-                    var v = (ImageSource)fi.GetValue(null);
-                    if ((cbx.SelectedItem as Image)?.Source == v)
-                    {
-                        foreach (ImageLoadType type in Enum.GetValues(typeof(ImageLoadType)))
-                        {
-                            if (type.ToString() == fi.Name)
-                            {
-                                return type;
-                            }
-                        }
-                    }
-                }
-                return ImageLoadType.None;
-            }
-        }
+
         private void btnInsert(object sender, RoutedEventArgs e)
         {
-            double.TryParse(defaultPower.Text, out var v);
-            new ThPDSUpdateToDwgService().AddLoadDimension(ThPDSProjectGraphService.CreatNewLoad(/*defaultKV: double.Parse(defaultKV.SelectedItem.ToString()), */defaultLoadID: defaultLoadID.Text, defaultPower: v /*defaultPower: double.Parse(defaultPower.Text)*/, defaultDescription: defaultDescription.Text, defaultFireLoad: defaultFireLoad.IsChecked == true, imageLoadType: ImageLoadType));
-            WeakReferenceMessenger.Default.Send(new GraphNodeAddMessage("btnInsert Click"));
+            //double.TryParse(defaultPower.Text, out var v);
+            //new ThPDSUpdateToDwgService().AddLoadDimension(ThPDSProjectGraphService.CreatNewLoad(/*defaultKV: double.Parse(defaultKV.SelectedItem.ToString()), */defaultLoadID: defaultLoadID.Text, defaultPower: v /*defaultPower: double.Parse(defaultPower.Text)*/, defaultDescription: defaultDescription.Text, defaultFireLoad: defaultFireLoad.IsChecked == true, imageLoadType: ImageLoadType));
+            //WeakReferenceMessenger.Default.Send(new GraphNodeAddMessage("btnInsert Click"));
             Close();
         }
         private void btnSave(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(defaultPower.Text, out var v))
-            {
-                ThPDSProjectGraphService.CreatNewLoad(
-                    defaultLoadID: defaultLoadID.Text,
-                    defaultPower: v,
-                    defaultDescription: defaultDescription.Text,
-                    defaultFireLoad: defaultFireLoad.IsChecked == true,
-                    imageLoadType: ImageLoadType);
-                WeakReferenceMessenger.Default.Send(new GraphNodeAddMessage("btnSave Click"));
-            }
+            var vm = this.DataContext as ThPDSCreateLoadVM;
+            //if (double.TryParse(defaultPower.Text, out var v))
+            //{
+            //    ThPDSProjectGraphService.CreatNewLoad(
+            //        defaultLoadID: defaultLoadID.Text,
+            //        defaultPower: v,
+            //        defaultDescription: defaultDescription.Text,
+            //        defaultFireLoad: defaultFireLoad.IsChecked == true,
+            //        imageLoadType: ImageLoadType);
+            //    WeakReferenceMessenger.Default.Send(new GraphNodeAddMessage("btnSave Click"));
+            //}
             Close();
         }
     }
--- App/TianHua.Electrical.PDS.UI/ViewModels/ThPDSCreateLoadVM.cs
+++ App/TianHua.Electrical.PDS.UI/ViewModels/ThPDSCreateLoadVM.cs
@@ -1,19 +1,127 @@
-﻿using TianHua.Electrical.PDS.Project.Module;
+﻿using System;
+using System.Windows.Media;
+using System.Collections.ObjectModel;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using TianHua.Electrical.PDS.Project.Module;
 
 namespace TianHua.Electrical.PDS.UI.ViewModels
 {
     public sealed class ThPDSCreateLoadVM : ObservableObject
     {
-        /// <summary>
-        /// 类型
-        /// </summary>
-        private ImageLoadType _Type;
-        public ImageLoadType Type
+        private readonly ImageSourceConverter cvt = new();
+        public ObservableCollection<ThPDSLoadItemTypeVM> Types { get; private set; }
+
+        public ThPDSCreateLoadVM()
         {
-            get => _Type;
-            set => SetProperty(ref _Type, value);
+            Types = new ObservableCollection<ThPDSLoadItemTypeVM>()
+            {
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.AL,
+                    Image = LoadImage("Lighting Distribution Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.AP,
+                    Image = LoadImage("Power Distribution Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.ALE,
+                    Image = LoadImage("Emergency Lighting Distribution Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.APE,
+                    Image = LoadImage("Emergency Power Distribution Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.FEL,
+                    Image = LoadImage("Fire Emergency Lighting Distribution Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.AW,
+                    Image = LoadImage("Electrical Meter Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.ACB,
+                    Image = LoadImage("AC Charger.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.RS,
+                    Image = LoadImage("Roller Shutter.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.INT,
+                    Image = LoadImage("Isolation Switch Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.RD,
+                    Image = LoadImage("Residential Distribution Panel.png"),
+                },
+                //new ThPDSLoadItemTypeVM()
+                //{
+                //    Type = ImageLoadType.AX,
+                //    Image = 
+
+                //},
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.Light,
+                    Image = LoadImage("Luminaire.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.Socket,
+                    Image = LoadImage("Socket.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.AC,
+                    Image = LoadImage("Electrical Control Panel.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.DC,
+                    Image = LoadImage("DC Charger.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.Motor,
+                    Image = LoadImage("Motor.png"),
+                },
+                new ThPDSLoadItemTypeVM()
+                {
+                    Type = ImageLoadType.Pump,
+                    Image = LoadImage("Pump.png"),
+                },
+            };
+
+            // 默认设备类型
+            Type = Types[0];
         }
+
+        private ImageSource LoadImage(string name)
+        {
+            var uri = string.Format("pack://application:,,,/ThControlLibraryWPF;component/Images/{0}", name);
+            return (ImageSource)cvt.ConvertFrom(new Uri(uri));
+        }
+
+        /// <summary>
+        /// 设备类型
+        /// </summary>
+        public ThPDSLoadItemTypeVM Type { get; private set; }
+
+        /// <summary>
+        /// 额定电压
+        /// </summary>
+        public double RatedVoltage { get; set; }
 
         /// <summary>
         /// 编号
@@ -53,6 +161,16 @@ namespace TianHua.Electrical.PDS.UI.ViewModels
         {
             get => _Storey;
             set => SetProperty(ref _Storey, value);
+        }
+
+        /// <summary>
+        /// 消防设备
+        /// </summary>
+        private bool _FireLoad;
+        public bool FireLoad
+        {
+            get => _FireLoad;
+            set => SetProperty(ref _FireLoad, value);
         }
     }
 }
