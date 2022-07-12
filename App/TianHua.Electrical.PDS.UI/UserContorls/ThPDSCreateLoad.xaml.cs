df04aa1c91f12c45411f8afa703c19d60d4ec7a5
董士崇 <dongshichong@thape.com.cn>
2022-07-12 15:09:59 +0800

    New Load UI (#2959)
    * Define View Model ThPDSCreateLoadVM
    
    * Update ThPDSCreateLoadVM
    
    * Implement ThPDSCreateLoadVM
    
    * Define ThPDSProjectGraphNodeData
    
    * Handle Save and Insert button
    
    * Fix build error
    
    * Update
    
    * Sync ThPDSProjectGraphNodeData with Power

--- App/TianHua.Electrical.PDS.UI/UserContorls/ThPDSCreateLoad.xaml.cs
+++ App/TianHua.Electrical.PDS.UI/UserContorls/ThPDSCreateLoad.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Controls;
-using System.Collections.Generic;
-using TianHua.Electrical.PDS.Service;
+﻿using System.Windows;
 using TianHua.Electrical.PDS.Project.Module;
-using TianHua.Electrical.PDS.UI.Models;
-using Microsoft.Toolkit.Mvvm.Messaging;
+using TianHua.Electrical.PDS.UI.ViewModels;
 
 namespace TianHua.Electrical.PDS.UI.UserContorls
 {
@@ -15,82 +9,33 @@ namespace TianHua.Electrical.PDS.UI.UserContorls
         public ThPDSCreateLoad()
         {
             InitializeComponent();
-            defaultKV.SelectedItem = "0.38";
-            defaultKV.ItemsSource = new List<string>() { "0.38", "0.22" };
-            defaultPower.Text = "0";
-            var array = typeof(PDSImageSources).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            for (int i = 0; i < array.Length; i++)
-            {
-                var fi = array[i];
-                var v = (ImageSource)fi.GetValue(null);
-                var img = new Image() { Source = v };
-                cbx.Items.Add(img);
-                if (i == 0) cbx.SelectedItem = img;
-            }
+            this.DataContext = new ThPDSCreateLoadVM();
         }
-        static class PDSImageSources
-        {
-            static readonly ImageSourceConverter cvt = new();
-            public static readonly ImageSource RD = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Residential Distribution Panel.png"));
-            public static readonly ImageSource RS = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Roller Shutter.png"));
-            public static readonly ImageSource Socket = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Socket.png"));
-            public static readonly ImageSource ACB = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/AC Charger.png"));
-            public static readonly ImageSource DC = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/DC Charger.png"));
-            public static readonly ImageSource AC = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Electrical Control Panel.png"));
-            public static readonly ImageSource AW = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Electrical Meter Panel.png"));
-            public static readonly ImageSource ALE = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Emergency Lighting Distribution Panel.png"));
-            public static readonly ImageSource APE = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Emergency Power Distribution Panel.png"));
-            public static readonly ImageSource FEL = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Fire Emergency Lighting Distribution Panel.png"));
-            public static readonly ImageSource INT = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Isolation Switch Panel.png"));
-            public static readonly ImageSource AL = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Lighting Distribution Panel.png"));
-            public static readonly ImageSource Light = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Luminaire.png"));
-            public static readonly ImageSource Motor = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Motor.png"));
-            public static readonly ImageSource AP = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Power Distribution Panel.png"));
-            public static readonly ImageSource Pump = (ImageSource)cvt.ConvertFrom(new Uri("pack://application:,,,/ThControlLibraryWPF;component/Images/Pump.png"));
-        }
-        ImageLoadType ImageLoadType
-        {
-            get
-            {
-                var array = typeof(PDSImageSources).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-                for (int i = 0; i < array.Length; i++)
-                {
-                    var fi = array[i];
-                    var v = (ImageSource)fi.GetValue(null);
-                    if ((cbx.SelectedItem as Image)?.Source == v)
-                    {
-                        foreach (ImageLoadType type in Enum.GetValues(typeof(ImageLoadType)))
-                        {
-                            if (type.ToString() == fi.Name)
-                            {
-                                return type;
-                            }
-                        }
-                    }
-                }
-                return ImageLoadType.None;
-            }
-        }
+
         private void btnInsert(object sender, RoutedEventArgs e)
         {
-            double.TryParse(defaultPower.Text, out var v);
-            new ThPDSUpdateToDwgService().AddLoadDimension(ThPDSProjectGraphService.CreatNewLoad(/*defaultKV: double.Parse(defaultKV.SelectedItem.ToString()), */defaultLoadID: defaultLoadID.Text, defaultPower: v /*defaultPower: double.Parse(defaultPower.Text)*/, defaultDescription: defaultDescription.Text, defaultFireLoad: defaultFireLoad.IsChecked == true, imageLoadType: ImageLoadType));
-            WeakReferenceMessenger.Default.Send(new GraphNodeAddMessage("btnInsert Click"));
+            var vm = this.DataContext as ThPDSCreateLoadVM;
+            ThPDSProjectGraphService.CreatNewLoad(CreateData(vm));
             Close();
         }
         private void btnSave(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(defaultPower.Text, out var v))
-            {
-                ThPDSProjectGraphService.CreatNewLoad(
-                    defaultLoadID: defaultLoadID.Text,
-                    defaultPower: v,
-                    defaultDescription: defaultDescription.Text,
-                    defaultFireLoad: defaultFireLoad.IsChecked == true,
-                    imageLoadType: ImageLoadType);
-                WeakReferenceMessenger.Default.Send(new GraphNodeAddMessage("btnSave Click"));
-            }
+            var vm = this.DataContext as ThPDSCreateLoadVM;
+            ThPDSProjectGraphService.CreatNewLoad(CreateData(vm));
             Close();
+        }
+
+        private ThPDSProjectGraphNodeData CreateData(ThPDSCreateLoadVM vm)
+        {
+            var data = ThPDSProjectGraphNodeData.Create();
+            data.Power = vm.Power;
+            data.Storey = vm.Storey;
+            data.Number = vm.Number;
+            data.Type = vm.Type.Type;
+            data.FireLoad = vm.FireLoad;
+            data.Description = vm.Description;
+            data.Sync();
+            return data;
         }
     }
 }
--- App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphService.cs
+++ App/TianHua.Electrical.PDS/Project/Module/ThPDSProjectGraphService.cs
@@ -623,24 +623,21 @@ namespace TianHua.Electrical.PDS.Project.Module
         /// <summary>
         /// 新建负载
         /// </summary>
-        /// <param name="defaultPhase">项数</param>
-        /// <param name="defaultPhaseSequence">相序</param>
-        /// <param name="defaultLoadID">设备编号</param>
-        /// <param name="defaultPower">设备功率</param>
-        /// <param name="defaultDescription">描述信息</param>
-        /// <param name="defaultFireLoad">是否消防</param>
-        public static ThPDSProjectGraphNode CreatNewLoad(ThPDSPhase defaultPhase = ThPDSPhase.三相, Circuit.PhaseSequence defaultPhaseSequence = Circuit.PhaseSequence.L123, string defaultLoadID = "", double defaultPower = 0, string defaultDescription = "备用", bool defaultFireLoad = false, ImageLoadType imageLoadType = ImageLoadType.None ,string floorNumber = "1F")
+        /// <param name="data"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public static ThPDSProjectGraphNode CreatNewLoad(ThPDSProjectGraphNodeData data)
         {
             //业务逻辑：业务新建的负载，都是空负载，建立不出别的负载
             var node = new ThPDSProjectGraphNode();
-            node.Load.Phase = defaultPhase;
-            node.Details.LoadCalculationInfo.PhaseSequence = defaultPhaseSequence;
-            node.Load.ID.LoadID = defaultLoadID;
-            node.Details.LoadCalculationInfo.HighPower = defaultPower;
-            node.Load.ID.Description = defaultDescription;
-            node.Load.SetFireLoad(defaultFireLoad);
-            node.Load.SetLocation(new ThPDSLocation() { FloorNumber = floorNumber });
-            switch (imageLoadType)
+            node.Load.Phase = data.Phase;
+            node.Details.LoadCalculationInfo.PhaseSequence = data.PhaseSequence;
+            node.Load.ID.LoadID = data.Number;
+            node.Details.LoadCalculationInfo.HighPower = data.Power;
+            node.Load.ID.Description = data.Description;
+            node.Load.SetFireLoad(data.FireLoad);
+            node.Load.SetLocation(new ThPDSLocation() { FloorNumber = data.Storey });
+            switch (data.Type)
             {
                 case ImageLoadType.None:
                     {
@@ -733,7 +730,7 @@ namespace TianHua.Electrical.PDS.Project.Module
                     {
                         node.Type = PDSNodeType.DistributionBox;
                         node.Load.LoadTypeCat_1 = ThPDSLoadTypeCat_1.DistributionPanel;
-                        node.Load.LoadTypeCat_2 = defaultPhase == ThPDSPhase.一相? ThPDSLoadTypeCat_2.OnePhaseSocket: ThPDSLoadTypeCat_2.ThreePhaseSocket;
+                        node.Load.LoadTypeCat_2 = data.Phase == ThPDSPhase.一相 ? ThPDSLoadTypeCat_2.OnePhaseSocket : ThPDSLoadTypeCat_2.ThreePhaseSocket;
                         node.Load.LoadTypeCat_3 = ThPDSLoadTypeCat_3.None;
                         break;
                     }
@@ -825,7 +822,11 @@ namespace TianHua.Electrical.PDS.Project.Module
         public static ThPDSProjectGraphEdge AddCircuit(ProjectGraph graph, ThPDSProjectGraphNode node, CircuitFormOutType type , ThPDSPhase defaultPhase = ThPDSPhase.三相)
         {
             //Step 1:新建空负载
-            var target = CreatNewLoad(node.Load.Phase, node.Details.LoadCalculationInfo.PhaseSequence, floorNumber:node.Load.Location.FloorNumber);
+            var data = ThPDSProjectGraphNodeData.Create();
+            data.Phase = node.Load.Phase;
+            data.Storey = node.Load.Location.FloorNumber;
+            data.PhaseSequence = node.Details.LoadCalculationInfo.PhaseSequence;
+            var target = CreatNewLoad(data);
             if (node.Details.CircuitFormType.CircuitFormType == CircuitFormInType.集中电源)
             {
                 target.Load.Phase = ThPDSPhase.一相;
@@ -852,7 +853,11 @@ namespace TianHua.Electrical.PDS.Project.Module
         public static ThPDSProjectGraphEdge AddCircuit(ProjectGraph graph, ThPDSProjectGraphNode node, string type)
         {
             //Step 1:新建空负载
-            var target = CreatNewLoad(node.Load.Phase, node.Details.LoadCalculationInfo.PhaseSequence, floorNumber: node.Load.Location.FloorNumber);
+            var data = ThPDSProjectGraphNodeData.Create();
+            data.Phase = node.Load.Phase;
+            data.Storey = node.Load.Location.FloorNumber;
+            data.PhaseSequence = node.Details.LoadCalculationInfo.PhaseSequence;
+            var target = CreatNewLoad(data);
             //Step 2:新建回路
             var newEdge = new ThPDSProjectGraphEdge(node, target) { Circuit = new ThPDSCircuit() { ID = new ThPDSID() { SourcePanelIDList = new List<string>() { node.Load.ID.LoadID } } } };
             //Step 3:获取对应的CircuitFormOutType
