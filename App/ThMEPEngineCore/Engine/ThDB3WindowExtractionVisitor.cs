b321b39d23e16538457cecd93ad3038d44b23383
jackwhoknow <trasure@126.com>
2022-07-18 13:36:36 +0800

    support blockreference (#3000)

--- App/ThMEPEngineCore/Engine/ThDB3WindowExtractionVisitor.cs
+++ App/ThMEPEngineCore/Engine/ThDB3WindowExtractionVisitor.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
-using ThMEPEngineCore.CAD;
-using ThMEPEngineCore.Algorithm;
+﻿using System.Linq;
+using System.Collections.Generic;
+using NFox.Cad;
+using ThCADExtension;
+using Dreambuild.AutoCAD;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.DatabaseServices;
-using ThCADExtension;
+using ThMEPEngineCore.CAD;
+using ThMEPEngineCore.Algorithm;
 
 namespace ThMEPEngineCore.Engine
 {
@@ -14,6 +17,10 @@ namespace ThMEPEngineCore.Engine
             if (dbObj is Polyline polyline)
             {
                 elements.AddRange(HandleCurve(polyline, matrix));
+            }
+            else if (dbObj is BlockReference br)
+            {
+                elements.AddRange(HandleBlockreference(br, matrix));
             }
         }
 
@@ -26,6 +33,7 @@ namespace ThMEPEngineCore.Engine
                 elements.RemoveAll(o => !xclip.Contains(o.Geometry as Curve));
             }
         }
+
         public override bool IsBuildElement(Entity entity)
         {
             if (entity.Hyperlinks.Count > 0)
@@ -35,6 +43,14 @@ namespace ThMEPEngineCore.Engine
             }
             return false;
         }
+
+        public override bool IsBuildElementBlockReference(BlockReference blockReference)
+        {
+            return blockReference.BlockTableRecord.IsValid && 
+                CheckLayerValid(blockReference) && 
+                IsBuildElement(blockReference);
+        }
+
         private List<ThRawIfcBuildingElementData> HandleCurve(Polyline polyline, Matrix3d matrix)
         {
             var results = new List<ThRawIfcBuildingElementData>();
@@ -51,6 +67,33 @@ namespace ThMEPEngineCore.Engine
                 }
             }
             return results;
+        }
+
+        private List<ThRawIfcBuildingElementData> HandleBlockreference(BlockReference br, Matrix3d matrix)
+        {
+            var results = new List<ThRawIfcBuildingElementData>();
+            var objs = ThDrawTool.Explode(br);
+            objs = VisibleEntities(objs);
+            objs.OfType<Curve>().ForEach(c =>
+            {
+                var clone = c.WashClone();
+                if (clone != null)
+                {
+                    clone.TransformBy(matrix);
+                    results.Add(new ThRawIfcBuildingElementData()
+                    {
+                        Geometry = clone,
+                    });
+                }
+            });
+            return results;
+        }
+
+        private DBObjectCollection VisibleEntities(DBObjectCollection objs)
+        {
+            return objs.OfType<Entity>()
+                .Where(e => e.Visible)
+                .ToCollection();
         }
     }
 }
