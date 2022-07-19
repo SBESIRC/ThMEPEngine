8343a598f732788bb758d39d09221c19c0cc42e3
liupan-AI <1750923@tongji.edu.cn>
2022-07-19 16:24:00 +0800

    Implement InsertNewLoad(ThPDSProjectGraphNode, ImageLoadType, ThPDSInsertBlockInfo) (#3014)
    Co-authored-by: 刘潘 <liupan1@thape.com.cn>

---
+++ App/TianHua.Electrical.PDS/Model/ThPDSInsertBlockInfo.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.AutoCAD.Geometry;
+
+namespace TianHua.Electrical.PDS.Model
+{
+    public class ThPDSInsertBlockInfo
+    {
+        /// <summary>
+        /// 负载插入点
+        /// </summary>
+        public Point3d InsertPoint { get; set; }
+
+        /// <summary>
+        /// 标注插入点
+        /// </summary>
+        public Point3d FirstPoint { get; set; }
+
+        /// <summary>
+        /// 引线端点
+        /// </summary>
+        public Point3d SecondPoint { get; set; }
+
+        /// <summary>
+        /// 标注线方向
+        /// </summary>
+        public Point3d ThirdPoint { get; set; }
+
+        /// <summary>
+        /// 插入比例
+        /// </summary>
+        public Scale3d Scale { get; set; }
+    }
+}
