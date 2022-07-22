4c49f324467eda819c6eb090e4a200608991e601
张文宣 <zhangwenxuan@thape.com.cn>
2022-07-22 16:11:31 +0800

    1.创建数据结构 ： SegLine分区线，ORamp斜交坡道，OInterparameter计算SubArea的个数 2.斜交有效车道求解代码

---
+++ App/ThParkingStall.Core/OInterProcess/SegLine.cs
@@ -0,0 +1,74 @@
+﻿using NetTopologySuite.Geometries;
+using NetTopologySuite.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThParkingStall.Core.OTools;
+
+namespace ThParkingStall.Core.OInterProcess
+{
+    [Serializable]
+    public class SegLine
+    {
+        public LineSegment Splitter=null;// 参与分割的线
+        public LineSegment VaildLane=null;//有效车道，具有车道性质的部分
+        //private Vector2D _DirectionVector;
+        //public Vector2D DirectionVector //方向向量
+        //{ 
+        //    get 
+        //        {
+        //        if (_DirectionVector == null) _DirectionVector = Splitter.DirVector();
+        //        return _DirectionVector;
+        //        } 
+        //}
+        //private Vector2D _NormalVector;
+        //public Vector2D NormalVector //法向量
+        //{
+        //    get
+        //    {
+        //        if (_NormalVector == null) _NormalVector = Splitter.NormalVector();
+        //        return _NormalVector;
+        //    }
+        //}
+        //public SegLine StartFrom;//起始点的位置，为null代表起始于边界
+        //public SegLine EndTo;//终点位置,为null代表终结于边界
+        public int RoadWidth=5500;//车道宽
+        public bool IsFixed =false;//是否固定
+        private double _MinVal;
+        public double MinVal//迭代最小值
+        {
+            get { if (IsFixed) return 0.0;else return _MinVal; }
+        }
+        private double _MaxVal;
+        public double MaxVal//迭代最大值
+        {
+            get { if (IsFixed) return 0.0; else return _MaxVal; }
+        }
+
+        public SegLine(LineSegment inputLine, bool isFixed = false, int roadWidth = -1,double minVal = 0,double maxVal = 0)
+        {
+            Splitter = inputLine.Positivize();
+            IsFixed = isFixed;
+            RoadWidth = roadWidth;
+            _MinVal = minVal;
+            _MaxVal = maxVal;
+        }
+        public SegLine Clone()
+        {
+            var clone = new SegLine(Splitter,IsFixed,RoadWidth, _MinVal, _MaxVal);
+            clone.VaildLane = this.VaildLane;
+            return clone;
+        }
+        //输入基因值0~1，输出移动后的分割线（基于最大最小值）
+        public SegLine GetMovedLine(double fraction)
+        {
+            if (fraction < 0.0 || fraction > 1.0) throw new ArgumentOutOfRangeException("fraction must between 0 and 1");
+            var distance = MinVal* fraction + MaxVal* (1-fraction);
+            var clone = this.Clone();
+            clone.Splitter = Splitter.Translate(Splitter.NormalVector().Multiply(distance));
+            return clone;
+        }
+    }
+}
