e7f49fc08d9bf875fa545681532814ac5d2a4212
张文宣 <zhangwenxuan@thape.com.cn>
2022-07-22 17:56:16 +0800

    Reconstruct

--- App/ThParkingStall.Core/OInterProcess/SegLine.cs
+++ App/ThParkingStall.Core/OInterProcess/SegLine.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThParkingStall.Core.InterProcess;
 using ThParkingStall.Core.OTools;
 
 namespace ThParkingStall.Core.OInterProcess
@@ -34,7 +35,7 @@ namespace ThParkingStall.Core.OInterProcess
         //}
         //public SegLine StartFrom;//起始点的位置，为null代表起始于边界
         //public SegLine EndTo;//终点位置,为null代表终结于边界
-        public int RoadWidth=5500;//车道宽
+        public int RoadWidth;//车道宽,
         public bool IsFixed =false;//是否固定
         private double _MinVal;
         public double MinVal//迭代最小值
@@ -47,26 +48,32 @@ namespace ThParkingStall.Core.OInterProcess
             get { if (IsFixed) return 0.0; else return _MaxVal; }
         }
 
+        public bool IsInitLine;//是否是初始分割线
+
         public SegLine(LineSegment inputLine, bool isFixed = false, int roadWidth = -1,double minVal = 0,double maxVal = 0)
         {
             Splitter = inputLine.Positivize();
             IsFixed = isFixed;
-            RoadWidth = roadWidth;
+            if(roadWidth == -1) RoadWidth = VMStock.RoadWidth;
+            else RoadWidth = roadWidth;
             _MinVal = minVal;
             _MaxVal = maxVal;
+            IsInitLine = true;
         }
         public SegLine Clone()
         {
             var clone = new SegLine(Splitter,IsFixed,RoadWidth, _MinVal, _MaxVal);
-            clone.VaildLane = this.VaildLane;
+            clone.VaildLane = VaildLane;
+            clone.IsInitLine = false;
             return clone;
         }
         //输入基因值0~1，输出移动后的分割线（基于最大最小值）
         public SegLine GetMovedLine(double fraction)
         {
-            if (fraction < 0.0 || fraction > 1.0) throw new ArgumentOutOfRangeException("fraction must between 0 and 1");
+            if (fraction < 0.0 || fraction > 1.0) throw new ArgumentOutOfRangeException("Fraction must between 0 and 1!");
+            if (!IsInitLine) throw new ArgumentException("Only InitLine Can Create new SegLine!");
             var distance = MinVal* fraction + MaxVal* (1-fraction);
-            var clone = this.Clone();
+            var clone = Clone();
             clone.Splitter = Splitter.Translate(Splitter.NormalVector().Multiply(distance));
             return clone;
         }
