017d798ef727c8eeb8211ebe93e4809bb05e9901
张文宣 <62444072+bobbyzwx@users.noreply.github.com>
2022-07-29 14:04:14 +0800

    Feature/parking stall final seg line of ramp bug fix (#3122)
    * fix a bug
    
    * changge size of the out put
    
    Co-authored-by: 张文宣 <zhangwenxuan@thape.com.cn>

--- App/ThParkingStall.Core/InterProcess/Ramp.cs
+++ App/ThParkingStall.Core/InterProcess/Ramp.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NetTopologySuite.Geometries;
+using ThParkingStall.Core.Tools;
 
 namespace ThParkingStall.Core.InterProcess
 {
@@ -18,6 +19,42 @@ namespace ThParkingStall.Core.InterProcess
         {
             InsertPt = insertPt;
             Area = area;
+        }
+        public LineSegment GetLine(double tol = 100)
+        {
+            var rampLine = Area.Shell.ToLineSegments().OrderBy(l=>l.Distance(InsertPt.Coordinate)).First();
+            var X_dif = Math.Abs(rampLine.P0.X - rampLine.P1.X);
+            var Y_dif = Math.Abs(rampLine.P0.Y - rampLine.P1.Y);
+            var insideSize = 500;
+            var outsideSize = VMStock.RoadWidth / 2;
+            if(X_dif > Y_dif)//坡道线横向
+            {
+                var pt0 = new Coordinate(InsertPt.X, InsertPt.Y - tol);
+                var pt1 = new Coordinate(InsertPt.X, InsertPt.Y + tol);
+                if(Area.Contains(pt0.ToPoint()))
+                {
+                    return new LineSegment(InsertPt.X, InsertPt.Y - insideSize, InsertPt.X, InsertPt.Y + outsideSize);
+                }
+                else
+                {
+                    return new LineSegment(InsertPt.X, InsertPt.Y - outsideSize, InsertPt.X, InsertPt.Y + insideSize);
+                }
+                
+            }
+            else
+            {
+                var pt0 = new Coordinate(InsertPt.X - tol, InsertPt.Y);
+                var pt1 = new Coordinate(InsertPt.X + tol, InsertPt.Y);
+                //return new LineSegment(InsertPt.X-tol, InsertPt.Y , InsertPt.X+tol, InsertPt.Y);
+                if (Area.Contains(pt0.ToPoint()))
+                {
+                    return new LineSegment(InsertPt.X - insideSize, InsertPt.Y, InsertPt.X + outsideSize, InsertPt.Y);
+                }
+                else
+                {
+                    return new LineSegment(InsertPt.X - outsideSize, InsertPt.Y, InsertPt.X + insideSize, InsertPt.Y);
+                }
+            }
         }
     }
 }
