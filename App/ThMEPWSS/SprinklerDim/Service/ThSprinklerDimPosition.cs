12ef7f783924744f9ba7270b41244333ec93e165
huangjian <2545698879@qq.com>
2022-07-29 13:33:25 +0800

    分组

--- App/ThMEPWSS/SprinklerDim/Service/ThSprinklerDimPosition.cs
+++ App/ThMEPWSS/SprinklerDim/Service/ThSprinklerDimPosition.cs
@@ -100,56 +100,52 @@ namespace ThMEPWSS.SprinklerDim.Service
         //边界标注合并函数
         //输入参数：各组的边界点的列表bd，步长step
         //输出参数：合并后的点的列表BoundaryDimPosition，与能合并的组的编号
-        public static List<List<Point3d>> BoundaryMerge(List<Dictionary<long, List<Point3d>>> bd, double step, bool IsxAxis, out List<long> AllreadyDim, out List<int> id)
+        public static List<DimLinesGroup> BoundaryMerge(List<Dictionary<long, List<Point3d>>> bd, double step, bool IsxAxis, out List<long> AllreadyDim)
         {
             //处理可以合并的边界标注
-            List<List<Point3d>> BoundaryDimPosition = new();
+            List<DimLinesGroup> BoundaryDimPosition = new();
             List<long> AllreadyDims = new();
-            List<int> ids = new();
             for (int i = 0; i < bd.Count-1; i++) 
             {
-                List<int> idd = new();
-                idd.Add(i);
-                foreach(long key in bd[i].Keys)
+                DimLinesGroup t = new();
+                t.tag.Add(i);
+                foreach (long key in bd[i].Keys)
                 {
-                    List<Point3d> t = bd[i][key];
+                    t.Pts = bd[i][key];
                     for (int j = i + 1; j < bd.Count; j++)
                     {
                         if (bd[j].Keys.Contains(key))
                         {
-                            t = t.Concat(bd[j][key]).ToList();
+                            t.Pts = t.Pts.Concat(bd[j][key]).ToList();
                             bd[j].Remove(key);
-                            idd.Add(j);
+                            t.tag.Add(j);
                         }
                     }
-                    t = t.OrderBy(p => GetValue(p, IsxAxis)).ThenBy(q => GetValue(q, !IsxAxis)).ToList();
-                    for (int k = 0; k < t.Count - 1; k++)
+                    t.Pts = t.Pts.OrderBy(p => GetValue(p, IsxAxis)).ThenBy(q => GetValue(q, !IsxAxis)).ToList();
+                    for (int k = 0; k < t.Pts.Count - 1; k++)
                     {
-                        if (t[k].DistanceTo(t[k + 1]) > 2 * step)//判断是否大于两个步长
+                        if (t.Pts[k].DistanceTo(t.Pts[k + 1]) > 2 * step)//判断是否大于两个步长
                         {
-                            t = bd[i][key];
+                            t.Pts = bd[i][key];
                             break;
                         }
                     }
-                    if (t.Count != bd[i][key].Count)
+                    if (t.Pts.Count != bd[i][key].Count)
                     {
                         BoundaryDimPosition.Add(t);
-                        ids = ids.Concat(idd).ToList();
-                        for (int k = 0; k < t.Count; k++)
+                        for (int k = 0; k < t.Pts.Count; k++)
                         {
-                            if (!AllreadyDims.Contains(GetValue(t[k], !IsxAxis))) AllreadyDims.Add(GetValue(t[k], !IsxAxis));
+                            if (!AllreadyDims.Contains(GetValue(t.Pts[k], !IsxAxis))) AllreadyDims.Add(GetValue(t.Pts[k], !IsxAxis));
                         }
                     }
                 }
             }
-            id = ids;
             AllreadyDim = AllreadyDims;
             return BoundaryDimPosition;
         }
 
-        public static List<Point3d> GetProperFirstLine(ThSprinklerNetGroup netlist, bool IsxAxis)
+        public static DimLinesGroup GetProperFirstLine(List<Point3d> point3Ds, bool IsxAxis)
         {
-            List<Point3d> point3Ds = netlist.Pts;
             point3Ds = point3Ds.OrderBy(p => GetValue(p, IsxAxis)).ThenBy(q => GetValue(q, !IsxAxis)).ToList();
             List<long> all = new();
             foreach (Point3d pt in point3Ds)
@@ -157,9 +153,9 @@ namespace ThMEPWSS.SprinklerDim.Service
                 if (!all.Contains(GetValue(pt, IsxAxis))) all.Add(GetValue(pt, IsxAxis));
             }
 
-            List<Point3d> max = new();
-            List<Point3d> min = new();
-            List<Point3d> Lline = new();
+            DimLinesGroup max = new();
+            DimLinesGroup min = new();
+            DimLinesGroup Lline = new();
 
             Dictionary<long, List<Point3d>> count = new();
             List<long> longs = new();
@@ -179,22 +175,22 @@ namespace ThMEPWSS.SprinklerDim.Service
             {
                 if (count[value].Count == nummax) longs.Add(value);
             }
-            if (IsxAxis) Lline = count[longs.Min()];
+            if (IsxAxis) Lline.Pts = count[longs.Min()];
             else
             {
-                Lline = count[longs.Max()];
+                Lline.Pts = count[longs.Max()];
             }
-            max = count[all.Max()];
-            min = count[all.Min()];
+            max.Pts = count[all.Max()];
+            min.Pts = count[all.Min()];
 
             int bdnum1 = 0;
             int bdnum2 = 0;
             List<long> bd_1 = new();
             List<long> bd_2 = new();
             List<long> ll_ = new();
-            foreach (Point3d pt in max) bd_1.Add(GetValue(pt, !IsxAxis));
-            foreach (Point3d pt in min) bd_2.Add(GetValue(pt, !IsxAxis));
-            foreach (Point3d pt in Lline) ll_.Add(GetValue(pt, !IsxAxis));
+            foreach (Point3d pt in max.Pts) bd_1.Add(GetValue(pt, !IsxAxis));
+            foreach (Point3d pt in min.Pts) bd_2.Add(GetValue(pt, !IsxAxis));
+            foreach (Point3d pt in Lline.Pts) ll_.Add(GetValue(pt, !IsxAxis));
             foreach (long value in bd_1)
             {
                 if (ll_.Contains(value)) bdnum1++;
@@ -204,7 +200,7 @@ namespace ThMEPWSS.SprinklerDim.Service
                 if (ll_.Contains(value)) bdnum2++;
             }
 
-            if (Lline.Count > 3 * Math.Max(bdnum1, bdnum2)) return Lline;
+            if (Lline.Pts.Count > 3 * Math.Max(bdnum1, bdnum2)) return Lline;
             else
             {
                 if (bdnum1 > bdnum2) return max;
@@ -228,22 +224,23 @@ namespace ThMEPWSS.SprinklerDim.Service
         //将边缘线放入合并函数（BoundaryMerge）当中，能合并的合并，不能合并的选取一条最优的标注线
         //输入“边界点的列表bd”
         //输出“合并后的边界标注位置BoundaryDimPosition”
-        public static List<List<Point3d>> GetDimPoints(List<ThSprinklerNetGroup> netList, double step, bool IsxAxis)
+        public static List<DimLinesGroup> GetDimPoints(List<ThSprinklerNetGroup> netList, double step, bool IsxAxis)
         {
             List<Dictionary<long, List<Point3d>>> bd = new();//存储各组边界线
-            List<List<Point3d>> DimPoints = new();
-            List<ThSprinklerDimGroup> Groups = new();
+            List<DimLinesGroup> DimPoints = new();
+            List<List<ThSprinklerDimGroup>> Groups = new();
 
             for (int j = 0; j < netList.Count; j++)
             {
+                List<ThSprinklerDimGroup> iGroups = new();
                 List<Point3d> point3Ds = netList[j].Pts;
-                point3Ds = point3Ds.OrderBy(p => GetValue(p, IsxAxis)).ThenBy(q => GetValue(q, !IsxAxis)).ToList();
+
+                //提取所有的要标注方向落在另一方向的值
                 List<long> all = new();
                 foreach (Point3d pt in point3Ds)
                 {
                     if (!all.Contains(GetValue(pt, IsxAxis))) all.Add(GetValue(pt, IsxAxis));
                 }
-
                 Dictionary<long, List<Point3d>> count = new();
                 foreach(long value in all)
                 {
@@ -255,20 +252,42 @@ namespace ThMEPWSS.SprinklerDim.Service
                     }
                     count.Add(value, t);
                 }
-                
-                foreach(long key in count.Keys)
+
+                //提取所有标注方向的值
+                List<long> R_all = new();
+                foreach (Point3d pt in point3Ds)
                 {
-                    List<Point3d> ts = count[key];
-                    while(ts.Count > 0)
+                    if (!R_all.Contains(GetValue(pt, !IsxAxis))) R_all.Add(GetValue(pt, !IsxAxis));
+                }
+                Dictionary<long, List<Point3d>> R_count = new();
+
+                foreach (long value in R_all)
+                {
+                    List<Point3d> t = new();
+                    for (int i = 0; i < point3Ds.Count; i++)
                     {
-                        ThSprinklerDimGroup group = new();
-                        ts = group.GetPoints(ts);
-                        Groups.Add(group);
+                        var pt = point3Ds[i];
+                        if (GetValue(pt, !IsxAxis) == value) t.Add(pt);
                     }
+                    R_count.Add(value, t);
                 }
 
-                List<Point3d> max = new();
-                List<Point3d> min = new();
+                //按照标注方向分组
+                foreach (long value in R_all)
+                {
+                    List<Point3d> ts = R_count[value];
+                    while (true) 
+                    {
+                        ThSprinklerDimGroup group = new();
+                        group.STol = 2.0 * step;
+                        ts = group.GetPoints(ts);
+                        if (ts == null) break;
+                        group.tag = value;
+                        iGroups.Add(group);
+                    }
+                }
+                Groups.Add(iGroups);
+
                 Dictionary<long, List<Point3d>> bdcount = new();
                 bdcount.Add(all.Min(), count[all.Min()]);
                 if (all.Count != 1) bdcount.Add(all.Max(), count[all.Max()]);
@@ -276,16 +295,76 @@ namespace ThMEPWSS.SprinklerDim.Service
             }
 
             List<long> allreadydim = new();
-            List<int> index = new();
-            DimPoints = DimPoints.Concat(BoundaryMerge(bd, step, IsxAxis, out allreadydim, out index)).ToList();
+            DimPoints = DimPoints.Concat(BoundaryMerge(bd, step, IsxAxis, out allreadydim)).ToList();
+
 
             for (int j = 0; j < netList.Count; j++)
             {
-                if (index.Contains(j)) continue;
-                DimPoints.Add(GetProperFirstLine(netList[j], IsxAxis));
+                if (DimPoints.Where(p => p.tag.Contains(j)) != null) continue;
+                DimLinesGroup t = GetProperFirstLine(netList[j].Pts, IsxAxis);
+                t.tag.Add(j);
+                DimPoints.Add(t);
             }
 
+            bool isdone = false;
+            while (!isdone) 
+            {
+                //标明已经标注的组
+                for (int i = 0; i < Groups.Count; i++) 
+                {
+                    foreach (ThSprinklerDimGroup group in Groups[i]) 
+                    {
+                        if (group.IsDim == true) continue;
+                        for (int m = 0; m < DimPoints.Count; m++)
+                        {
+                            bool UnStol = group.GetMinDis(DimPoints[m].Pts) < group.STol;
+                            bool IsDimed = false;
+                            foreach (Point3d pt in DimPoints[m].Pts)
+                            {
+                                if (IsDimed = group.Contains(pt)) break;
+                            }
 
+                            if (IsDimed)
+                            {
+                                group.IsDim = true;                             //已经有标注的(满足共线误差）
+                                break;
+                            }
+                            else if (UnStol && !IsDimed && DimPoints[m].tag.Contains(i))                        //在Stol距离之内但是没有标注的（在标注线上增加点位）
+                            {
+                                DimPoints[m].Pts.Add(group.GetProj(DimPoints[m].Pts));
+                                group.IsDim = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                for (int k = 0; k < Groups.Count; k++) 
+                {
+                    //更新未标注的组，重复找第一条标注的过程，直到所有的组都已经标注完
+                    List<ThSprinklerDimGroup> LoopGroups = new();
+                    for (int i = 0; i < Groups[k].Count; i++)
+                    {
+                        if (Groups[k][i].IsDim == false) LoopGroups.Add(Groups[k][i]);
+                    }
+                    List<Point3d> LoopPts = new();
+                    foreach (ThSprinklerDimGroup group in LoopGroups)
+                    {
+                        LoopPts = LoopPts.Concat(group.Pts).ToList();
+                    }
+                    if (LoopGroups.Count == 0) continue;
+                    DimLinesGroup t = GetProperFirstLine(LoopPts, IsxAxis);
+                    t.tag.Add(k);
+                    DimPoints.Add(t);
+                }
+
+                foreach (List<ThSprinklerDimGroup> groups in Groups)
+                {
+                    foreach (ThSprinklerDimGroup group in groups)
+                    {
+                        if (!(isdone = group.IsDim)) break;
+                    }
+                }
+            }
 
             return DimPoints;
         }
@@ -299,13 +378,13 @@ namespace ThMEPWSS.SprinklerDim.Service
             for (int i = 0; i < DimPosition.Count; i++)
             {
                 List<Line> DimLines = new();
-                DimPosition[i] = DimPosition[i].OrderBy(p => (long)p.X / 10).ThenBy(q => (long)q.Y / 10).ToList();
-                if (DimPosition[i].Count == 1) continue;
-                for (int j = 0; j < DimPosition[i].Count - 1; j++)
+                DimPosition[i].Pts = DimPosition[i].Pts.OrderBy(p => (long)p.X / 10).ThenBy(q => (long)q.Y / 10).ToList();
+                if (DimPosition[i].Pts.Count == 1) continue;
+                for (int j = 0; j < DimPosition[i].Pts.Count - 1; j++)
                 {
-                    DimLines.Add(new Line(DimPosition[i][j], DimPosition[i][j + 1]));
+                    DimLines.Add(new Line(DimPosition[i].Pts[j], DimPosition[i].Pts[j + 1]));
                 }
-                DrawUtils.ShowGeometry(DimLines, "huang_test_" + printag, 5);
+                DrawUtils.ShowGeometry(DimLines, "huang_test_" + printag, 1, 50);
             }
             //for (int i = 0; i < DimPosition.Count; i++)
             //{
