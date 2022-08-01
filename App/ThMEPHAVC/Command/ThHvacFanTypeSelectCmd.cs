6694f991c3ecdcf45fd2a290af2bb45b4c89a8ca
董士崇 <dongshichong@thape.com.cn>
2022-08-01 17:14:54 +0800

    THFJXXCK command fix randome crash (memory issue)

--- App/ThMEPHAVC/Command/ThHvacFanTypeSelectCmd.cs
+++ App/ThMEPHAVC/Command/ThHvacFanTypeSelectCmd.cs
@@ -40,10 +40,10 @@ namespace ThMEPHVAC.Command
                 blockName = ThFanSelectionEngine.BlockLayerName(pFanModel, out layerName);
                 LoadBlockLayerToDocument(acdb.Database);
             }
-            var thisIdBlocks = eqpmDocument.GetDocumentFanBlocks(pFanModel);
-            var needFanCount = pFanModel.ListVentQuan.Count;
             using (AcadDatabase acadDatabase = AcadDatabase.Active())
             {
+                var needFanCount = pFanModel.ListVentQuan.Count;
+                var thisIdBlocks = eqpmDocument.GetDocumentFanBlocks(pFanModel);
                 if (thisIdBlocks.Count == 0)
                 {
                     // 场景1：若检测到图纸中没有对应的风机图块
@@ -54,40 +54,44 @@ namespace ThMEPHVAC.Command
                 {
                     // 场景2：若检测到图纸中有对应的风机图块，但图块数量不同
                     // 删除已经生成的块，重新进入插块模式
-                    thisIdBlocks.ForEach(o =>
+                    foreach (ObjectId objId in thisIdBlocks)
                     {
-                        o.Id.RemoveModel();
-                    });
+                        objId.RemoveModel();
+                    }
                     ThFanSelectionEngine.InsertModels(pFanModel, cFanModel);
                 }
                 else if (thisIdBlocks.Count == needFanCount)
                 {
                     // 场景3：若检测到图纸中有对应的风机图块，且图块数量相同
-                    var block = thisIdBlocks.First();
+                    var thisBlocks = thisIdBlocks
+                        .OfType<ObjectId>()
+                        .Select(o => acadDatabase.Element<BlockReference>(o))
+                        .ToList();
+                    var blockId = thisIdBlocks[0];
                     var allModels = eqpmDocument.DocumentFanToFanModels(thisIdBlocks);
                     var blockPModel = allModels.First();
                     if (FanSelectCheck.IsModelStyleChanged(blockPModel, pFanModel))
                     {
                         // 风机形式变化
                         // 删除已经生成的块，重新进入插块模式
-                        thisIdBlocks.ForEach(o =>
+                        foreach(ObjectId objId in thisIdBlocks)
                         {
-                            o.Id.RemoveModel();
-                        });
+                            objId.RemoveModel();
+                        }
                         ThFanSelectionEngine.InsertModels(pFanModel, cFanModel);
                     }
                     else if (FanSelectCheck.IsModelBlockNameChanged(blockPModel, pFanModel))
                     {
                         // 风机图块变化
-                        ThFanSelectionEngine.ReplaceModelsInplace(thisIdBlocks, pFanModel, cFanModel);
+                        ThFanSelectionEngine.ReplaceModelsInplace(thisBlocks, pFanModel, cFanModel);
                     }
                     else
                     {
                         // 风机规格和型号变化
-                        if (FanSelectCheck.IsModelNameChanged(block.Id, pFanModel))
+                        if (FanSelectCheck.IsModelNameChanged(blockId, pFanModel))
                         {
                             //需要更新xdata，将xdata中的风机型号名称改掉
-                            ThFanSelectionEngine.ModifyModelNames(thisIdBlocks, pFanModel);
+                            ThFanSelectionEngine.ModifyModelNames(thisBlocks, pFanModel);
                         }
 
                         // 风机编号变化
@@ -100,33 +104,43 @@ namespace ThMEPHVAC.Command
 
                         // 风机楼层变化
                         var storey = blockPModel.InstallFloor;
-                        var modelNumber = block.Id.GetModelNumber();
-                        var storeyNumber = block.Id.GetStoreyNumber();
+                        var modelNumber = blockId.GetModelNumber();
+                        var storeyNumber = blockId.GetStoreyNumber();
                         if (storeyNumber != EQPMFanCommon.StoreyNumber(storey, modelNumber.ToString()))
                         {
                             ThFanSelectionEngine.ModifyModelNumbers(pFanModel, cFanModel);
                         }
 
                         // 参数变化
-                        var blockReference = new ThBlockReferenceData(block.Id);
+                        var blockReference = new ThBlockReferenceData(blockId);
                         var attributes = new Dictionary<string, string>(blockReference.Attributes);
                         if (FanSelectCheck.IsAttributeModified(pFanModel.Attributes(), attributes))
                         {
                             // Workaround:
                             //  若“服务区域”发生变化，我们需要通过调整“翻转状态1”使其文字对齐
-                            var oldState = ThFanSelectionEngine.GetModelRotateState(thisIdBlocks);
-                            ThFanSelectionEngine.ResetModelRotateState(thisIdBlocks);
-                            ThFanSelectionEngine.SetModelRotateState(thisIdBlocks, oldState);
-                            ThFanSelectionEngine.ModifyModels(thisIdBlocks, pFanModel.Attributes());
+                            var oldState = ThFanSelectionEngine.GetModelRotateState(thisBlocks);
+                            ThFanSelectionEngine.ResetModelRotateState(thisBlocks);
+                            ThFanSelectionEngine.SetModelRotateState(thisBlocks, oldState);
+                            ThFanSelectionEngine.ModifyModels(thisBlocks, pFanModel.Attributes());
                         }
                     }
-                    thisIdBlocks = eqpmDocument.GetDocumentFanBlocks(pFanModel);
+
                     // 导航到图块
-                    Active.Editor.ZoomToObjects(thisIdBlocks.ToArray(), 2.0);
-                    Active.Editor.PickFirstObjects(thisIdBlocks.Select(o => o.ObjectId).ToArray());
+                    PickFirstModel(pFanModel);
                 }
             }
         }
+
+        private void PickFirstModel(FanDataModel dataModel)
+        {
+            var thisIdBlocks = eqpmDocument.GetDocumentFanBlocks(dataModel);
+            if (thisIdBlocks.Count > 0)
+            {
+                Active.Editor.ZoomToObjects(thisIdBlocks, 2.0);
+                Active.Editor.PickFirstObjects(thisIdBlocks.ToArray());
+            }
+        }
+
         public void LoadBlockLayerToDocument(Database database)
         {
             using (AcadDatabase currentDb = AcadDatabase.Use(database))
