f148b4d039aa3e98fd2af24229f9a46bb22524a3
jackwhoknow <trasure@126.com>
2022-08-05 15:07:18 +0800

    support TCH walls and TCH doors (#3174)

--- App/ThMEPTCH/CAD/ThTCHArchWallExtractionVisitor.cs
+++ App/ThMEPTCH/CAD/ThTCHArchWallExtractionVisitor.cs
@@ -3,11 +3,27 @@ using ThMEPEngineCore.Algorithm;
 using Autodesk.AutoCAD.Geometry;
 using System.Collections.Generic;
 using Autodesk.AutoCAD.DatabaseServices;
+using ThMEPTCH.TCHArchDataConvert.TCHArchTables;
+using ThMEPTCH.TCHArchDataConvert;
+using ThCADExtension;
 
 namespace ThMEPTCH.CAD
 {
     public class ThTCHArchWallExtractionVisitor : ThBuildingElementExtractionVisitor
     {
+        public override bool IsBuildElementBlock(BlockTableRecord blockTableRecord)
+        {
+            // 忽略图纸空间
+            if (blockTableRecord.IsLayout)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
         public override void DoExtract(List<ThRawIfcBuildingElementData> elements, Entity dbObj, Matrix3d matrix)
         {
             elements.AddRange(HandleTCHElement(dbObj, matrix));
@@ -15,7 +31,12 @@ namespace ThMEPTCH.CAD
 
         public override void DoXClip(List<ThRawIfcBuildingElementData> elements, BlockReference blockReference, Matrix3d matrix)
         {
-            //throw new NotImplementedException();
+            var xclip = blockReference.XClipInfo();
+            if (xclip.IsValid)
+            {
+                xclip.TransformBy(matrix);
+                elements.RemoveAll(o => !xclip.Contains(o.Geometry as Curve));
+            }
         }
 
         public override bool IsBuildElement(Entity e)
@@ -31,14 +52,31 @@ namespace ThMEPTCH.CAD
         private List<ThRawIfcBuildingElementData> HandleTCHElement(Entity tch, Matrix3d matrix)
         {
             var results = new List<ThRawIfcBuildingElementData>();
-            if (IsBuildElement(tch) && CheckLayerValid(tch))
+            if (IsBuildElement(tch) && CheckLayerValid(tch) && tch.Visible && tch.Bounds.HasValue)
             {
+                var archWall = tch.Database.LoadWallFromDb(tch.ObjectId);
+                var outline = CreateOutline(archWall);
+                outline.TransformBy(matrix);
                 results.Add(new ThRawIfcBuildingElementData()
                 {
-                    Data = tch.Database.LoadWallFromDb(tch.ObjectId),
+                    Data = archWall,
+                    Geometry = outline,
                 });
             }
             return results;
+        }
+
+        private Polyline CreateOutline(TArchWall archWall)
+        {
+            var wallEntity = DBToTHEntityCommon.TArchWallToEntityWall(archWall,0,0,0,0,new Vector3d(0,0,0));
+            if(wallEntity.OutLine !=null)
+            {
+                return wallEntity.OutLine.Shell();
+            }
+            else
+            {
+                return new Polyline() { Closed = true };
+            }
         }
     }
 }
