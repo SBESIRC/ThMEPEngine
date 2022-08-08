424e80bdacaca3af11cc14e43167a847fbea11c0
susns <44757869+susns@users.noreply.github.com>
2022-08-08 15:05:57 +0800

    Feature/sprinkler dim sss (#3184)
    * clean code
    
    * 找靠墙标注点

--- App/ThMEPWSS/SprinklerDim/Service/ThSprinklerNetGroupListService.cs
+++ App/ThMEPWSS/SprinklerDim/Service/ThSprinklerNetGroupListService.cs
@@ -5,11 +5,166 @@ using Autodesk.AutoCAD.Geometry;
 using ThMEPWSS.SprinklerDim.Model;
 using Autodesk.AutoCAD.DatabaseServices;
 using ThMEPEngineCore.Diagnostics;
+using ThCADCore.NTS;
+using ThCADExtension;
 
 namespace ThMEPWSS.SprinklerDim.Service
 {
     public class ThSprinklerNetGroupListService
     {
+        /// <summary>
+        /// 房间重新分隔出net group
+        /// </summary>
+        /// <param name="netList"></param>
+        /// <param name="rooms"></param>
+        /// <returns></returns>
+        public static List<ThSprinklerNetGroup> ReGroupByRoom(List<ThSprinklerNetGroup> netList, List<Polyline> rooms, string printTag)
+        {
+            List<MPolygon> roomList = PreprocessRooms(rooms);
+
+            List<ThSprinklerNetGroup> newNetList = new List<ThSprinklerNetGroup>();
+            if (roomList.Count > 0)
+            {
+                foreach (ThSprinklerNetGroup net in netList)
+                {
+                    // 获取所有线
+                    DBObjectCollection lines = new DBObjectCollection();
+                    foreach (ThSprinklerGraph graph in net.PtsGraph)
+                    {
+                        foreach (Line l in graph.Print(net.Pts))
+                            lines.Add(l);
+                    }
+                    ThCADCoreNTSSpatialIndex linesSI = new ThCADCoreNTSSpatialIndex(lines);
+
+                    // 房间框线框住的线重新生成net group
+                    for (int i = 0; i < roomList.Count; i++)
+                    {
+                        MPolygon room = roomList[i];
+                        if (room == null)
+                            continue;
+
+                        // 获取window line
+                        List<Line> selectWindowLines = new List<Line>();
+                        DBObjectCollection dbSelect = linesSI.SelectWindowPolygon(room);
+                        foreach (DBObject dbo in dbSelect)
+                        {
+                            selectWindowLines.Add((Line)dbo);
+                        }
+
+                        if (selectWindowLines.Count > 0)
+                        {
+                            // 获取fence line
+                            List<Line> selectFenceLines = new List<Line>();
+                            DBObjectCollection tdbSelect = linesSI.SelectFence(room);
+                            foreach (DBObject dbo in tdbSelect)
+                            {
+                                selectFenceLines.Add((Line)dbo);
+                            }
+
+                            // 加入满足首尾两点被框进房间的fence line到window line
+                            if (selectFenceLines.Count > 0)
+                            {
+                                foreach (Line l in selectFenceLines)
+                                {
+                                    if (IsContained(room, l.StartPoint) && IsContained(room, l.EndPoint))
+                                    {
+                                        selectWindowLines.Add(l);
+                                    }
+                                }
+
+                            }
+
+                            newNetList.Add(ThSprinklerNetGraphService.CreateNetwork(net.Angle, selectWindowLines));
+                        }
+
+                    }
+
+                }
+
+            }
+            else
+                newNetList = netList;
+
+
+            //test
+            for (int i = 0; i < newNetList.Count; i++)
+            {
+                var net = newNetList[i];
+                List<Point3d> pts = ThChangeCoordinateService.MakeTransformation(net.Pts, net.Transformer.Inverse());
+                for (int j = 0; j < net.PtsGraph.Count; j++)
+                {
+                    var lines = net.PtsGraph[j].Print(pts);
+                    DrawUtils.ShowGeometry(lines, string.Format("SSS-{2}-1Room-{0}-{1}", i, j, printTag), i % 7);
+                }
+            }
+
+
+            return newNetList;
+        }
+
+
+        private static List<MPolygon> PreprocessRooms(List<Polyline> rooms)
+        {
+            // 按面积从大到小排房间
+            List<MPolygon> roomList = new List<MPolygon>();
+            foreach (Polyline room in rooms)
+            {
+                MPolygon mPolygon = ThMPolygonTool.CreateMPolygon(room);
+                roomList.Add(mPolygon);
+            }
+            roomList.Sort((x, y) => 0 - x.Area.CompareTo(y.Area));
+
+            for (int i = 0; i < roomList.Count; i++)
+            {
+                for (int j = i + 1; j < roomList.Count; j++)
+                {
+                    // DrawUtils.ShowGeometry(roomList[i], "SSS-1ROOM-" + i.ToString()+"-"+j.ToString());
+
+                    DBObjectCollection dboc = new DBObjectCollection() { roomList[j] };
+                    DBObjectCollection dbocd = roomList[i].DifferenceMP(dboc);
+
+                    if (dbocd.Count > 0)
+                    {
+                        if (dbocd.OfType<MPolygon>().Count() > 0)
+                        {
+                            roomList[i] = dbocd.OfType<MPolygon>().OrderByDescending(x => x.Area).FirstOrDefault();
+                        }
+                        else
+                        {
+                            roomList[i] = ThMPolygonTool.CreateMPolygon(dbocd.OfType<Polyline>().FirstOrDefault());
+                        }
+                    }
+                    else
+                    {
+                        roomList[i] = null;
+                        break;
+                    }
+
+                }
+            }
+
+            return roomList;
+        }
+
+        private static bool IsContained(MPolygon room, Point3d pt)
+        {
+            if (!ThCADCoreNTSPolygonExtension.Contains(room.Shell(), pt))
+            {
+                return false;
+            }
+
+            List<Polyline> holes = room.Holes();
+            foreach (Polyline hole in holes)
+            {
+                if (ThCADCoreNTSPolygonExtension.Contains(hole, pt))
+                    return false;
+            }
+
+            return true;
+        }
+
+
+
         /// <summary>
         /// 把喷淋区转换到正交坐标系
         /// </summary>
@@ -35,7 +190,7 @@ namespace ThMEPWSS.SprinklerDim.Service
 
 
         /// <summary>
-        /// 断开正交坐标系下容差45mm以上的线
+        /// 正交坐标系下容差45mm以上视为不共线，需要断开
         /// </summary>
         /// <param name="transNetList"></param>
         /// <param name="tolerance"></param>
@@ -303,26 +458,80 @@ namespace ThMEPWSS.SprinklerDim.Service
         }
 
 
-        //public static List<List<int>> SeperateLine(List<Point3d> pts, List<int> line, bool isXAxis, double step)
-        //{
-        //    line.Sort((x, y) => ThChangeCoordinateService.GetOriginalValue(pts[x], isXAxis).CompareTo(ThChangeCoordinateService.GetOriginalValue(pts[y], isXAxis)));
-        //    List<List<int>> lines = new List<List<int>>();
+        /// <summary>
+        /// 根据墙断开不能连接的线，并记录下来
+        /// </summary>
+        /// <param name="transNetList"></param>
+        /// <param name="walls"></param>
+        /// <param name="printTag"></param>
+        public static void CutOffLinesCrossWall(List<ThSprinklerNetGroup> transNetList, List<Polyline> walls, string printTag)
+        {
+            for (int idx = 0; idx < transNetList.Count; idx++)
+            {
+                ThSprinklerNetGroup net = transNetList[idx];
+                net.LinesCuttedOffByWall.Clear();
+                List<Point3d> pts = ThChangeCoordinateService.MakeTransformation(net.Pts, net.Transformer.Inverse());
 
-        //    List<int> one = new List<int> { line[0] };
-        //    for(int i = 1; i < line.Count; i++)
-        //    {
-        //        int iPtIndex = one[one.Count - 1];
-        //        int jPtIndex = line[i];
-        //        if (ThChangeCoordinateService.GetOriginalValue(pts[jPtIndex], isXAxis) - ThChangeCoordinateService.GetOriginalValue(pts[iPtIndex], isXAxis) > 1.5 * step)
-        //        {
-        //            lines.Add(one);
-        //            one = new List<int> { jPtIndex };
-        //        }
-        //    }
-        //    lines.Add(one);
+                //生成所有线（图）
+                DBObjectCollection graphLines = new DBObjectCollection();
+                foreach (ThSprinklerGraph graph in net.PtsGraph)
+                {
+                    foreach (Line l in graph.Print(pts))
+                        graphLines.Add(l);
+                }
+                ThCADCoreNTSSpatialIndex graphLinesSI = new ThCADCoreNTSSpatialIndex(graphLines);
 
-        //    return lines;
-        //}
+                //找出图与墙相交线
+                List<Line> crossWallLines = new List<Line>();
+                foreach (Polyline wall in walls)
+                {
+                    DBObjectCollection dbSelect = graphLinesSI.SelectFence(wall);
+                    foreach (DBObject dbo in dbSelect)
+                    {
+                        crossWallLines.Add((Line)dbo);
+                    }
+
+                }
+
+                //判断相交线是否需要断开
+                foreach (Line line in crossWallLines)
+                {
+                    if (ThSprinklerDimConflictService.IsConflicted(line, walls))
+                    {
+                        int i = SearchIndex(pts, line.StartPoint);
+                        int j = SearchIndex(pts, line.EndPoint);
+                        net.LinesCuttedOffByWall.Add(new Tuple<int, int>(i, j));
+                        net.LinesCuttedOffByWall.Add(new Tuple<int, int>(j, i));
+
+                        foreach (ThSprinklerGraph graph in net.PtsGraph)
+                        {
+                            graph.DeleteEdge(i, j);
+                            graph.DeleteEdge(j, i);
+                        }
+                    }
+
+                }
+
+            }
+
+            //test
+            for (int i = 0; i < transNetList.Count; i++)
+            {
+                var net = transNetList[i];
+                List<Point3d> pts = ThChangeCoordinateService.MakeTransformation(net.Pts, net.Transformer.Inverse());
+                for (int j = 0; j < net.PtsGraph.Count; j++)
+                {
+                    var lines = net.PtsGraph[j].Print(pts);
+                    DrawUtils.ShowGeometry(lines, string.Format("SSS-{2}-4Wall-{0}-{1}", i, j, printTag), i % 7);
+                }
+            }
+        }
+
+        private static int SearchIndex(List<Point3d> pts, Point3d pt)
+        {
+            return pts.IndexOf(pt);
+        }
+
 
     }
 }
