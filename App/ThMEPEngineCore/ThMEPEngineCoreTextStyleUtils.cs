d7926adda3e259752d2641eb7d8abc448841bab5
董士崇 <dongshichong@thape.com.cn>
2022-08-09 15:02:01 +0800

    THKJTQ and THKJMCTQ import TH-STYLE3 when missing

---
+++ App/ThMEPEngineCore/ThMEPEngineCoreTextStyleUtils.cs
@@ -0,0 +1,18 @@
+﻿using Linq2Acad;
+using ThCADExtension;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace ThMEPEngineCore
+{
+    public static class ThMEPEngineCoreTextStyleUtils
+    {
+        public static ObjectId ImportTextStyle(this Database database, string textStyle)
+        {
+            using (AcadDatabase acadDatabase = AcadDatabase.Use(database))
+            using (AcadDatabase blockDb = AcadDatabase.Open(ThCADCommon.StoreyFrameDwgPath(), DwgOpenMode.ReadOnly, false))
+            {
+                return acadDatabase.TextStyles.Import(blockDb.TextStyles.ElementOrDefault(textStyle), false).Item.ObjectId;
+            }
+        }
+    }
+}
