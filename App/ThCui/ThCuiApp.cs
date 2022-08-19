30ec2c392c6598ca84e9dbe78d6b125b9f657115
董士崇 <dongshichong@thape.com.cn>
2022-08-19 19:19:53 +0800

    Disable id check

--- App/ThCui/ThCuiApp.cs
+++ App/ThCui/ThCuiApp.cs
@@ -62,7 +62,7 @@ namespace TianHua.AutoCAD.ThCui
             //}
 
             //注册DocumentCollection事件
-            AcadApp.DocumentManager.DocumentLockModeChanged += DocCollEvent_DocumentLockModeChanged_Handler;
+            //AcadApp.DocumentManager.DocumentLockModeChanged += DocCollEvent_DocumentLockModeChanged_Handler;
             ////注册SystemVariableChanged 事件
             //AcadApp.SystemVariableChanged += SystemVariableChangedHandler;
 
@@ -111,7 +111,7 @@ namespace TianHua.AutoCAD.ThCui
             //}
 
             //反注册DocumentCollection事件
-            AcadApp.DocumentManager.DocumentLockModeChanged -= DocCollEvent_DocumentLockModeChanged_Handler;
+            //AcadApp.DocumentManager.DocumentLockModeChanged -= DocCollEvent_DocumentLockModeChanged_Handler;
             ////反注册SystemVariableChanged 事件
             //AcadApp.SystemVariableChanged -= SystemVariableChangedHandler;
         }
