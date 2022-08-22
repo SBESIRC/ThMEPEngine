7907e340a24b3730c71532aa348f7de2d2b1c198
董士崇 <dongshichong@thape.com.cn>
2022-08-22 09:55:34 +0800

    run-protoc support ruby

--- App/run-protoc.ps1
+++ App/run-protoc.ps1
@@ -4,13 +4,25 @@ function Get-ProtocLocation
     return Join-Path -Path $root -ChildPath "Tools\protoc-21.5-win64\bin\protoc.exe"
 }
 
-Task Compile.Protoc {
+Task CSharp {
+    $script:out= "csharp"
+}
+
+Task Ruby {
+    $script:out= "ruby"
+}
+
+Task Compile {
     $protoc = Get-ProtocLocation
     $path = "$PSScriptRoot\ThMEPTCH\proto"
     Push-Location $path
     foreach($proto in Get-ChildItem "$path" -Recurse -Include "*.proto") {
         exec {
-            & $protoc --csharp_out=".\" (Get-Item $proto).Name
+            if ($script:out -eq "csharp") {
+                & $protoc --csharp_out=".\" (Get-Item $proto).Name
+            } elseif ($script:out -eq "ruby") {
+                & $protoc --ruby_out=".\" (Get-Item $proto).Name
+            }
         }
     }
     Pop-Location
