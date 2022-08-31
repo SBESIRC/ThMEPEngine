0562728866995bd09280c85e905e995ecd2486d6
刘光胜 <trasure@126.com>
2022-08-31 16:01:22 +0800

    define ThDrawRailingCmd

---
+++ App/ThPlatform3D/Command/ThDrawRailingCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThMEPEngineCore.Command;
+
+namespace ThPlatform3D.Command
+{
+    public class ThDrawRailingCmd : ThMEPBaseCommand
+    {
+        public ThDrawRailingCmd()
+        {
+            CommandName = "THDrawRailing";
+            ActionName = "绘制栏杆线";
+        }
+
+        public override void SubExecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
