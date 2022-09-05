4c04e131f006009f1f629594c7f69ebd9d32e019
zyk-work <81728112+zyk-work@users.noreply.github.com>
2022-09-05 11:39:35 +0800

    IFCEntityId Add FloorNum (#3339)
    Co-authored-by: 赵玉康 <zhaoyukang@thape.com.cn>

--- App/ThMEPTCH/Model/ThTCHSlab.cs
+++ App/ThMEPTCH/Model/ThTCHSlab.cs
@@ -1,12 +1,13 @@
 ﻿using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using ProtoBuf;
+using System;
 using System.Collections.Generic;
 
 namespace ThMEPTCH.Model
 {
     [ProtoContract]
-    public class ThTCHSlab : ThTCHElement
+    public class ThTCHSlab : ThTCHElement, ICloneable
     {
         /// <summary>
         /// 降板信息
@@ -38,6 +39,31 @@ namespace ThMEPTCH.Model
             Height = thickness;
             ExtrudedDirection = extVector;
             Descendings = new List<ThTCHSlabDescendingData>();
+        }
+
+        public object Clone()
+        {
+            var clone = new ThTCHSlab();
+            clone.Descendings = new List<ThTCHSlabDescendingData>();
+            clone.Uuid = this.Uuid;
+            clone.Useage = this.Useage;
+            clone.ZOffSet = this.ZOffSet;
+            clone.ExtrudedDirection = this.ExtrudedDirection;
+            clone.Height = this.Height;
+            if (this.Outline != null)
+                clone.Outline = this.Outline.Clone() as Entity;
+            if (null != this.Descendings) 
+            {
+                foreach (var item in this.Descendings)
+                {
+                    clone.Descendings.Add(item.Clone() as ThTCHSlabDescendingData);
+                }
+            }
+            foreach (var item in this.Properties)
+            {
+                clone.Properties.Add(item.Key,item.Value);
+            }
+            return clone;
         }
     }
 }
