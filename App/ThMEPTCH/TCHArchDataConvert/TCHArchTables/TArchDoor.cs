77f7266f610e72e7376efafa2060f5fe6b26c147
liupan-AI <1750923@tongji.edu.cn>
2022-09-14 10:21:59 +0800

    Modify TCH door type (#3392)
    Co-authored-by: 刘潘 <liupan1@thape.com.cn>

--- App/ThMEPTCH/TCHArchDataConvert/TCHArchTables/TArchDoor.cs
+++ App/ThMEPTCH/TCHArchDataConvert/TCHArchTables/TArchDoor.cs
@@ -8,11 +8,37 @@ namespace ThMEPTCH.TCHArchDataConvert.TCHArchTables
         /// <summary>
         /// 平开门
         /// </summary>
-        SWING,
+        SWING0001 = 1,
+        SWING0002 = 2,
+        SWING0003 = 3,
+        SWING0004 = 4,
+        SWING0009 = 9,
+        SWING0010 = 10,
+        SWING0011 = 11,
+        SWING0012 = 12,
+        SWING0021 = 21,
+        SWING0114 = 114,
+        SWING0116 = 116,
+        SWING0222 = 222,
+        SWING0223 = 223,
+        SWING0224 = 224,
+        SWING0225 = 225,
+        SWING0226 = 226,
+        SWING0228 = 228,
+        SWING0231 = 231,
+
         /// <summary>
         /// 推拉门
         /// </summary>
-        SLIDING,
+        SLIDING0127 = 127,
+        SLIDING0128 = 128,
+        SLIDING0129 = 129,
+        SLIDING0130 = 130,
+        SLIDING0131 = 131,
+        SLIDING0132 = 132,
+        SLIDING0134 = 134,
+        SLIDING0135 = 135,
+        SLIDING0138 = 138,
     }
 
     public enum SwingEnum
