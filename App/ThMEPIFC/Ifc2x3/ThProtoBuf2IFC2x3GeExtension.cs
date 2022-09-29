1a15f5fe3630573e524cf21bd3f385e3421bf484
董士崇 <dongshichong@thape.com.cn>
2022-09-29 15:55:15 +0800

    Implement CreateProxy with ThSUPolygonMesh
    (cherry picked from commit 0c94a685d9981801c81ff9045622eaf34de2f4f5)

---
+++ App/ThMEPIFC/Ifc2x3/ThProtoBuf2IFC2x3GeExtension.cs
@@ -0,0 +1,33 @@
+﻿using Xbim.Ifc;
+using Xbim.Ifc2x3.GeometryResource;
+using Xbim.Ifc2x3.GeometricConstraintResource;
+
+namespace ThMEPIFC.Ifc2x3
+{
+    public static class ThProtoBuf2IFC2x3GeExtension
+    {
+        public static IfcCartesianPoint ToIfcCartesianPoint(this IfcStore model, ThTCHPoint3d point)
+        {
+            var pt = model.Instances.New<IfcCartesianPoint>();
+            pt.SetXYZ(point.X, point.Y, point.Z);
+            return pt;
+        }
+
+        public static IfcAxis2Placement3D ToIfcAxis2Placement3D(this IfcStore model, ThTCHPoint3d point)
+        {
+            var placement = model.Instances.New<IfcAxis2Placement3D>();
+            placement.Location = model.ToIfcCartesianPoint(point);
+            return placement;
+        }
+
+        public static IfcLocalPlacement ToIfcLocalPlacement(this IfcStore model, 
+            ThTCHPoint3d origin, IfcObjectPlacement relative_to = null)
+        {
+            return model.Instances.New<IfcLocalPlacement>(l =>
+            {
+                l.PlacementRelTo = relative_to;
+                l.RelativePlacement = model.ToIfcAxis2Placement3D(origin);
+            });
+        }
+    }
+}
