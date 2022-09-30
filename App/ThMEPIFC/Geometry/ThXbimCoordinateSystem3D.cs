1d0f5191f2162a08afef5019331455c2809f81b4
董士崇 <dongshichong@thape.com.cn>
2022-09-30 14:36:23 +0800

    Implement ThXbimCoordinateSystem3D

---
+++ App/ThMEPIFC/Geometry/ThXbimCoordinateSystem3D.cs
@@ -0,0 +1,16 @@
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Spatial.Euclidean;
+using Xbim.Common.Geometry;
+
+namespace ThMEPIFC.Geometry
+{
+    public class ThXbimCoordinateSystem3D
+    {
+        public CoordinateSystem CS { get; private set; }
+
+        public ThXbimCoordinateSystem3D(XbimMatrix3D m)
+        {
+            CS = new CoordinateSystem(new DenseMatrix(4, 4, m.ToDoubleArray()));
+        }
+    }
+}
