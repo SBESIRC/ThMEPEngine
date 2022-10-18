0db9b48afc637d09f68fea77a17be8d8175e91d4
KomaHe <khthds@163.com>
2022-10-18 15:36:57 +0800

    Feature/thbim work20221018 (#3524)
    * new command THIFCHM
    
    * new command "THCAD2SERVERPUSH"
    
    Co-authored-by: THAPE\tiankaihe <1204973192@qq.com>

--- App/ThPlatform3D/ThPlatform3DIPCCmds.cs
+++ App/ThPlatform3D/ThPlatform3DIPCCmds.cs
@@ -77,6 +77,67 @@ namespace ThPlatform3D
             sw.Stop();
         }
 
+        [CommandMethod("TIANHUACAD", "THCAD2SERVERPUSH", CommandFlags.Modal)]
+        public void THCAD2SERVERPUSH()
+        {
+            Active.Editor.WriteLine($"开始读入图纸。");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var service = new ThDWGToIFCService("");
+            var project = service.DWGToProjectData(false, false);
+            if (project != null)
+            {
+                sw.Stop();
+                Active.Editor.WriteLine($"读入并解析图纸完成，耗时{sw.ElapsedMilliseconds}毫秒。");
+
+                sw.Reset();
+                Active.Editor.WriteLine($"开始传输数据。");
+                sw.Start();
+
+                using (var pipeClient = new NamedPipeClientStream(".",
+                    "THCAD2SERVERPIPE",
+                    PipeDirection.Out,
+                    PipeOptions.None,
+                    TokenImpersonationLevel.Impersonation))
+                {
+                    try
+                    {
+                        var bytes = project.ToThBimData(ProtoBufDataType.PushType, PlatformType.CADPlatform);
+                        pipeClient.Connect(5000);
+                        pipeClient.Write(bytes, 0, bytes.Length);
+                        Active.Editor.WriteLine($"传输数据完成，耗时{sw.ElapsedMilliseconds}毫秒。");
+                        using (AcadDatabase acdb = AcadDatabase.Active())
+                        {
+                            if (Active.Document.IsNamedDrawing)
+                            {
+                                var dwgFullName = Active.Document.Name;
+                                var path = Path.GetDirectoryName(dwgFullName);
+                                var fileName = Path.GetFileNameWithoutExtension(dwgFullName);
+                                var thbimPath = Path.Combine(path, fileName + ".thbim");
+                                //var thbimBytes = project.ToThBimData(ProtoBufDataType.ExternalLink, PlatformType.CADPlatform);
+                                var thbimBytes = bytes;
+                                thbimBytes[2] = 3;
+                                using (var stream = new FileStream(thbimPath, FileMode.Create))
+                                {
+                                    stream.Write(thbimBytes, 0, thbimBytes.Length);
+                                }
+                                Active.Editor.WriteLine($"外链文件生成，路径：[{thbimPath}]");
+                            }
+                            else
+                            {
+                                Active.Editor.WriteLine($"未检测到当前打开的DWG文件目录，生成外链文件失败!");
+                            }
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Active.Editor.WriteLine($"传输数据失败：{ex.Message}。");
+                    }
+                }
+            }
+            sw.Stop();
+        }
+
         [CommandMethod("TIANHUACAD", "THCAD2SUPUSH", CommandFlags.Modal)]
         public void THCAD2SUPUSH()
         {
