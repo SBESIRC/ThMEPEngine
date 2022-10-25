d70d68b3198639f59b30628a604e55579b38b194
董士崇 <dongshichong@thape.com.cn>
2022-10-25 17:27:57 +0800

    Upgrade ProductCode for version 1.9.47.0

--- App/ThMEPLighting/Properties/AssemblyInfo.cs
+++ App/ThMEPLighting/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.46.0")]
-[assembly: AssemblyFileVersion("1.9.46.0")]
+[assembly: AssemblyVersion("1.9.47.0")]
+[assembly: AssemblyFileVersion("1.9.47.0")]
 
 [assembly: CommandClass(typeof(ThMEPLighting.ThFEICmds))]
 [assembly: CommandClass(typeof(ThMEPLighting.ThEmgLightCmds))]
