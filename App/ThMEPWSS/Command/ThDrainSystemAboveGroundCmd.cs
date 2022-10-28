6d8d33a3eaf3745d1327d90bc1bcde8603d34816
余忠晟 <59475372+Yzhongsheng@users.noreply.github.com>
2022-10-28 10:58:31 +0800

    修改天正元素输出时误增的cad图块 (#3558)
    Co-authored-by: 余忠晟 <yuzhongsheng@thape.com.cn>

--- App/ThMEPWSS/Command/ThDrainSystemAboveGroundCmd.cs
+++ App/ThMEPWSS/Command/ThDrainSystemAboveGroundCmd.cs
@@ -284,7 +284,6 @@ namespace ThMEPWSS.Command
                     var roofCheck = new RoofCollisionCheck(roofFloors, _allRailings);
                     var addPLines = roofCheck.GetCheckResults(createBlockInfos.Where(c => !string.IsNullOrEmpty(c.tag) && (c.tag == "FL" || c.tag == "PL")).ToList());
                     createBasicElems.AddRange(addPLines);
-                    
                     if (OutputTCHElements)
                     {
                         PrintTCHElements(pipeTags);
@@ -381,9 +380,9 @@ namespace ThMEPWSS.Command
                 createBasicElems = createBasicElems.Where(c => !notCreateLineIds.Any(x => x == c.uid))/*.Where(e => !e.ConvertToTCHElement)*/.ToList();
                 createTextElems = createTextElems.Where(c => !notCreateTextIds.Any(x => x == c.uid))/*.Where(e => !e.ConvertToTCHElement)*/.ToList();
                 ConvertCoordinateToWCS(ref createBlockInfos, ref createBasicElems, ref createTextElems, Active.Editor.UCS2WCS());
-                var createBlocks = CreateBlockService.CreateBlocks(acdb.Database, createBlockInfos);
-                var createElems = CreateBlockService.CreateBasicElement(acdb.Database, createBasicElems);
-                var createTexts = CreateBlockService.CreateTextElement(acdb.Database, createTextElems);
+                //var createBlocks = CreateBlockService.CreateBlocks(acdb.Database, createBlockInfos);
+                //var createElems = CreateBlockService.CreateBasicElement(acdb.Database, createBasicElems);
+                //var createTexts = CreateBlockService.CreateTextElement(acdb.Database, createTextElems);
                 ConvertTCHPipeToWCS(ref verPipes, Active.Editor.UCS2WCS());
                 ConvertSymbMultiLeadersToWCS(ref symbMultiLeaders, Active.Editor.UCS2WCS());
                 tchPipeService.InitPipe(verPipes);
