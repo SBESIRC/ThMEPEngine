152bcb8cc6518a63248adc992a8d9d7cc27a0610
董士崇 <dongshichong@thape.com.cn>
2022-11-17 13:33:16 +0800

    Implement ThMEPBOLService

---
+++ App/ThMEPEngineCore/CAD/ThMEPBOLService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using AcApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace ThMEPEngineCore.CAD
+{
+    public class ThMEPBOLService
+    {
+        public static List<string> Layers()
+        {
+            var names = new List<string>();
+            foreach (var item in AcApp.UIBindings.Collections.Layers)
+            {
+                var properties = item.GetProperties();
+                names.Add(properties["Name"].GetValue(item) as string);
+            }
+            return names;
+        }
+
+        public static List<string> Blocks()
+        {
+            var names = new List<string>();
+            foreach (var item in AcApp.UIBindings.Collections.Blocks)
+            {
+                var properties = item.GetProperties();
+                names.Add(properties["Name"].GetValue(item) as string);
+            }
+            return names;
+        }
+    }
+}
