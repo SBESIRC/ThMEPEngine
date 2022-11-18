d56369b13de4a7229f368e9d2558782714241ded
Spacee <649489943@qq.com>
2022-11-18 14:14:12 +0800

    carsporter
    rough version

--- App/ThParkingStall.Core/LaneDeformation/Model/BlockNode.cs
+++ App/ThParkingStall.Core/LaneDeformation/Model/BlockNode.cs
@@ -19,6 +19,7 @@ namespace ThParkingStall.Core.LaneDeformation
         public Coordinate RightUpPoint;
         public List<List<BlockNode>> NeighborNodes;
         protected List<double> MoveTolerances;
+        protected List<double> Movements;
         protected List<int> InDegrees;
         public BlockNode()  //父类无参构造
         {
@@ -41,6 +42,7 @@ namespace ThParkingStall.Core.LaneDeformation
             NeighborNodes.Add(new List<BlockNode>());
             NeighborNodes.Add(new List<BlockNode>());
             MoveTolerances = new List<double> { 0, 0 };
+            Movements = new List<double> { 0, 0 };
             InDegrees = new List<int> { 0, 0 };
         }
         protected virtual void InitCoordinates()
@@ -73,11 +75,21 @@ namespace ThParkingStall.Core.LaneDeformation
         {
             return MoveTolerances[(int)dir] + SelfTolerance;
         }
+        public double Movement(PassDirection dir)
+        {
+            return Movements[(int)dir];
+        }
         public virtual double ToleranceForChild(PassDirection dir, double left, double right)
         {
             if (left > RightUpPoint.X || right < LeftDownPoint.X)
                 return -1;
             return Tolerance(dir);
+        }
+        public virtual double MovementForChild(PassDirection dir, double left, double right)
+        {
+            if (left > RightUpPoint.X || right < LeftDownPoint.X)
+                return 0;
+            return Movement(dir);
         }
         public double MoveTolerance(PassDirection dir)
         {
@@ -86,6 +98,10 @@ namespace ThParkingStall.Core.LaneDeformation
         public void SetMoveTolerance(PassDirection dir, double tolerance)
         {
             MoveTolerances[(int)dir] = tolerance;
+        }
+        public void SetMovement(PassDirection dir, double movement)
+        {
+            Movements[(int)dir] = movement;
         }
         public int InDegree(PassDirection dir)
         {
@@ -110,6 +126,10 @@ namespace ThParkingStall.Core.LaneDeformation
             if (t2 < 0)
                 return t1;
             return Math.Min(t1, t2);
+        }
+        public static double MaxMove(double t1, double t2)
+        {
+            return Math.Max(t1, t2);
         }
     }
     public class SpotBlock : BlockNode
@@ -158,18 +178,18 @@ namespace ThParkingStall.Core.LaneDeformation
         {
             public double ValueRight;
             public double Coord;
-            public MarkPoint(double coord, double tolerance)
+            public MarkPoint(double coord, double value)
             {
                 Coord = coord;
-                ValueRight = tolerance;
+                ValueRight = value;
             }
         }
+        // 容差值
         public List<MarkPoint> ToleranceTable = null;
-        // 接口
         public void InitTolerances(PassDirection passDir, bool narrow = false)
         {
-            InitTable(passDir, narrow);
-            MergeTable();
+            InitToleranceTable(passDir, narrow);
+            MergeToleranceTable();
         }
         public override double ToleranceForChild(PassDirection passDir, double left, double right)
         {
@@ -193,7 +213,7 @@ namespace ThParkingStall.Core.LaneDeformation
 
             return res;
         }
-        private void InitTable(PassDirection passDir, bool narrow = false)
+        private void InitToleranceTable(PassDirection passDir, bool narrow = false)
         {
             ToleranceTable = new List<MarkPoint>
             {
@@ -249,7 +269,7 @@ namespace ThParkingStall.Core.LaneDeformation
                     ToleranceTable.Insert(lm + 1, leftMark);
             }
         }
-        private void MergeTable()
+        private void MergeToleranceTable()
         {
             var deleteList = new List<int>();
             // 合并值相同的区域
@@ -287,6 +307,131 @@ namespace ThParkingStall.Core.LaneDeformation
             }
             for (int i = deleteList.Count - 1; i >= 0; i--)
                 ToleranceTable.RemoveAt(deleteList[i]);
+        }
+        // 移动值
+        public List<MarkPoint> MovementTable = null;
+        public void InitMovements(PassDirection passDir, bool expand = false)
+        {
+            InitMovementTable(passDir, expand);
+            MergeMovementTable();
+        }
+        public override double MovementForChild(PassDirection passDir, double left, double right)
+        {
+            left = Math.Max(left, LeftDownPoint.X);
+            right = Math.Min(right, RightUpPoint.X);
+            if (left >= right)
+                return 0;
+
+            if (MovementTable is null)
+                return Movement(passDir);
+
+            int cur = 0;
+            while (cur < MovementTable.Count && MovementTable[cur].Coord <= left)
+                cur++;
+            double res = MovementTable[cur - 1].ValueRight;
+            while (cur < MovementTable.Count && MovementTable[cur].Coord < right)
+            {
+                res = MaxMove(res, MovementTable[cur].ValueRight);
+                cur++;
+            }
+
+            return res;
+        }
+        private void InitMovementTable(PassDirection passDir, bool expand = false)
+        {
+            MovementTable = new List<MarkPoint>
+            {
+                new MarkPoint(LeftDownPoint.X, -1),
+                new MarkPoint(RightUpPoint.X, 0)
+            };
+            if (LastNodes(passDir).Count == 0)
+            {
+                MovementTable[0].ValueRight = 0;
+                return;
+            }
+
+            var segList = new List<ValueTuple<double, double, double>>();
+            foreach (var node in this.LastNodes(passDir))
+            {
+                if (node is BreakableBlock bb && bb.MovementTable != null)
+                {
+                    for (int i = 0; i < bb.MovementTable.Count - 1; i++)
+                    {
+                        segList.Add((bb.MovementTable[i].Coord, bb.MovementTable[i + 1].Coord, bb.MovementTable[i].ValueRight));
+                    }
+                }
+                else
+                {
+                    segList.Add((node.LeftDownPoint.X, node.RightUpPoint.X, node.Movement(passDir)));
+                }
+            }
+            foreach (var seg in segList)
+            {
+                var left = Math.Max(seg.Item1 - (expand ? VehicleLane.VehicleLaneWidth : 0), LeftDownPoint.X);
+                var right = Math.Min(seg.Item2 + (expand ? VehicleLane.VehicleLaneWidth : 0), RightUpPoint.X);
+                var value = MaxMove(seg.Item3 - SelfTolerance, 0);
+                if (left >= right) continue;
+
+                int cur = 0;
+                while (cur < MovementTable.Count && MovementTable[cur].Coord <= left)
+                    cur++;
+                int lm = cur - 1;
+                while (cur < MovementTable.Count && MovementTable[cur].Coord < right)
+                    cur++;
+                int rm = cur;
+
+                var leftMark = new MarkPoint(left, MaxMove(value, MovementTable[lm].ValueRight));
+                var rightMark = new MarkPoint(right, MovementTable[rm - 1].ValueRight);
+
+                for (int i = lm + 1; i < rm; i++)
+                    MovementTable[i].ValueRight = MaxMove(value, MovementTable[i].ValueRight);
+
+                if (right < MovementTable[rm].Coord)
+                    MovementTable.Insert(rm, rightMark);
+                if (left == MovementTable[lm].Coord)
+                    MovementTable[lm] = leftMark;
+                else
+                    MovementTable.Insert(lm + 1, leftMark);
+            }
+        }
+        private void MergeMovementTable()
+        {
+            var deleteList = new List<int>();
+            // 合并值相同的区域
+            for (int i = 1; i < MovementTable.Count - 1; i++)
+            {
+                if (MovementTable[i].ValueRight == MovementTable[i - 1].ValueRight)
+                    deleteList.Add(i);
+            }
+            for (int i = deleteList.Count - 1; i >= 0; i--)
+                MovementTable.RemoveAt(deleteList[i]);
+            // 去掉值为-1的区域
+            if (MovementTable[0].ValueRight < 0)
+            {
+                if (MovementTable[1].Coord - MovementTable[0].Coord < 10)
+                    MovementTable[0].ValueRight = MovementTable[1].ValueRight;
+                else
+                    MovementTable[0].ValueRight = 0;
+            }
+            for (int i = 1; i < MovementTable.Count - 1; i++)
+            {
+                if (MovementTable[i].ValueRight < 0)
+                {
+                    if (MovementTable[i + 1].Coord - MovementTable[i].Coord < 10)
+                        MovementTable[i].ValueRight = Math.Min(MovementTable[i - 1].ValueRight, MovementTable[i + 1].ValueRight);
+                    else
+                        MovementTable[i].ValueRight = 0;
+                }
+            }
+            // 合并值相同的区域
+            deleteList.Clear();
+            for (int i = 1; i < MovementTable.Count - 1; i++)
+            {
+                if (MovementTable[i].ValueRight == MovementTable[i - 1].ValueRight)
+                    deleteList.Add(i);
+            }
+            for (int i = deleteList.Count - 1; i >= 0; i--)
+                MovementTable.RemoveAt(deleteList[i]);
         }
     }
     public class LaneBlock : BreakableBlock
---
+++ App/ThParkingStall.Core/LaneDeformation/Service/CarsPorter.cs
@@ -0,0 +1,129 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+
+namespace ThParkingStall.Core.LaneDeformation
+{
+
+    class CarsPorter
+    {
+        private List<BlockNode> Nodes;
+        private Queue<BlockNode> WaitQueue = new Queue<BlockNode>();
+        private PassDirection Dir;
+        public CarsPorter(List<BlockNode> nodes)
+        {
+            Nodes = nodes;
+        }
+        void Preprocess()
+        {
+            WaitQueue.Clear();
+            // 从移动车位开始寻找所有孩子
+            // 被找到的孩子入度加1
+            // 入度为0则入队
+        }
+        public void Run(PassDirection dir)
+        {
+            Dir = dir;
+            Preprocess();
+            while (WaitQueue.Count > 0)
+            {
+                BlockNode node = WaitQueue.Dequeue();
+                if (node is FreeBlock free)
+                {
+                    free.InitMovements(Dir);
+                }
+                else if (node is SpotBlock spot)
+                {
+                    GetMaxMovement(spot);
+                    if (spot.Movement(dir) > 0)
+                    {
+                        // OutPut.MoveSpots.Add(spot);
+                    }
+                }
+                else if (node is ParkBlock park)
+                {
+                    if (park.IsAnchor)
+                        park.SetMovement(Dir, 0);
+                    else
+                        GetMaxMovement(park);
+                }
+                else if (node is LaneBlock lane)
+                {
+                    // 不可动车道
+                    if (lane.IsAnchor)
+                    {
+                        lane.SetMovement(Dir, 0);
+                    }
+                    // 横向车道
+                    else if (lane.IsHorizontal)
+                    {
+                        lane.InitMovements(Dir, lane.IsFtherLane[(int)Dir]);
+                    }
+                    // 其余车道
+                    else
+                    {
+                        GetMaxMovement(lane);
+                    }
+                }
+
+                // 更新后继的入度,为0则入队
+                UpdateNextNodes(node);
+            }
+        }
+        private void UpdateNextNodes(BlockNode node)
+        {
+            foreach (BlockNode n in node.NextNodes(Dir))
+            {
+                n.InDegreeDecre(Dir);
+                if (n.InDegree(Dir) is 0)
+                    WaitQueue.Enqueue(n);
+            }
+        }
+        private void GetMaxMovement(BlockNode node)
+        {
+            if (node.LastNodes(Dir).Count is 0)
+                node.SetMovement(Dir, 0);
+            else
+            {
+                double max = 0;
+                foreach (BlockNode n in node.LastNodes(Dir))
+                {
+                    /*                    if (n.Type is BlockType.LANE &&
+                                            ((LaneBlock) n).)*/
+                    max = BlockNode.MaxMove(n.MovementForChild(Dir, node.LeftDownPoint.X, node.RightUpPoint.X), max);
+                }
+                node.SetMovement(Dir, max);
+            }
+        }
+        public void Pipeline()
+        {
+            Run(PassDirection.BACKWARD);
+            //Run(PassDirection.BACKWARD);
+
+            // Draw
+/*            var pointsToDraw = new List<Point>();
+            var valuesToDraw = new List<double>();
+            foreach (var node in Nodes)
+            {
+                if (node is BreakableBlock bb && bb.ToleranceTable != null)
+                {
+                    for (int i = 0; i < bb.ToleranceTable.Count - 1; i++)
+                    {
+                        Point p = new Point(bb.ToleranceTable[i].Coord, node.Obb.Centroid.Y);
+                        pointsToDraw.Add(p);
+                        valuesToDraw.Add(bb.ToleranceTable[i].ValueRight);
+                    }
+                }
+                else
+                {
+                    Point p = new Point(node.Obb.Centroid.X - 800, node.Obb.Centroid.Y);
+                    pointsToDraw.Add(p);
+                    valuesToDraw.Add(node.Tolerance(PassDirection.FORWARD));
+                }
+            }
+            LDOutput.DrawTmpOutPut0.TolerancePositions = pointsToDraw;
+            LDOutput.DrawTmpOutPut0.ToleranceResults = valuesToDraw;*/
+        }
+
+    }
+}
