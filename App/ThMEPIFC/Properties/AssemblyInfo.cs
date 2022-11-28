b2bd37298d99ce994be0d34ff93e259352beb84c
董士崇 <dongshichong@thape.com.cn>
2022-11-28 18:42:57 +0800

    Bump version to 1.9.50.0

--- App/ThMEPIFC/Properties/AssemblyInfo.cs
+++ App/ThMEPIFC/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using Autodesk.AutoCAD.Runtime;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.49.0")]
-[assembly: AssemblyFileVersion("1.9.49.0")]
+[assembly: AssemblyVersion("1.9.50.0")]
+[assembly: AssemblyFileVersion("1.9.50.0")]
 
 [assembly: CommandClass(typeof(ThMEPIFC.ThMEPIFCApp))]
 [assembly: CommandClass(typeof(ThMEPIFC.ThMEPIFCExportCmds))]
